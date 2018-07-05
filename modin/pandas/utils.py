4497d5f17ef1f54348e51a4f69a37ee583f5cae0
Simon Mo <xmo@berkeley.edu>
2018-07-05 09:44:12 -0700

    Flake8

--- modin/pandas/utils.py
+++ modin/pandas/utils.py
@@ -410,7 +410,8 @@ def _blocks_to_col(*partition):
     if len(partition):
         df = pandas.DataFrame(np.concatenate(partition, axis=0))
 
-        if len(np.unique(partition[0].dtypes.values)) > 1:  # heterogenous types
+        # heterogenous types
+        if len(np.unique(partition[0].dtypes.values)) > 1:
             df = df.astype(partition[0].dtypes, copy=False)
 
         return df
