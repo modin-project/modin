957c2c0802c3a09df19503f19709ec0ef04b4171
Kunal Gosar <gosarkunal9@gmail.com>
2018-07-06 14:46:21 -0500

    cleaning log file name

--- benchmarks/run_benchmark.py
+++ benchmarks/run_benchmark.py
@@ -5,7 +5,7 @@ files = os.listdir("benchmarks/data")
 for f in files:
     os.system("python benchmarks/arithmetic_benchmark.py "
               "--path benchmarks/data/{} "
-              "--logfile benchmark-results/aaamodin-arithmetic.log".format(f))
+              "--logfile benchmark-results/modin-arithmetic.log".format(f))
     os.system("python benchmarks/io_benchmark.py "
               "--path benchmarks/data/{} "
-              "--logfile benchmark-results/aaamodin-io.log".format(f))
+              "--logfile benchmark-results/modin-io.log".format(f))
