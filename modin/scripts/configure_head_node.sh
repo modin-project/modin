0e64cac087e0d7f9887ad95156bb20e575643c85
Peter Schafhalter <pschafhalter@berkeley.edu>
2018-07-19 11:25:58 -0700

    Use system python

--- modin/scripts/configure_head_node.sh
+++ modin/scripts/configure_head_node.sh
@@ -4,7 +4,8 @@ HOSTNAME=$1
 KEY=$2
 
 ssh -i $2 -o "StrictHostKeyChecking no" $1 << "ENDSSH"
-pip3 install modin jupyter
+python -m pip install modin jupyter
+PATH=$PATH:~/.local/bin/    # ensure Ray is in the path
 ray stop
 ray start --head
 ENDSSH
--- modin/scripts/configure_node.sh
+++ modin/scripts/configure_node.sh
@@ -5,7 +5,7 @@ KEY=$2
 REDIS_ADDRESS=$3
 
 ssh -i $2 -o "StrictHostKeyChecking no" $1 REDIS_ADDRESS=$REDIS_ADDRESS "bash -s" << "ENDSSH"
-pip3 install modin
+python -m pip install modin
 PATH=$PATH:~/.local/bin/    # ensure Ray is in the path
 ray stop
 ray start --redis-address $REDIS_ADDRESS
