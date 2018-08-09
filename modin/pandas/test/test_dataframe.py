c6a62dfda7a749ac01df4bfb71356ce0715e80c2
frreiss <frreiss@us.ibm.com>
2018-08-09 13:35:53 -0700

    Fix whitespace

--- modin/pandas/test/test_dataframe.py
+++ modin/pandas/test/test_dataframe.py
@@ -3415,11 +3415,11 @@ def test___repr__():
     #
     # assert repr(pandas_df) == repr(ray_df)
 
-    # ___repr___ method has a different code path depending on 
+    # ___repr___ method has a different code path depending on
     # whether the number of rows is >60; and a different code path
-    # depending on the number of columns is >20.  
-    # Previous test cases already check the case when cols>20 
-    # and rows>60. The cases that follow exercise the other three 
+    # depending on the number of columns is >20.
+    # Previous test cases already check the case when cols>20
+    # and rows>60. The cases that follow exercise the other three
     # combinations.
     # rows <= 60, cols > 20
     frame_data = np.random.randint(0, 100, size=(10, 100))
