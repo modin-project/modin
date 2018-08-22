ae9f397109620cf00243169654f2f4bec7809b72
Devin Petersohn <devin-petersohn@users.noreply.github.com>
2018-08-22 12:52:04 -0700

    Adding __git_revision__ and __version__ (#73)
    * Adding __git_revision__ and __version__
    
    * Lint

--- modin/__init__.py
+++ modin/__init__.py
@@ -1,1 +1,28 @@
+import os
+import subprocess
+
+
+def git_version():
+    def _execute_cmd_in_temp_env(cmd):
+        # construct environment
+        env = {}
+        for k in ['SYSTEMROOT', 'PATH', 'HOME']:
+            v = os.environ.get(k)
+            if v is not None:
+                env[k] = v
+        # LANGUAGE is used on win32
+        env['LANGUAGE'] = 'C'
+        env['LANG'] = 'C'
+        env['LC_ALL'] = 'C'
+        return subprocess.Popen(
+            cmd, stdout=subprocess.PIPE, env=env).communicate()[0]
+
+    try:
+        git_revision = _execute_cmd_in_temp_env(['git', 'rev-parse', 'HEAD'])
+        return git_revision.strip().decode('ascii')
+    except OSError:
+        return "Unknown"
+
+
+__git_revision__ = git_version()
 __version__ = "0.1.1"
