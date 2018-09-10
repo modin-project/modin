c9e1b085479d5584e029ddb7981bde1e60bbec85
Simon Mo <xmo@berkeley.edu>
2018-09-09 18:18:17 -0700

    Update install-dependencies.sh

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -4,5 +4,4 @@ wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O mi
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 pip install -r requirements.txt
-pip install -q pandas==0.22 feather-format lxml openpyxl xlrd
-pip install -q flake8 flake8-comprehensions yapf
+pip install -q pytest flake8 flake8-comprehensions yapf pandas==0.22 feather-format lxml openpyxl xlrd
