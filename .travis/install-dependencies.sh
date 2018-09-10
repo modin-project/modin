c9d15f2aed4046013ca3c173ce81eee431c3b9a5
Simon Mo <xmo@berkeley.edu>
2018-09-09 22:54:54 -0700

    Update install-dependencies.sh

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -3,5 +3,8 @@
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh -nv
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
-pip install -r requirements.txt
-pip install -q pytest flake8 flake8-comprehensions yapf pandas==0.22 feather-format lxml openpyxl xlrd
+
+# There was a pandas + numpy installation version issue
+# https://github.com/pandas-dev/pandas/issues/20775
+PIP_NO_BUILD_ISOLATION=false pip install -r requirements.txt
+pip install -q pytest flake8 flake8-comprehensions yapf feather-format lxml openpyxl xlrd numpy
