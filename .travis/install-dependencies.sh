6044828ea209ddad8ff21f5855bb2f3ec4b0d752
Simon Mo <xmo@berkeley.edu>
2018-09-16 16:16:28 -0700

    Update install-dependencies.sh

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
- if [[ "$PYTHON" == "2.7" ]]; then
+if [[ "$PYTHON" == "2.7" ]]; then
   wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh -nv
   bash miniconda.sh -b -p $HOME/miniconda
   export PATH="$HOME/miniconda/bin:$PATH"
@@ -10,6 +10,8 @@ elif [[ "$PYTHON" == "3.6" ]]; then
   bash miniconda.sh -b -p $HOME/miniconda
   export PATH="$HOME/miniconda/bin:$PATH"
   conda install -y python==3.6.5
+
+fi
 
 pip install -r requirements.txt
 pip install -q pytest flake8 flake8-comprehensions yapf feather-format lxml openpyxl xlrd numpy
