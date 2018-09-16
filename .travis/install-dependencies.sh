85dda5db77f4c5c6fd4fc80441aea8d9d369f302
Simon Mo <xmo@berkeley.edu>
2018-09-16 16:25:36 -0700

    Update install-dependencies.sh

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -5,12 +5,11 @@ if [[ "$PYTHON" == "2.7" ]]; then
   bash miniconda.sh -b -p $HOME/miniconda
   export PATH="$HOME/miniconda/bin:$PATH"
 
-elif [[ "$PYTHON" == "3.6" ]]; then
+elif [[ "$PYTHON" == "3.6" ]] || [[ "$LINT" == "1" ]]; then
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh -nv
   bash miniconda.sh -b -p $HOME/miniconda
   export PATH="$HOME/miniconda/bin:$PATH"
   conda install -y python==3.6.5
-
 fi
 
 pip install -r requirements.txt
