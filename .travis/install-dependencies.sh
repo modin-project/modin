dbb24786ffb05e876a5b6e20516e692587243378
simon-mo <xmo@berkeley.edu>
2018-09-23 22:46:41 +0000

    [Travis] Install py3.6 for lint

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -45,6 +45,7 @@ elif [[ "$LINT" == "1" ]]; then
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh -nv
   bash miniconda.sh -b -p $HOME/miniconda
   export PATH="$HOME/miniconda/bin:$PATH"
+  conda install -y python==3.6.5
   
 else
   echo "Unrecognized environment."
