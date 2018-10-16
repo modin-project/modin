4564d3c9cd4050e9632b9b6607a79dcaf72e719d
Simon Mo <xmo@berkeley.edu>
2018-10-15 23:52:08 -0700

    Pass env var to docker

--- .jenkins/run_ci.sh
+++ .jenkins/run_ci.sh
@@ -2,5 +2,8 @@ set -ex
 
 sha_tag=`git rev-parse --verify --short HEAD`
 docker build -t modin-project/test:$sha_tag -f .jenkins/Dockerfile .
-docker run --rm --shm-size=1g --cpuset-cpus="0-3" modin-project/test:$sha_tag
+docker run --rm --shm-size=1g \
+	-e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
+	-e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
+	modin-project/test:$sha_tag
 
