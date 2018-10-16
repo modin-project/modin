1352c3a11570875eac51cda1c0368ca7077f4699
Simon Mo <xmo@berkeley.edu>
2018-10-15 22:54:23 -0700

    Upload html to aws

--- .jenkins/run_test.sh
+++ .jenkins/run_test.sh
@@ -1,6 +1,15 @@
 set -ex
+
+pip install awscli pytest-html
+
 python -c "import ray; ray.init()"
-python -m pytest --disable-pytest-warnings modin/pandas/test/test_dataframe.py
-python -m pytest --disable-pytest-warnings modin/pandas/test/test_concat.py
-python -m pytest --disable-pytest-warnings modin/pandas/test/test_io.py
-python -m pytest --disable-pytest-warnings modin/pandas/test/test_groupby.py
+pytest --html=test_dataframe.html --self-contained-html modin/pandas/test/test_dataframe.py
+pytest --html=test_concat.html --self-contained-html modin/pandas/test/test_concat.py
+pytest --html=test_io.html --self-contained-html modin/pandas/test/test_io.py
+pytest --html=test_groupby.html --self-contained-html modin/pandas/test/test_groupby.py
+
+sha_tag=`git rev-parse --verify --short HEAD`
+aws cp test_dataframe.html s3://modin-jenkins-result/${sha_tag}/
+aws cp test_concat.html s3://modin-jenkins-result/${sha_tag}/
+aws cp test_io.html s3://modin-jenkins-result/${sha_tag}/
+aws cp test_groupby.html s3://modin-jenkins-result/${sha_tag}/
