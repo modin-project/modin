14f69e27e4b82adb2ac14e9d34ffecbf30e67ba5
Simon Mo <xmo@berkeley.edu>
2018-10-15 23:12:29 -0700

    Use aws s3

--- .jenkins/run_test.sh
+++ .jenkins/run_test.sh
@@ -9,7 +9,7 @@ pytest --html=test_io.html --self-contained-html modin/pandas/test/test_io.py
 pytest --html=test_groupby.html --self-contained-html modin/pandas/test/test_groupby.py
 
 sha_tag=`git rev-parse --verify --short HEAD`
-aws cp test_dataframe.html s3://modin-jenkins-result/${sha_tag}/
-aws cp test_concat.html s3://modin-jenkins-result/${sha_tag}/
-aws cp test_io.html s3://modin-jenkins-result/${sha_tag}/
-aws cp test_groupby.html s3://modin-jenkins-result/${sha_tag}/
+aws s3 cp test_dataframe.html s3://modin-jenkins-result/${sha_tag}/
+aws s3 cp test_concat.html s3://modin-jenkins-result/${sha_tag}/
+aws s3 cp test_io.html s3://modin-jenkins-result/${sha_tag}/
+aws s3 cp test_groupby.html s3://modin-jenkins-result/${sha_tag}/
