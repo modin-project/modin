080dc9ce4c2ea89be06b2deb4f19a440dbd954af
Simon Mo <xmo@berkeley.edu>
2018-10-15 22:36:01 -0700

    Print ray init result

--- .jenkins/run_test.sh
+++ .jenkins/run_test.sh
@@ -1,5 +1,5 @@
 set -ex
-
+python -c "import ray; ray.init()"
 python -m pytest --disable-pytest-warnings modin/pandas/test/test_dataframe.py
 python -m pytest --disable-pytest-warnings modin/pandas/test/test_concat.py
 python -m pytest --disable-pytest-warnings modin/pandas/test/test_io.py
