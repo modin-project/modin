432098a706f08b8c39adddd0374b4a955d5d5df7
Simon Mo <xmo@berkeley.edu>
2018-10-15 21:43:04 -0700

    Add Docker Files

---
+++ .jenkins/run_test.sh
@@ -0,0 +1,6 @@
+set -ex
+
+python -m pytest --disable-pytest-warnings modin/pandas/test/test_dataframe.py
+python -m pytest --disable-pytest-warnings modin/pandas/test/test_concat.py
+python -m pytest --disable-pytest-warnings modin/pandas/test/test_io.py
+python -m pytest --disable-pytest-warnings modin/pandas/test/test_groupby.py
