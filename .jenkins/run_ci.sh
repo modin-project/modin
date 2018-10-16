eb246106638fda33213ebeb9834adeaf24284095
Simon Mo <xmo@berkeley.edu>
2018-10-15 22:20:56 -0700

    Pin to 4 cores

--- .jenkins/run_ci.sh
+++ .jenkins/run_ci.sh
@@ -2,5 +2,5 @@ set -ex
 
 sha_tag=`git rev-parse --verify --short HEAD`
 docker build -t modin-project/test:$sha_tag -f .jenkins/Dockerfile .
-docker run --rm --shm-size=1g modin-project/test:$sha_tag
+docker run --rm --shm-size=1g --cpus=4.0 modin-project/test:$sha_tag
 
