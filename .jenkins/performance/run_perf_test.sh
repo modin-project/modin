ef26e3cc7906dc65b311236e824ae88daec715ae
Omkar Salpekar <osalpekar@berkeley.edu>
2018-10-25 16:33:01 -0700

    adding perf scipts

---
+++ .jenkins/performance/run_perf_test.sh
@@ -0,0 +1,14 @@
+set -x
+
+pip install awscli
+python .jenkins/inject_aws_credentials.py
+
+python -c "import ray; ray.init()"
+pytest --benchmark-autosave --disable-pytest-warnings modin/pandas/test/test_performance.py
+
+sha_tag=`git rev-parse --verify --short HEAD`
+# save the results to S3
+# aws s3 cp .benchmark/*/*.json s3://modin-jenkins-performance-result/${sha_tag}/
+
+# delete the .benchmarks directory
+rm -rf .benchmarks
