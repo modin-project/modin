c48653eb2ae94bce7cac349d16e56b15d3a7ee9f
Omkar Salpekar <osalpekar@berkeley.edu>
2018-10-27 17:02:16 -0700

    remove s3 ls statements

--- .jenkins/performance-tests/run_perf_test.sh
+++ .jenkins/performance-tests/run_perf_test.sh
@@ -9,7 +9,5 @@ pytest --benchmark-autosave --disable-pytest-warnings modin/pandas/test/test_per
 sha_tag=`git rev-parse --verify --short HEAD`
 # save the results to S3
 aws s3 cp .benchmarks/*/*.json s3://modin-jenkins-result/${sha_tag}-perf/ --acl public-read
-aws s3 ls s3://modin-jenkins-result/
-aws s3 ls s3://modin-jenkins-result/${sha_tag}-perf/
 
 rm -rf .benchmarks
