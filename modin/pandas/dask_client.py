2d3d68f0a6988bb6ee819f5af31158b7dbfdd062
Devin Petersohn <devin.petersohn@gmail.com>
2018-12-03 08:52:15 -0800

    Fixing python2 travis test failure

--- modin/pandas/dask_client.py
+++ modin/pandas/dask_client.py
@@ -1,11 +1,20 @@
-import functools
-
 import dask.distributed
 
 
-# the lru_cache prevents creating more clients
-@functools.lru_cache()
-def get_client():
+def _get_client():
     """Setup and get dask.distributed client"""
     # TODO add more parameters or better implement as a Client class
     return dask.distributed.Client()
+
+
+try:
+    from functools import lru_cache
+
+    # the lru_cache prevents creating more clients
+    @lru_cache()
+    def get_client():
+        return _get_client()
+# Python2 does not support lru_cache
+# TODO add a way to cache in Python2
+except AttributeError:
+    get_client = _get_client()
