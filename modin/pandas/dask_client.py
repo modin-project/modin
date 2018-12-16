4ea8deeed979237451a58011d7eeb154c2bfe9bb
Devin Petersohn <devin-petersohn@users.noreply.github.com>
2018-12-15 21:33:57 -0800

    Simplify Dask import and startup (#297)
    * Simplify Dask import and startup
    
    * Lint

--- modin/pandas/dask_client.py
+++
@@ -1,9 +0,0 @@
-import dask.distributed
-
-
-# TODO add a way to only initalize Client one time
-# Note: functools.lru_cache() is not supported in Python2
-def get_client():
-    """Setup and get dask.distributed client"""
-    # TODO add more parameters or better implement as a Client class
-    return dask.distributed.Client()
