872a2dfe030e47ca3fb693d23690fb7bb2a64608
Devin Petersohn <devin.petersohn@gmail.com>
2018-12-23 14:36:57 -0800

    Remove copy

--- modin/engines/ray/gandiva_on_ray/remote_partition.py
+++ modin/engines/ray/gandiva_on_ray/remote_partition.py
@@ -133,8 +133,6 @@ def deploy_ray_func(func, partition, kwargs):
     # Sometimes Arrow forces us to make a copy of an object before we operate
     # on it. We don't want the error to propagate to the user, and we want to
     # avoid copying unless we absolutely have to.
-    except ValueError:
-        result = func(partition.copy(), **kwargs)
     except:
         result = func(partition.to_pandas(), **kwargs)
         if isinstance(result, pandas.Series):
