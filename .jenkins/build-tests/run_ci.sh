26c55eeb91d2e804c94d8e00e253885119ca0a3e
Simon Mo <xmo@berkeley.edu>
2019-01-02 17:47:22 -0600

    Pass env var

--- .jenkins/build-tests/run_ci.sh
+++ .jenkins/build-tests/run_ci.sh
@@ -6,4 +6,5 @@ docker build -t modin-project/test:$sha_tag -f .jenkins/build-tests/Dockerfile .
 docker run --rm --shm-size=1g \
 	-e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
 	-e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
+	-e GITHUB_TOKEN=${GITHUB_TOKEN} \
 	modin-project/test:$sha_tag
