990f87efd66a2e2a243431f3c8f43827f9bc7d23
Devin Petersohn <devin-petersohn@users.noreply.github.com>
2019-01-03 12:37:08 -0800

    Filtering default exclude values in describe based on the include passed in (#363)
    * Filtering default exclude values based on the include passed in
    
    * Fix error checking

--- modin/pandas/dataframe.py
+++ modin/pandas/dataframe.py
@@ -1080,14 +1080,14 @@ class DataFrame(object):
 
         Returns: Series/DataFrame of summary statistics
         """
-        if exclude is None:
-            exclude = "object"
-        elif "object" not in include:
-            exclude = (
-                ([exclude] + ["object"])
-                if isinstance(exclude, str)
-                else list(exclude) + ["object"]
-            )
+        if include is not None:
+            if not is_list_like(include):
+                include = [include]
+            include = [np.dtype(i) for i in include]
+        if exclude is not None:
+            if not is_list_like(include):
+                exclude = [exclude]
+            exclude = [np.dtype(e) for e in exclude]
         if percentiles is not None:
             pandas.DataFrame()._check_percentile(percentiles)
         return DataFrame(
