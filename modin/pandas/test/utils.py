704579458c860b68c11c26af98e15ca4e35db63e
William Ma <12377941+williamma12@users.noreply.github.com>
2019-01-18 16:30:51 -0800

    Removed empty dataframes

--- modin/pandas/test/utils.py
+++ modin/pandas/test/utils.py
@@ -17,8 +17,8 @@ RAND_HIGH = 100
 # Input data and functions for the tests
 # The test data that we will test our code against
 test_data = {
-    "empty_data": {},
-    "columns_only": {"col1": [], "col2": [], "col3": [], "col4": [], "col5": []},
+    # "empty_data": {},
+    # "columns_only": {"col1": [], "col2": [], "col3": [], "col4": [], "col5": []},
     "int_data": {
         "col{}".format((i - NCOLS/2) % NCOLS + 1): random_state.randint(
             RAND_LOW, RAND_HIGH, size=(NROWS)
