79c111337ef2d0f6240821619a70e4583cf846d0
Devin Petersohn <devin-petersohn@users.noreply.github.com>
2019-01-24 10:38:32 -0800

    Bump version to 0.3.0 (#428)

--- modin/__init__.py
+++ modin/__init__.py
@@ -19,7 +19,7 @@ def get_partition_format():
     return "Pandas"
 
 
-__version__ = "0.3.0rc1"
+__version__ = "0.3.0"
 __execution_engine__ = get_execution_engine()
 __partition_format__ = get_partition_format()
 
