15844c6a31f7a32937a98b828fb8ab9f164b3fb4
William Ma <12377941+williamma12@users.noreply.github.com>
2019-01-25 01:06:30 -0800

    linting

--- modin/pandas/test/test_concat.py
+++ modin/pandas/test/test_concat.py
@@ -4,10 +4,15 @@ from __future__ import print_function
 
 import pytest
 import pandas
-import numpy as np
 import modin.pandas as pd
-from modin.pandas.utils import from_pandas, to_pandas
-from .utils import df_equals, test_data_keys, test_data_values, random_state, RAND_LOW, RAND_HIGH
+from .utils import (
+    df_equals,
+    test_data_keys,
+    test_data_values,
+    random_state,
+    RAND_LOW,
+    RAND_HIGH,
+)
 
 pd.DEFAULT_NPARTITIONS = 4
 
@@ -34,18 +39,26 @@ def test_modin_concat_with_series(data1, data2):
     modin_df2 = pd.DataFrame(data2)
 
     # Test axis=0
-    pandas_series = pandas.Series(random_state.random_integers(RAND_LOW, RAND_HIGH, pandas_df1.shape[1]))
-    modin_series = pd.Series(random_state.random_integers(RAND_LOW, RAND_HIGH, modin_df1.shape[1]))
+    pandas_series = pandas.Series(
+        random_state.random_integers(RAND_LOW, RAND_HIGH, pandas_df1.shape[1])
+    )
+    modin_series = pd.Series(
+        random_state.random_integers(RAND_LOW, RAND_HIGH, modin_df1.shape[1])
+    )
     df_equals(
         pd.concat([modin_df1, modin_df2, pandas_series], axis=0),
         pandas.concat([pandas_df1, pandas_df2, pandas_series], axis=0),
     )
 
     # Test axis=1
-    pandas_series = pandas.Series(random_state.randint(RAND_LOW, RAND_HIGH, pandas_df1.shape[0]))
-    modin_series = pd.Series(random_state.randint(RAND_LOW, RAND_HIGH, modin_df1.shape[0]))
+    pandas_series = pandas.Series(
+        random_state.randint(RAND_LOW, RAND_HIGH, pandas_df1.shape[0])
+    )
+    modin_series = pd.Series(
+        random_state.randint(RAND_LOW, RAND_HIGH, modin_df1.shape[0])
+    )
     df_equals(
-        pd.concat([modin_df1, modin_df2, pandas_series], axis=1),
+        pd.concat([modin_df1, modin_df2, modin_series], axis=1),
         pandas.concat([pandas_df1, pandas_df2, pandas_series], axis=1),
     )
 
@@ -69,7 +82,8 @@ def test_modin_concat_on_index(data1, data2):
     )
 
     df_equals(
-        pd.concat([modin_df1, modin_df2], axis=0), pandas.concat([pandas_df1, pandas_df2], axis=0)
+        pd.concat([modin_df1, modin_df2], axis=0),
+        pandas.concat([pandas_df1, pandas_df2], axis=0),
     )
 
 
@@ -82,7 +96,8 @@ def test_ray_concat_on_column(data1, data2):
     modin_df2 = pd.DataFrame(data2)
 
     df_equals(
-        pd.concat([modin_df1, modin_df2], axis=1), pandas.concat([pandas_df1, pandas_df2], axis=1)
+        pd.concat([modin_df1, modin_df2], axis=1),
+        pandas.concat([pandas_df1, pandas_df2], axis=1),
     )
 
     df_equals(
@@ -94,8 +109,6 @@ def test_ray_concat_on_column(data1, data2):
 @pytest.mark.parametrize("data1", test_data_values, ids=test_data_keys)
 @pytest.mark.parametrize("data2", test_data_values, ids=test_data_keys)
 def test_invalid_axis_errors(data1, data2):
-    pandas_df1 = pandas.DataFrame(data1)
-    pandas_df2 = pandas.DataFrame(data2)
     modin_df1 = pd.DataFrame(data1)
     modin_df2 = pd.DataFrame(data2)
 
@@ -114,7 +127,10 @@ def test_mixed_concat(data1, data2, data3):
     modin_df2 = pd.DataFrame(data2)
     modin_df3 = pd.DataFrame(data3)
 
-    df_equals(pd.concat([modin_df1, modin_df2, modin_df3]), pandas.concat([pandas_df1, pandas_df2, pandas_df3]))
+    df_equals(
+        pd.concat([modin_df1, modin_df2, modin_df3]),
+        pandas.concat([pandas_df1, pandas_df2, pandas_df3]),
+    )
 
 
 @pytest.mark.parametrize("data1", test_data_values, ids=test_data_keys)
@@ -128,4 +144,7 @@ def test_mixed_inner_concat(data1, data2, data3):
     modin_df2 = pd.DataFrame(data2)
     modin_df3 = pd.DataFrame(data3)
 
-    df_equals(pd.concat([modin_df1, modin_df2, modin_df3], join='inner'), pandas.concat([pandas_df1, pandas_df2, pandas_df3], join='inner'))
+    df_equals(
+        pd.concat([modin_df1, modin_df2, modin_df3], join="inner"),
+        pandas.concat([pandas_df1, pandas_df2, pandas_df3], join="inner"),
+    )
