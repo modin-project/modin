8055514ddfbbd6efaf16b7c6d5f4478eefe2d655
Alex Wu <itswu.alex@gmail.com>
2019-01-28 02:10:47 -0800

    Implemented `df.drop_duplicates()` and some simple tests for it
    Clean up
    
    Reformatted with black

--- modin/pandas/dataframe.py
+++ modin/pandas/dataframe.py
@@ -1267,9 +1267,28 @@ class DataFrame(object):
         return self._create_dataframe_from_compiler(new_query_compiler, inplace)
 
     def drop_duplicates(self, subset=None, keep="first", inplace=False):
-        return self._default_to_pandas(
-            pandas.DataFrame.drop_duplicates, subset=subset, keep=keep, inplace=inplace
-        )
+        """
+                Return DataFrame with duplicate rows removed, optionally only
+                considering certain columns
+                Parameters
+                ----------
+                subset : column label or sequence of labels, optional
+                    Only consider certain columns for identifying duplicates, by
+                    default use all of the columns
+                keep : {'first', 'last', False}, default 'first'
+                    - ``first`` : Drop duplicates except for the first occurrence.
+                    - ``last`` : Drop duplicates except for the last occurrence.
+                    - False : Drop all duplicates.
+                inplace : boolean, default False
+                    Whether to drop duplicates in place or to return a copy
+                Returns
+                -------
+                deduplicated : DataFrame
+        """
+        inplace = validate_bool_kwarg(inplace, "inplace")
+        duplicates = self.duplicated(subset=subset, keep=keep)
+        indices, = duplicates.nonzero()
+        return self.drop(indices, inplace=inplace)
 
     def duplicated(self, subset=None, keep="first"):
         return self._default_to_pandas(
