9330fb8a08b0841b5254f5efff92c6667a143bcf
Devin Petersohn <devin.petersohn@gmail.com>
2019-02-05 12:51:39 -0800

    Re-enable performance tests

--- modin/pandas/test/performance-tests/conftest.py
+++ modin/pandas/test/performance-tests/conftest.py
@@ -1,9 +1,9 @@
-# import subprocess
-# import os
-#
-#
-# def pytest_benchmark_update_commit_info(config, commit_info):
-#     commit_info["commit_number"] = int(
-#         subprocess.check_output(["git", "rev-list", "HEAD", "--count"]).strip()
-#     )
-#     commit_info["engine"] = os.environ.get("MODIN_ENGINE", "Ray").title()
+import subprocess
+import os
+
+
+def pytest_benchmark_update_commit_info(config, commit_info):
+    commit_info["commit_number"] = int(
+        subprocess.check_output(["git", "rev-list", "HEAD", "--count"]).strip()
+    )
+    commit_info["engine"] = os.environ.get("MODIN_ENGINE", "Ray").title()
