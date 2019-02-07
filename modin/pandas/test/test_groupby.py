91afb247fdd4960d8be6c4d0f4a33c394b7a90d6
Devin Petersohn <devin.petersohn@gmail.com>
2019-02-06 18:12:33 -0800

    Add fix that Resolves #460

--- modin/pandas/test/test_groupby.py
+++ modin/pandas/test/test_groupby.py
@@ -771,12 +771,22 @@ def test_groupby_on_index_values_with_loop():
     data = {
         "a": np.random.randint(0, 100, size=length),
         "b": np.random.randint(0, 100, size=length),
+        "c": np.random.randint(0, 100, size=length),
     }
     idx = ["g1" if i % 3 != 0 else "g2" for i in range(length)]
-    modin_df = pd.DataFrame(data, index=idx)
-    pandas_df = pandas.DataFrame(data, index=idx)
+    modin_df = pd.DataFrame(data, index=idx, columns=list("aba"))
+    pandas_df = pandas.DataFrame(data, index=idx, columns=list("aba"))
     modin_groupby_obj = modin_df.groupby(modin_df.index)
     pandas_groupby_obj = pandas_df.groupby(pandas_df.index)
+
+    modin_dict = {k: v for k, v in modin_groupby_obj}
+    pandas_dict = {k: v for k, v in pandas_groupby_obj}
+
+    for k in modin_dict:
+        ray_df_equals_pandas(modin_dict[k], pandas_dict[k])
+
+    modin_groupby_obj = modin_df.groupby(modin_df.columns, axis=1)
+    pandas_groupby_obj = pandas_df.groupby(pandas_df.columns, axis=1)
 
     modin_dict = {k: v for k, v in modin_groupby_obj}
     pandas_dict = {k: v for k, v in pandas_groupby_obj}
