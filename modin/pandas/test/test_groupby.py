010902fa7a1ad10dbc4be27fa44038ade683375c
Devin Petersohn <devin-petersohn@users.noreply.github.com>
2019-02-06 21:05:20 -0800

    Fixing issue with duplicated rows when looping through groupby object (#459)
    * Fixing issue with duplicated rows when looping through groupby object
    
    * Resolves #458
    * Only gets the unique values of the index
    * Add test to validate changes
    
    * Revert change to axis=1
    
    * Lint
    
    * Lint
    
    * Add fix that Resolves #460

--- modin/pandas/test/test_groupby.py
+++ modin/pandas/test/test_groupby.py
@@ -764,3 +764,32 @@ def test_groups(ray_groupby, pandas_groupby):
 @pytest.fixture
 def test_shift(ray_groupby, pandas_groupby):
     assert ray_groupby.groups == pandas_groupby.groups
+
+
+def test_groupby_on_index_values_with_loop():
+    length = 2 ** 6
+    data = {
+        "a": np.random.randint(0, 100, size=length),
+        "b": np.random.randint(0, 100, size=length),
+        "c": np.random.randint(0, 100, size=length),
+    }
+    idx = ["g1" if i % 3 != 0 else "g2" for i in range(length)]
+    modin_df = pd.DataFrame(data, index=idx, columns=list("aba"))
+    pandas_df = pandas.DataFrame(data, index=idx, columns=list("aba"))
+    modin_groupby_obj = modin_df.groupby(modin_df.index)
+    pandas_groupby_obj = pandas_df.groupby(pandas_df.index)
+
+    modin_dict = {k: v for k, v in modin_groupby_obj}
+    pandas_dict = {k: v for k, v in pandas_groupby_obj}
+
+    for k in modin_dict:
+        ray_df_equals_pandas(modin_dict[k], pandas_dict[k])
+
+    modin_groupby_obj = modin_df.groupby(modin_df.columns, axis=1)
+    pandas_groupby_obj = pandas_df.groupby(pandas_df.columns, axis=1)
+
+    modin_dict = {k: v for k, v in modin_groupby_obj}
+    pandas_dict = {k: v for k, v in pandas_groupby_obj}
+
+    for k in modin_dict:
+        ray_df_equals_pandas(modin_dict[k], pandas_dict[k])
