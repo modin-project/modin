modin-project/modin

commit 4c2332c52454b6638b1064dc8dd42aee7d8a3386

DataFrame.drop_duplicates (#433)
* Implemented `df.drop_duplicates()` and some simple tests for it

* Clean up

* Reformatted with black

* Implemented `df.drop_duplicates()` and some simple tests for it

Clean up

Reformatted with black

* Update modin/pandas/test/test_dataframe.py

Co-Authored-By: wuisawesome <itswu.alex@gmail.com>

* Update modin/pandas/dataframe.py

Co-Authored-By: wuisawesome <itswu.alex@gmail.com>

* Update modin/pandas/dataframe.py

Co-Authored-By: wuisawesome <itswu.alex@gmail.com>

* Updated test cases

* Using test_data_values correctly now

* Lint

modin/pandas/dataframe.py
@@ -1267,9 +1267,26 @@ class DataFrame(object):
         return self._create_dataframe_from_compiler(new_query_compiler, inplace)
 
     def drop_duplicates(self, subset=None, keep="first", inplace=False):
-        return self._default_to_pandas(
-            pandas.DataFrame.drop_duplicates, subset=subset, keep=keep, inplace=inplace
-        )
+        """Return DataFrame with duplicate rows removed, optionally only considering certain columns
+
+            Args:
+                subset : column label or sequence of labels, optional
+                    Only consider certain columns for identifying duplicates, by
+                    default use all of the columns
+                keep : {'first', 'last', False}, default 'first'
+                    - ``first`` : Drop duplicates except for the first occurrence.
+                    - ``last`` : Drop duplicates except for the last occurrence.
+                    - False : Drop all duplicates.
+                inplace : boolean, default False
+                    Whether to drop duplicates in place or to return a copy
+
+            Returns:
+                deduplicated : DataFrame
+        """
+        inplace = validate_bool_kwarg(inplace, "inplace")
+        duplicates = self.duplicated(subset=subset, keep=keep)
+        indices, = duplicates.nonzero()
+        return self.drop(indices, inplace=inplace)
 
     def duplicated(self, subset=None, keep="first"):
         return self._default_to_pandas(

modin/pandas/test/test_dataframe.py
@@ -1385,14 +1385,42 @@ def test_drop_api_equivalence():
         modin_df.drop(axis=1)
 
 
-@pytest.mark.skip(reason="Defaulting to Pandas")
 @pytest.mark.parametrize("data", test_data_values, ids=test_data_keys)
 def test_drop_duplicates(data):
     modin_df = pd.DataFrame(data)
-    pandas_df = pandas.DataFrame(data)  # noqa F841
+    pandas_df = pandas.DataFrame(data)
 
-    with pytest.raises(NotImplementedError):
-        modin_df.drop_duplicates()
+    df_equals(
+        modin_df.drop_duplicates(keep="first", inplace=False),
+        pandas_df.drop_duplicates(keep="first", inplace=False),
+    )
+
+    df_equals(
+        modin_df.drop_duplicates(keep="last", inplace=False),
+        pandas_df.drop_duplicates(keep="last", inplace=False),
+    )
+
+    df_equals(
+        modin_df.drop_duplicates(keep=False, inplace=False),
+        pandas_df.drop_duplicates(keep=False, inplace=False),
+    )
+
+    df_equals(
+        modin_df.drop_duplicates(inplace=False),
+        pandas_df.drop_duplicates(inplace=False),
+    )
+
+    modin_df.drop_duplicates(inplace=True)
+    df_equals(modin_df, pandas_df.drop_duplicates(inplace=False))
+
+    frame_data = {
+        "A": list(range(3)) * 2,
+        "B": list(range(1, 4)) * 2,
+        "C": list(range(6)),
+    }
+    modin_df = pd.DataFrame(frame_data)
+    modin_df.drop_duplicates(subset=["A", "B"], keep=False, inplace=True)
+    df_equals(modin_df, pandas.DataFrame({"A": [], "B": [], "C": []}))
 
 
 @pytest.mark.parametrize("data", test_data_values, ids=test_data_keys)

commit 10df13d52a174df9f7ed24ec1a736f6d8b539ef1

Correct support for `drop_duplicates`
* Resolves #464
* No longer rely directly on index values
* Corrects issue introduced in #433
* Add tests

modin/pandas/dataframe.py
@@ -1278,7 +1278,7 @@ class DataFrame(object):
         inplace = validate_bool_kwarg(inplace, "inplace")
         duplicates = self.duplicated(subset=subset, keep=keep)
         indices, = duplicates.nonzero()
-        return self.drop(indices, inplace=inplace)
+        return self.drop(index=self.index[indices], inplace=inplace)
 
     def duplicated(self, subset=None, keep="first"):
         return self._default_to_pandas(

modin/pandas/test/test_dataframe.py
@@ -1486,6 +1486,84 @@ def test_drop_duplicates(data):
     df_equals(modin_df, pandas.DataFrame({"A": [], "B": [], "C": []}))
 
 
+def test_drop_duplicates_with_missing_index_values():
+    data = {
+        "columns": ["value", "time", "id"],
+        "index": [
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            12,
+            13,
+            14,
+            15,
+            20,
+            21,
+            22,
+            23,
+            24,
+            25,
+            26,
+            27,
+            32,
+            33,
+            34,
+            35,
+            36,
+            37,
+            38,
+            39,
+            40,
+            41,
+        ],
+        "data": [
+            ["3", 1279213398000.0, 88.0],
+            ["3", 1279204682000.0, 88.0],
+            ["0", 1245772835000.0, 448.0],
+            ["0", 1270564258000.0, 32.0],
+            ["0", 1267106669000.0, 118.0],
+            ["7", 1300621123000.0, 5.0],
+            ["0", 1251130752000.0, 957.0],
+            ["0", 1311683506000.0, 62.0],
+            ["9", 1283692698000.0, 89.0],
+            ["9", 1270234253000.0, 64.0],
+            ["0", 1285088818000.0, 50.0],
+            ["0", 1218212725000.0, 695.0],
+            ["2", 1383933968000.0, 348.0],
+            ["0", 1368227625000.0, 257.0],
+            ["1", 1454514093000.0, 446.0],
+            ["1", 1428497427000.0, 134.0],
+            ["1", 1459184936000.0, 568.0],
+            ["1", 1502293302000.0, 599.0],
+            ["1", 1491833358000.0, 829.0],
+            ["1", 1485431534000.0, 806.0],
+            ["8", 1351800505000.0, 101.0],
+            ["0", 1357247721000.0, 916.0],
+            ["0", 1335804423000.0, 370.0],
+            ["24", 1327547726000.0, 720.0],
+            ["0", 1332334140000.0, 415.0],
+            ["0", 1309543100000.0, 30.0],
+            ["18", 1309541141000.0, 30.0],
+            ["0", 1298979435000.0, 48.0],
+            ["14", 1276098160000.0, 59.0],
+            ["0", 1233936302000.0, 109.0],
+        ],
+    }
+
+    pandas_df = pandas.DataFrame(
+        data["data"], index=data["index"], columns=data["columns"]
+    )
+    modin_df = pd.DataFrame(data["data"], index=data["index"], columns=data["columns"])
+    modin_result = modin_df.sort_values(["id", "time"]).drop_duplicates(["id"])
+    pandas_result = pandas_df.sort_values(["id", "time"]).drop_duplicates(["id"])
+    df_equals(modin_result, pandas_result)
+
+
 @pytest.mark.parametrize("data", test_data_values, ids=test_data_keys)
 @pytest.mark.parametrize("axis", axis_values, ids=axis_keys)
 @pytest.mark.parametrize("how", ["any", "all"], ids=["any", "all"])