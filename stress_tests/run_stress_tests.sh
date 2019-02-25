a7ce568ff72b466d5402067c9f3f1814c961ef8d
William Ma <12377941+williamma12@users.noreply.github.com>
2019-02-25 12:37:28 -0800

    Create virtual env for stress tests

--- stress_tests/run_stress_tests.sh
+++ stress_tests/run_stress_tests.sh
@@ -8,6 +8,29 @@ RESULT_FILE=$ROOT_DIR/results-$(date '+%Y-%m-%d_%H-%M-%S').log
 echo "Logging to" $RESULT_FILE
 touch $RESULT_FILE
 
+setup_environment(){
+    pushd "$ROOT_DIR"
+    # Create a virtual environment for the stress tests
+    python -m virtualenv stress_tests_env
+    source stress_tests_env/bin/activate
+
+    # Install modin from source to virtual environment
+    pushd "../$ROOT_DIR"
+    pip install -e .
+    popd
+
+    # Install basic data science packages
+    pip install matplotlib numpy seaborn scipy
+
+    # Install machine learning packages
+    pip install scikit-learn xgboost lightgbm keras
+
+    # Install packages for kaggle18
+    pip install nltk wordcloud plotly bokeh pyLDAvis
+
+    popd
+}
+
 run_test(){
     local test_name=$1
 
@@ -18,6 +41,7 @@ run_test(){
 }
 
 pushd "$ROOT_DIR"
+    setup_environment
     run_test test_kaggle_ipynb
 popd
 
