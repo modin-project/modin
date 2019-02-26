faf72930b05e4302c9448c8b1cca2af11c6a77cb
William Ma <12377941+williamma12@users.noreply.github.com>
2019-02-25 18:09:48 -0800

    Use custom ray

--- stress_tests/run_stress_tests.sh
+++ stress_tests/run_stress_tests.sh
@@ -3,6 +3,11 @@
 # Show explicitly which commands are currently running.
 set -x
 
+# TODO (williamma12): Once we use clusters, make sure to download latest wheels
+# from s3 bucket instead of building ray
+# Ray directory
+RAY_DIR=${1}
+
 ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
 RESULT_FILE=$ROOT_DIR/results-$(date '+%Y-%m-%d_%H-%M-%S').log
 echo "Logging to" $RESULT_FILE
@@ -11,29 +16,36 @@ touch $RESULT_FILE
 setup_environment(){
     pushd "$ROOT_DIR"
     # Create a virtual environment for the stress tests
-    python -m virtualenv stress_tests_env
-    source stress_tests_env/bin/activate
+    python -m virtualenv stress_tests_env >> $RESULT_FILE
+    source stress_tests_env/bin/activate >> $RESULT_FILE
+
+    # Install ray from source if available
+    if [[ ! -z "$RAY_DIR" ]]; then
+        pushd "$RAY_DIR"
+        pip install -e . --verbose >> $RESULT_FILE
+        popd
+    fi
 
     # Install modin from source to virtual environment
     pushd "$ROOT_DIR/.."
-    pip install -e .
+    pip install -e . >> $RESULT_FILE
     popd
 
     # Install basic data science packages
-    pip install matplotlib numpy seaborn scipy
+    pip install matplotlib numpy seaborn scipy >> $RESULT_FILE
 
     # Install machine learning packages
-    pip install scikit-learn xgboost lightgbm keras
+    pip install scikit-learn xgboost lightgbm keras >> $RESULT_FILE
 
     # Install packages for kaggle18
-    pip install nltk wordcloud plotly bokeh pyLDAvis
+    pip install nltk wordcloud plotly bokeh pyLDAvis >> $RESULT_FILE
 
     popd
 }
 
 teardown_environment(){
     pushd "$ROOT_DIR"
-    rm -rf stress_tests_env
+    rm -rf stress_tests_env >> $RESULT_FILE
     popd
 }
 
