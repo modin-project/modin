0882c366e29e851baa561b620322a695279c1cef
eavidan <eran.avidan@intel.com>
2019-03-13 13:33:12 +0200

    read from sql added using limit and offset

--- modin/engines/ray/pandas_on_ray/io.py
+++ modin/engines/ray/pandas_on_ray/io.py
@@ -589,6 +589,49 @@ class PandasOnRayIO(BaseIO):
         # blocking operation
         result.to_pandas()
 
+    @classmethod
+    def read_sql(cls, sql, con, index_col=None, **kwargs):
+        """Reads a SQL query or database table into a DataFrame.
+        Args:
+            sql: string or SQLAlchemy Selectable (select or text object) SQL query to be executed or a table name.
+            con: SQLAlchemy connectable (engine/connection) or database string URI or DBAPI2 connection (fallback mode)
+            index_col: Column(s) to set as index(MultiIndex).
+            kwargs: Pass into pandas.read_sql function.
+        """
+        row_cnt_query = "SELECT COUNT(*) FROM ({})".format(sql)
+        row_cnt = pandas.read_sql(row_cnt_query, con).iloc[0][0]
+        cols_names_df = pandas.read_sql("SELECT * FROM ({}) LIMIT 0".format(sql), con)
+        if index_col is not None:
+            cols_names_df = cols_names_df.drop(columns=index_col)
+        cols_names = cols_names_df.columns
+        num_parts = cls.block_partitions_cls._compute_num_partitions()
+        partition_ids = []
+        index_ids = []
+        limit = int(round(row_cnt / num_parts))
+        for part in range(num_parts):
+            offset = part * limit
+            query = "SELECT * FROM ({}) LIMIT {} OFFSET {}".format(sql, limit, offset)
+            partition_id = _read_sql_with_limit_offset._remote(
+                args=(num_parts, query, con, index_col, kwargs),
+                num_return_vals=num_parts + 1,
+            )
+            partition_ids.append(
+                [PandasOnRayRemotePartition(obj) for obj in partition_id[:-1]]
+            )
+            index_ids.append(partition_id[-1])
+
+        if index_col is None:  # sum all lens returned from partitions
+            index_lens = ray.get(index_ids)
+            new_index = pandas.RangeIndex(sum(index_lens))
+        else:  # concat index returned from partitions
+            index_lst = [x for part_index in ray.get(index_ids) for x in part_index]
+            new_index = pandas.Index(index_lst).set_names(index_col)
+
+        new_query_compiler = cls.query_compiler_cls(
+            cls.block_partitions_cls(np.array(partition_ids)), new_index, cols_names
+        )
+        return new_query_compiler
+
 
 @ray.remote
 def get_index(index_name, *partition_indices):  # pragma: no cover
@@ -726,3 +769,19 @@ def _read_feather_columns(path, columns, num_splits):  # pragma: no cover
     df = feather.read_feather(path, columns=columns)
     # Append the length of the index here to build it externally
     return _split_result_for_readers(0, num_splits, df) + [len(df.index)]
+
+
+@ray.remote
+def _read_sql_with_limit_offset(
+    num_splits, sql, con, index_col, kwargs
+):  # pragma: no cover
+    """Use a Ray task to read a chunk of SQL source.
+
+    Note: Ray functions are not detected by codecov (thus pragma: no cover)
+    """
+    pandas_df = pandas.read_sql(sql, con, index_col=index_col, **kwargs)
+    if index_col is None:
+        index = len(pandas_df)
+    else:
+        index = pandas_df.index
+    return _split_result_for_readers(1, num_splits, pandas_df) + [index]
