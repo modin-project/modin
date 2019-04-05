98c8ea1e3ef46679c06dc71d2071442946d9d0a4
William Ma <12377941+williamma12@users.noreply.github.com>
2019-04-05 15:17:36 -0700

    clean calculation of new columns

--- modin/backends/pandas/query_compiler.py
+++ modin/backends/pandas/query_compiler.py
@@ -1693,38 +1693,8 @@ class PandasQueryCompiler(BaseQueryCompiler):
         Returns:
             DataFrame object containing the descriptive statistics of the DataFrame.
         """
-        # Only describe numeric if there are numeric columns
-        # Otherwise, describe all
-        new_columns = self.numeric_columns(include_bool=False)
-        include = kwargs.get("include", None)
-        if len(new_columns) != 0 and include is not None:
-            if not isinstance(include, np.dtype) and include == "all":
-                new_columns = self.columns
-            else:
-                new_columns = self.dtypes[
-                    [
-                        any(
-                            (isinstance(inc, np.dtype) and inc == d)
-                            or (
-                                not isinstance(inc, np.dtype)
-                                and inc.__subclasscheck__(getattr(np, d.__str__()))
-                            )
-                            for inc in include
-                        )
-                        for d in self.dtypes.values
-                    ]
-                ].index
-        elif len(new_columns) == 0:
-            new_columns = [
-                self.columns[i]
-                for i in range(len(self.columns))
-                if self.dtypes[i] != np.dtype("datetime64[ns]")
-            ]
-        else:
-            exclude = kwargs.get("exclude", None)
-            if exclude is not None:
-                # Update `new_columns` to reflect the included types
-                new_columns = self.dtypes[~self.dtypes.isin(exclude)].index
+        # Use pandas to calculate the correct columns
+        new_columns = pandas.DataFrame(columns=self.columns).astype(self.dtypes).describe(**kwargs).columns
 
         def describe_builder(df, internal_indices=[], **kwargs):
             return df.iloc[:, internal_indices].describe(**kwargs)
