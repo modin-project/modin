7f9d6f782f157cf6398c1171ad45524cd8a9a5ce
Devin Petersohn <devin-petersohn@users.noreply.github.com>
2019-04-18 13:09:10 -0700

    Add fastrack for empty mask computation (#565)
    * Resolves #517
    * No longer trigger compute with an empty mask
    * Add tests

--- modin/pandas/test/test_dataframe.py
+++ modin/pandas/test/test_dataframe.py
@@ -4670,6 +4670,35 @@ class TestDFPartTwo:
             pd_col = pandas_df[key]
             df_equals(pd_col, modin_col)
 
+    def test_getitem_empty_mask(self):
+        # modin-project/modin#517
+        modin_frames = []
+        pandas_frames = []
+        data1 = np.random.randint(0, 100, size=(100, 4))
+        mdf1 = pd.DataFrame(data1, columns=list("ABCD"))
+        pdf1 = pandas.DataFrame(data1, columns=list("ABCD"))
+        modin_frames.append(mdf1)
+        pandas_frames.append(pdf1)
+
+        data2 = np.random.randint(0, 100, size=(100, 4))
+        mdf2 = pd.DataFrame(data2, columns=list("ABCD"))
+        pdf2 = pandas.DataFrame(data2, columns=list("ABCD"))
+        modin_frames.append(mdf2)
+        pandas_frames.append(pdf2)
+
+        data3 = np.random.randint(0, 100, size=(100, 4))
+        mdf3 = pd.DataFrame(data3, columns=list("ABCD"))
+        pdf3 = pandas.DataFrame(data3, columns=list("ABCD"))
+        modin_frames.append(mdf3)
+        pandas_frames.append(pdf3)
+
+        modin_data = pd.concat(modin_frames)
+        pandas_data = pandas.concat(pandas_frames)
+        df_equals(
+            modin_data[[False for _ in modin_data.index]],
+            pandas_data[[False for _ in modin_data.index]],
+        )
+
     @pytest.mark.parametrize("data", test_data_values, ids=test_data_keys)
     def test___getattr__(self, request, data):
         modin_df = pd.DataFrame(data)
