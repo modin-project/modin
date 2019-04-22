ba5c0e97300c1005bb597487e48a645a2d3c2510
Alex Wu <itswu.alex@gmail.com>
2019-04-22 02:22:28 -0700

    Being less stupid now

--- modin/pandas/test/test_dataframe.py
+++ modin/pandas/test/test_dataframe.py
@@ -4691,7 +4691,6 @@ class TestDFPartTwo:
 
     @pytest.mark.parametrize("data", test_data_values, ids=test_data_keys)
     def test___setitem__(self, data):
-        data = {"A": list(range(5)), "B": [str(x) for x in range(5)]}
         modin_df = pd.DataFrame(data)
         pandas_df = pandas.DataFrame(data)
 
