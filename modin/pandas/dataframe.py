57f3c8dd82a22a99da3bfe5de1abc2785722de3d
Devin Petersohn <devin.petersohn@gmail.com>
2019-05-01 12:37:36 -0700

    Fix bug

--- modin/pandas/dataframe.py
+++ modin/pandas/dataframe.py
@@ -67,7 +67,7 @@ class DataFrame(BasePandasDataset):
                 data = [
                     obj._to_pandas() if isinstance(obj, Series) else obj for obj in data
                 ]
-            elif is_dict_like(data):
+            elif is_dict_like(data) and not isinstance(data, (pandas.Series, Series)):
                 data = {
                     k: v._to_pandas() if isinstance(v, Series) else v
                     for k, v in data.items()
