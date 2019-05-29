a6fc667be264b23d57f4a91960d48cb29ca23b94
Devin Petersohn <devin.petersohn@gmail.com>
2019-05-29 11:58:48 -0700

    Fix read_parquet issues

--- modin/engines/ray/pandas_on_ray/io.py
+++ modin/engines/ray/pandas_on_ray/io.py
@@ -50,8 +50,8 @@ def _read_parquet_columns(path, columns, num_splits, kwargs):  # pragma: no cove
             default Index.
     """
     import pyarrow.parquet as pq
-
-    df = pq.ParquetDataset(path, **kwargs).read(columns=columns).to_pandas()
+    kwargs["use_pandas_metadata"] = True
+    df = pq.read_table(path, columns=columns, **kwargs).to_pandas()
 
     # Append the length of the index here to build it externally
     return _split_result_for_readers(0, num_splits, df) + [len(df.index)]
