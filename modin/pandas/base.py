9ab8a02f13d49d21ac211033c99346f4f4ba6963
Devin Petersohn <devin-petersohn@users.noreply.github.com>
2019-06-25 09:33:13 -0700

    Fix sorting for MultiIndex (#705)
    * Resolves #698
    * Correctly creates a new DataFrame based on the QueryCompiler instead of the DataFrame object itself
    * Adds conditional checking for MultiIndex when `axis=0` and `axis=1`.
    * Add tests

--- modin/pandas/base.py
+++ modin/pandas/base.py
@@ -2546,7 +2546,10 @@ class BasePandasDataset(object):
             A sorted DataFrame
         """
         axis = self._get_axis_number(axis)
-        if level is not None:
+        if level is not None or (
+            (axis == 0 and isinstance(self.index, pandas.MultiIndex))
+            or (axis == 1 and isinstance(self.columns, pandas.MultiIndex))
+        ):
             new_query_compiler = self._default_to_pandas(
                 "sort_index",
                 axis=axis,
@@ -2556,7 +2559,7 @@ class BasePandasDataset(object):
                 kind=kind,
                 na_position=na_position,
                 sort_remaining=sort_remaining,
-            )
+            )._query_compiler
             return self._create_or_update_from_compiler(new_query_compiler, inplace)
         if by is not None:
             warnings.warn(
