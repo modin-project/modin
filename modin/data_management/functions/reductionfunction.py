6f895dd29f9dd555a33fcbbf0fc99920bd09f039
Devin Petersohn <devin.petersohn@gmail.com>
2019-10-03 13:25:48 -0700

    Add new files

---
+++ modin/data_management/functions/reductionfunction.py
@@ -0,0 +1,21 @@
+from .function import Function
+
+
+class ReductionFunction(Function):
+    @classmethod
+    def call(cls, reduction_function, **call_kwds):
+        def caller(query_compiler, *args, **kwargs):
+            return query_compiler.__constructor__(
+                query_compiler._modin_frame._fold_reduce(
+                    call_kwds.get("axis")
+                    if "axis" in call_kwds
+                    else kwargs.get("axis"),
+                    lambda x: reduction_function(x, *args, **kwargs),
+                )
+            )
+
+        return caller
+
+    @classmethod
+    def register(cls, function, *args, **kwargs):
+        return cls.call(function, *args, **kwargs)
