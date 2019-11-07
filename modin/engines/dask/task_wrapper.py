a5a6daf973a9f7e9a2cdb9bced9dbda7acdda802
Devin Petersohn <devin.petersohn@gmail.com>
2019-11-06 20:31:30 -0800

    Add task wrapper file

---
+++ modin/engines/dask/task_wrapper.py
@@ -0,0 +1,20 @@
+from modin import __execution_engine__
+
+if __execution_engine__ == "Dask":
+    from distributed.client import _get_global_client
+
+
+class DaskTask:
+    @classmethod
+    def deploy(cls, func, num_return_vals, kwargs):
+        client = _get_global_client()
+        remote_task_future = client.submit(func, **kwargs)
+        return [
+            client.submit(lambda l: l[i], remote_task_future)
+            for i in range(num_return_vals)
+        ]
+
+    @classmethod
+    def materialize(cls, future):
+        client = _get_global_client()
+        return client.gather(future)
