0f314a9a138f097a4b955a1250656ebbcf400f5e
Devin Petersohn <devin.petersohn@gmail.com>
2019-12-30 18:37:12 -0800

    Remove unused tests

--- modin/pandas/test/performance-tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import os
-
-
-def pytest_benchmark_update_commit_info(config, commit_info):
-    commit_info["commit_number"] = int(
-        subprocess.check_output(["git", "rev-list", "HEAD", "--count"]).strip()
-    )
-    commit_info["engine"] = os.environ.get("MODIN_ENGINE", "Ray").title()
