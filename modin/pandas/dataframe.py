7874281c068bf4584ec2305d6ab5c2f9a260e5f4
Devin Petersohn <devin-petersohn@users.noreply.github.com>
2020-01-12 14:55:57 -0800

    Support console with __repr__ like pandas (#984)
    * Support console with __repr__ like pandas
    
    * Resolves #898
    * Pre-compute the number of columns based on the width of the column
      names
      * This guarantees that we will get at least as many columns from the
        partitions as we need.
      * Compute width using pandas utility to match the behavior
    * Tested locally to verify behavior

--- modin/pandas/dataframe.py
+++ modin/pandas/dataframe.py
@@ -88,9 +88,28 @@ class DataFrame(BasePandasDataset):
             self._query_compiler = query_compiler
 
     def __repr__(self):
-        num_rows = pandas.get_option("max_rows") or 10
-        num_cols = pandas.get_option("max_columns") or 20
+        from pandas.io.formats import console
 
+        num_rows = pandas.get_option("display.max_rows") or 10
+        num_cols = pandas.get_option("display.max_columns") or 20
+        if pandas.get_option("display.max_columns") is None and pandas.get_option(
+            "display.expand_frame_repr"
+        ):
+            width, _ = console.get_console_size()
+            col_counter = 0
+            i = 0
+            while col_counter < width:
+                col_counter += len(str(self.columns[i])) + 1
+                i += 1
+
+            num_cols = i
+            i = len(self.columns) - 1
+            col_counter = 0
+            while col_counter < width:
+                col_counter += len(str(self.columns[i])) + 1
+                i -= 1
+
+            num_cols += len(self.columns) - i
         result = repr(self._build_repr_df(num_rows, num_cols))
         if len(self.index) > num_rows or len(self.columns) > num_cols:
             # The split here is so that we don't repr pandas row lengths.
