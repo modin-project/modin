275a0cd3353bc42cb6872ca3c62fdf6b494a4bc3
Devin Petersohn <devin.petersohn@gmail.com>
2020-01-14 19:02:33 -0800

    Add implementation for `between_time` for `Series` and `DataFrame`
    * Resolves #990
    * Add implementation for `DataFrame` and `Series`
    * Improve tests for `between_time`

--- modin/pandas/test/test_dataframe.py
+++ modin/pandas/test/test_dataframe.py
@@ -2178,10 +2178,25 @@ class TestDataFrameDefault:
         )
 
     def test_between_time(self):
-        i = pd.date_range("2018-04-09", periods=4, freq="12H")
-        ts = pd.DataFrame({"A": [1, 2, 3, 4]}, index=i)
-        with pytest.warns(UserWarning):
-            ts.between_time("0:15", "0:45")
+        i = pd.date_range("2008-01-01", periods=1000, freq="12H")
+        modin_df = pd.DataFrame(
+            {"A": list(range(1000)), "B": list(range(1000))}, index=i
+        )
+        pandas_df = pandas.DataFrame(
+            {"A": list(range(1000)), "B": list(range(1000))}, index=i
+        )
+        df_equals(
+            modin_df.between_time("12:00", "17:00"),
+            pandas_df.between_time("12:00", "17:00"),
+        )
+        df_equals(
+            modin_df.between_time("3:00", "4:00"),
+            pandas_df.between_time("3:00", "4:00"),
+        )
+        df_equals(
+            modin_df.T.between_time("12:00", "17:00", axis=1),
+            pandas_df.T.between_time("12:00", "17:00", axis=1),
+        )
 
     def test_bfill(self):
         test_data = TestData()
