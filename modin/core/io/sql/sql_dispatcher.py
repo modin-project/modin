351ba87be3f46c82805ed93a4014a666171d1606
eavidan <eran.avidan@intel.com>
2020-01-29 18:22:46 +0200

    sql query generator added in a utils file with support for both base case and ms sql sql reader updated to use it

--- modin/core/io/sql/sql_dispatcher.py
+++ modin/core/io/sql/sql_dispatcher.py
@@ -1,88 +1,70 @@
-# Licensed to Modin Development Team under one or more contributor license agreements.
-# See the NOTICE file distributed with this work for additional information regarding
-# copyright ownership.  The Modin Development Team licenses this file to you under the
-# Apache License, Version 2.0 (the "License"); you may not use this file except in
-# compliance with the License.  You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software distributed under
-# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
-# ANY KIND, either express or implied. See the License for the specific language
-# governing permissions and limitations under the License.
-
-"""
-Module houses `SQLDispatcher` class.
-
-`SQLDispatcher` contains utils for handling SQL queries or database tables,
-inherits util functions for handling files from `FileDispatcher` class and can be
-used as base class for dipatchers of SQL queries.
-"""
-
 import math
 import numpy as np
 import pandas
 import warnings
+from .utils import SqlQuery
 
-from modin.core.io.file_dispatcher import FileDispatcher
-from modin.db_conn import ModinDatabaseConnection
-from modin.config import NPartitions
+from modin.engines.base.io.file_reader import FileReader
 
 
-class SQLDispatcher(FileDispatcher):
-    """Class handles utils for reading SQL queries or database tables."""
-
+class SQLReader(FileReader):
     @classmethod
-    def _read(cls, sql, con, index_col=None, **kwargs):
-        """
-        Read a SQL query or database table into a query compiler.
+    def read(cls, sql, con, index_col=None, **kwargs):
+        """Reads a SQL query or database table into a DataFrame.
 
-        Parameters
-        ----------
-        sql : str or SQLAlchemy Selectable (select or text object)
-            SQL query to be executed or a table name.
-        con : SQLAlchemy connectable, str, sqlite3 connection, or ModinDatabaseConnection
-            Connection object to database.
-        index_col : str or list of str, optional
-            Column(s) to set as index(MultiIndex).
-        **kwargs : dict
-            Parameters to pass into `pandas.read_sql` function.
-
-        Returns
-        -------
-        BaseQueryCompiler
-            Query compiler with imported data for further processing.
+        Args:
+            sql: string or SQLAlchemy Selectable (select or text object) SQL query to be
+                executed or a table name.
+            con: SQLAlchemy connectable (engine/connection) or database string URI or
+                DBAPI2 connection (fallback mode)
+            index_col: Column(s) to set as index(MultiIndex).
+            kwargs: Pass into pandas.read_sql function.
         """
-        is_modin_db_connection = isinstance(con, ModinDatabaseConnection)
-        if not (is_modin_db_connection or isinstance(con, str)):
-            warnings.warn(
-                "To use parallel implementation of `read_sql`, pass either "
-                "the SQL connection string or a ModinDatabaseConnection "
-                "with the arguments required to make a connection, instead "
-                "of {}. For documentation of ModinDatabaseConnection, see https://modin.readthedocs.io/en/latest/supported_apis/io_supported.html#connecting-to-a-database-for-read-sql".format(
-                    type(con)
+        import sqlalchemy as sa
+
+        try:
+            import psycopg2 as pg
+
+            if isinstance(con, pg.extensions.connection):
+                con = "postgresql+psycopg2://{}:{}@{}{}/{}".format(  # Table in DB
+                    con.info.user,  # <Username>: for DB
+                    con.info.password,  # Password for DB
+                    con.info.host if con.info.host != "/tmp" else "",  # @<Hostname>
+                    (":" + str(con.info.port))
+                    if con.info.host != "/tmp"
+                    else "",  # <port>
+                    con.info.dbname,  # Table in DB
                 )
+        except ImportError:
+            pass
+        # In the case that we are given a SQLAlchemy Connection or Engine, the objects
+        # are not pickleable. We have to convert it to the URL string and connect from
+        # each of the workers.
+        if isinstance(con, (sa.engine.Engine, sa.engine.Connection)):
+            warnings.warn(
+                "To use parallel implementation of `read_sql`, pass the "
+                "connection string instead of {}.".format(type(con))
             )
             return cls.single_worker_read(sql, con=con, index_col=index_col, **kwargs)
-        row_cnt_query = "SELECT COUNT(*) FROM ({}) as foo".format(sql)
-        connection_for_pandas = con.get_connection() if is_modin_db_connection else con
-        row_cnt = pandas.read_sql(row_cnt_query, connection_for_pandas).squeeze()
-        cols_names_df = pandas.read_sql(
-            "SELECT * FROM ({}) as foo LIMIT 0".format(sql),
-            connection_for_pandas,
-            index_col=index_col,
-        )
+
+        sa_engine = sa.create_engine(con)
+        sql_query_engine = SqlQuery(sa_driver=sa_engine.driver)
+        row_cnt_query = sql_query_engine.row_cnt(sql=sql)
+        cols_names_query = sql_query_engine.empty(sql=sql)
+
+        row_cnt = pandas.read_sql(row_cnt_query, con).squeeze()
+        cols_names_df = pandas.read_sql(cols_names_query, con, index_col=index_col)
         cols_names = cols_names_df.columns
-        num_partitions = NPartitions.get()
+        from modin.pandas import DEFAULT_NPARTITIONS
+
+        num_partitions = DEFAULT_NPARTITIONS
         partition_ids = []
         index_ids = []
         dtype_ids = []
         limit = math.ceil(row_cnt / num_partitions)
         for part in range(num_partitions):
             offset = part * limit
-            query = "SELECT * FROM ({}) as foo LIMIT {} OFFSET {}".format(
-                sql, limit, offset
-            )
+            query = sql_query_engine.partitioned(sql=sql, limit=limit, offset=offset)
             partition_id = cls.deploy(
                 cls.parse,
                 num_partitions + 2,
@@ -91,7 +73,7 @@ class SQLDispatcher(FileDispatcher):
                     sql=query,
                     con=con,
                     index_col=index_col,
-                    **kwargs,
+                    **kwargs
                 ),
             )
             partition_ids.append(
@@ -108,5 +90,5 @@ class SQLDispatcher(FileDispatcher):
             ]
             new_index = pandas.Index(index_lst).set_names(index_col)
         new_frame = cls.frame_cls(np.array(partition_ids), new_index, cols_names)
-        new_frame.synchronize_labels(axis=0)
+        new_frame._apply_index_objs(axis=0)
         return cls.query_compiler_cls(new_frame)
