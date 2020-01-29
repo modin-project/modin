e8295cc900856baff0b43b5d115cb5bc6a1cab2c
Devin Petersohn <devin.petersohn@gmail.com>
2020-01-29 10:50:39 -0800

    Update Test infrastructure to use testmon when possible
    * Previously ran into errors with too many changes, this resolves that
    * Still update the testmon files in S3
    * Force run certain tests no matter what

--- run-tests.sh
+++ run-tests.sh
@@ -12,18 +12,33 @@ if [ "$arg" == "ray" ] || [ "$arg" == "all" ]; then
     aws s3 cp  --no-sign-request s3://modin-testing/testmondata_ray .
     mv testmondata_ray .testmondata
     MODIN_ENGINE=ray pytest modin/pandas/test/ $@
+    # This happens on sqlite error from testmon if too many files were changed.
+    if [ "$?" -eq 3 ]; then
+      rm .testmondata
+      MODIN_ENGINE=ray pytest modin/pandas/test/ $@
+    fi
 fi
 if [ "$arg" == "python" ] || [ "$arg" == "all" ]; then
     echo "Running Python tests"
     aws s3 cp  --no-sign-request s3://modin-testing/testmondata_python .
     mv testmondata_python .testmondata
     MODIN_ENGINE=python pytest modin/pandas/test/ $@
+    # This happens on sqlite error from testmon if too many files were changed.
+    if [ "$?" -eq 3 ]; then
+      rm .testmondata
+      MODIN_ENGINE=python pytest modin/pandas/test/ $@
+    fi
 fi
 if [ "$arg" == "dask" ] || [ "$arg" == "all" ]; then
     echo "Running Dask tests"
     aws s3 cp  --no-sign-request s3://modin-testing/testmondata_dask .
     mv testmondata_dask .testmondata
     MODIN_ENGINE=dask pytest modin/pandas/test/ $@
+    # This happens on sqlite error from testmon if too many files were changed.
+    if [ "$?" -eq 3 ]; then
+      rm .testmondata
+      MODIN_ENGINE=dask pytest modin/pandas/test/ $@
+    fi
 fi
 if [ "$arg" == "pyarrow" ] || [ "$arg" == "all" ]; then
     echo "Running Pyarrow tests"
