e4a31d1699b651b663f504b457232d10705efc06
Elon Portugaly <elonp@microsoft.com>
2020-02-13 23:24:30 +0000

    bugfix - sometimes df.loc[s] applies to columns instead of rows

--- modin/pandas/indexing.py
+++ modin/pandas/indexing.py
@@ -200,36 +200,20 @@ class _LocIndexer(_LocationIndexerBase):
     """A indexer for ray_df.loc[] functionality"""
 
     def __getitem__(self, key):
-        # When getting along a single axis,
-        if not isinstance(key, tuple):
-            # Try to fasttrack the code through already optimized path
-            try:
-                return self.df.__getitem__(key)
-            # This can happen if it is a list of rows
-            except KeyError:
-                pass
-        else:
-            if len(key) > self.df.ndim:
-                raise IndexingError("Too many indexers")
-            # If we're only slicing columns, handle the case with `__getitem__`
-            if isinstance(key[0], slice) and key[0] == slice(None):
-                if not isinstance(key[1], slice):
-                    # Boolean indexers can just be sliced into the columns object and
-                    # then passed to `__getitem__`
-                    if is_boolean_array(key[1]):
-                        return self.df.__getitem__(self.df.columns[key[1]])
-                    return self.df.__getitem__(key[1])
-                else:
-                    result_slice = self.df.columns.slice_locs(key[1].start, key[1].stop)
-                    return self.df.iloc[:, slice(*result_slice)]
         row_loc, col_loc, ndim, self.row_scaler, self.col_scaler = _parse_tuple(key)
+        if isinstance(row_loc, slice) and row_loc == slice(None):
+            # If we're only slicing columns, handle the case with `__getitem__`
+            if not isinstance(col_loc, slice):
+                # Boolean indexers can just be sliced into the columns object and
+                # then passed to `__getitem__`
+                if is_boolean_array(col_loc):
+                    col_loc = self.df.columns[col_loc]
+                return self.df.__getitem__(col_loc)
+            else:
+                result_slice = self.df.columns.slice_locs(col_loc.start, col_loc.stop)
+                return self.df.iloc[:, slice(*result_slice)]
+
         row_lookup, col_lookup = self._compute_lookup(row_loc, col_loc)
-        # Check that the row_lookup/col_lookup is longer than 1 or that the
-        # row_loc/col_loc is not boolean list to determine the ndim of the
-        # result properly for multiindex.
-        ndim = (0 if len(row_lookup) == 1 and not is_boolean_array(row_loc) else 1) + (
-            0 if len(col_lookup) == 1 and not is_boolean_array(col_loc) else 1
-        )
         result = super(_LocIndexer, self).__getitem__(row_lookup, col_lookup, ndim)
         # Pandas drops the levels that are in the `loc`, so we have to as well.
         if hasattr(result, "index") and isinstance(result.index, pandas.MultiIndex):
