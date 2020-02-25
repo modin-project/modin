1bd2ae6eb6166f641ef49bc4918655bea78dfd0e
Devin Petersohn <devin.petersohn@gmail.com>
2020-02-25 12:13:43 -0800

    Make query compiler interface consistent for binary ops
    * Resolves #1064
    * Add a new function type for internal query compilers
      * `BinaryFunction`
    * Enumerate the binary function calls into the query compiler
    * Code quality updates and organization
    
    Signed-off-by: Devin Petersohn <devin.petersohn@gmail.com>

--- modin/data_management/functions/__init__.py
+++ modin/data_management/functions/__init__.py
@@ -2,5 +2,12 @@ from .mapfunction import MapFunction
 from .mapreducefunction import MapReduceFunction
 from .reductionfunction import ReductionFunction
 from .foldfunction import FoldFunction
+from .binary_function import BinaryFunction
 
-__all__ = ["MapFunction", "MapReduceFunction", "ReductionFunction", "FoldFunction"]
+__all__ = [
+    "MapFunction",
+    "MapReduceFunction",
+    "ReductionFunction",
+    "FoldFunction",
+    "BinaryFunction",
+]
