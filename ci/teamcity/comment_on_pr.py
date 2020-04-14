3d875e6358bee365ebea4186ce89412f7c654841
Devin Petersohn <devin.petersohn@gmail.com>
2020-04-14 15:17:33 -0700

    Update log reporter to comment based on separate builds.
    Signed-off-by: Devin Petersohn <devin.petersohn@gmail.com>

--- ci/teamcity/comment_on_pr.py
+++ ci/teamcity/comment_on_pr.py
@@ -16,20 +16,25 @@ try:
     pr_id = int(os.environ["GITHUB_PR_NUMBER"].split("/")[-1])
 except Exception:
     sys.exit(0)
-header = """
-<h2 align="center">:robot: TeamCity test results bot :robot:</h2>
 
-"""
+engine = os.environ["MODIN_ENGINE"]
 
-pytest_outputs = ["ray_tests.log", "dask_tests.log", "python_tests.log"]
+header = """<h1 align="center"><img width=7% alt="" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Teamcity_Logo.png/600px-Teamcity_Logo.png">
+    TeamCity {} test results bot</h1>""".format(
+    engine.title()
+)
+if engine == "ray":
+    pytest_outputs = ["ray_tests.log"]
+elif engine == "dask":
+    pytest_outputs = ["dask_tests.log"]
+elif engine == "python":
+    pytest_outputs = ["python_tests.log"]
+else:
+    raise Exception("Unknown Engine, set `MODIN_ENGINE` environment variable")
 
 full_comment = header
 
 for out in pytest_outputs:
-
-    full_comment += "<details><summary>{} Tests</summary>\n".format(
-        out.split("_")[0].title()
-    )
     full_comment += "\n\n```\n"
     full_comment += open(out, "r").read()
     full_comment += "\n```\n\n</details>\n"
@@ -39,9 +44,16 @@ g = Github(token)
 repo = g.get_repo("modin-project/modin")
 
 pr = repo.get_pull(pr_id)
-if any(i.user.login == "modin-bot" for i in pr.get_issue_comments()):
+if any(
+    i.user.login == "modin-bot"
+    and "TeamCity {} test results bot".format(engine.title()) in i.body.lower()
+    for i in pr.get_issue_comments()
+):
     pr_comment_list = [
-        i for i in list(pr.get_issue_comments()) if i.user.login == "modin-bot"
+        i
+        for i in list(pr.get_issue_comments())
+        if i.user.login == "modin-bot"
+        and "TeamCity {} test results bot".format(engine.title()) in i.body.lower()
     ]
     assert len(pr_comment_list) == 1, "Too many comments from modin-bot already"
     pr_comment_list[0].edit(full_comment)
