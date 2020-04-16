3456e29af2665fbd08f5d920f965d36eca288ffe
Devin Petersohn <devin.petersohn@gmail.com>
2020-04-16 10:50:54 -0700

    Truncate long comments > 65000
    Signed-off-by: Devin Petersohn <devin.petersohn@gmail.com>

--- ci/teamcity/comment_on_pr.py
+++ ci/teamcity/comment_on_pr.py
@@ -20,7 +20,7 @@ except Exception:
 engine = os.environ["MODIN_ENGINE"]
 
 header = """<h1 align="center"><img width=7% alt="" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Teamcity_Logo.png/600px-Teamcity_Logo.png">
-    TeamCity {} test results bot</h1>""".format(
+    TeamCity {} test results bot</h1>\n\n""".format(
     engine.title()
 )
 if engine == "ray":
@@ -32,12 +32,35 @@ elif engine == "python":
 else:
     raise Exception("Unknown Engine, set `MODIN_ENGINE` environment variable")
 
-full_comment = header
+full_comment = ""
+# Do not include coverage info in PR comment
+split_by_first = (
+    "----------- coverage: platform linux, python 3.7.5-final-0 -----------"
+)
+split_by_second = "--------------------------------------------------------------------------------------"
 
 for out in pytest_outputs:
+    full_comment += "<details><summary>Tests Logs</summary>\n"
     full_comment += "\n\n```\n"
-    full_comment += open(out, "r").read()
+    content = open(out, "r").read()
+    full_comment += "".join(
+        "".join(
+            [
+                i.split(split_by_first)[0],
+                i.split(split_by_first)[-1].split(split_by_second)[-1],
+            ]
+        )
+        for i in content.split("+ python3 -m pytest -n=48 ")
+    )
+    if len(full_comment) > 65_000:
+        full_comment = full_comment[-65_000:] + "\n\n<b>Remaining output truncated<b>\n\n"
     full_comment += "\n```\n\n</details>\n"
+if "FAILURES" not in full_comment:
+    header += '<h3 align="center">Tests PASSed</h3>\n\n'
+else:
+    header += '<h3 align="center">Tests FAILed</h3>\n\n'
+
+full_comment = header + full_comment
 
 token = os.environ["GITHUB_TOKEN"]
 g = Github(token)
