ae4d72702fc50e8914351fa60e243034c3bae97a
Devin Petersohn <devin.petersohn@gmail.com>
2020-06-09 17:02:06 -0700

    Implement groupby.size for axis=1
    * Implemented with transpose, because result is a series,
      no need to transpose result
    
    Signed-off-by: Devin Petersohn <devin.petersohn@gmail.com>

--- modin/pandas/groupby.py
+++ modin/pandas/groupby.py
@@ -251,7 +251,8 @@ class DataFrameGroupBy(object):
     def apply(self, func, *args, **kwargs):
         return self._apply_agg_function(
             # Grouping column in never dropped in groupby.apply, so drop=False
-            lambda df: df.apply(func, *args, **kwargs), drop=False
+            lambda df: df.apply(func, *args, **kwargs),
+            drop=False,
         )
 
     @property
@@ -380,32 +381,42 @@ class DataFrameGroupBy(object):
         )
 
     def size(self):
-        if self._as_index:
-            work_object = self[self._df.columns[0]]
-        else:
-            # Size always works in as_index=True mode so it is necessary to make a copy
-            # of _kwargs and change as_index in it
-            kwargs = self._kwargs.copy()
-            kwargs["as_index"] = True
-            kwargs["squeeze"] = True
-            work_object = SeriesGroupBy(
-                self._df[self._df.columns[0]],
-                self._by,
-                self._axis,
-                idx_name=self._idx_name,
-                drop=False,
-                **kwargs
-            )
+        if self._axis == 0:
+            if self._as_index:
+                work_object = self[self._df.columns[0]]
+            else:
+                # Size always works in as_index=True mode so it is necessary to make a copy
+                # of _kwargs and change as_index in it
+                kwargs = self._kwargs.copy()
+                kwargs["as_index"] = True
+                kwargs["squeeze"] = True
+                work_object = SeriesGroupBy(
+                    self._df[self._df.columns[0]],
+                    self._by,
+                    self._axis,
+                    idx_name=self._idx_name,
+                    drop=False,
+                    **kwargs
+                )
 
-        result = work_object._groupby_reduce(
-            lambda df: pandas.DataFrame(df.size()),
-            lambda df: df.sum(),
-            numeric_only=False,
-        )
-        series_result = Series(query_compiler=result._query_compiler)
-        # Pandas does not name size() output
-        series_result.name = None
-        return series_result
+            result = work_object._groupby_reduce(
+                lambda df: pandas.DataFrame(df.size()),
+                lambda df: df.sum(),
+                numeric_only=False,
+            )
+            series_result = Series(query_compiler=result._query_compiler)
+            # Pandas does not name size() output
+            series_result.name = None
+            return series_result
+        else:
+            return DataFrameGroupBy(
+                self._df.T,
+                self._by,
+                0,
+                drop=self._drop,
+                idx_name=self._idx_name,
+                **self._kwargs
+            ).size()
 
     def sum(self, **kwargs):
         return self._groupby_reduce(lambda df: df.sum(**kwargs), None)
--- modin/pandas/test/test_groupby.py
+++ modin/pandas/test/test_groupby.py
@@ -209,10 +209,22 @@ def test_mixed_dtypes_groupby(as_index):
         # col2 contains NaN, is it necessary to test functions like size()
         "col2",
         ["col2"],
-        pytest.param(["col1", "col2"], marks=pytest.mark.xfail(reason="Excluded because of bug #1554")),
-        pytest.param(["col2", "col4"], marks=pytest.mark.xfail(reason="Excluded because of bug #1554")),
-        pytest.param(["col4", "col2"], marks=pytest.mark.xfail(reason="Excluded because of bug #1554")),
-        pytest.param(["col3", "col4", "col2"], marks=pytest.mark.xfail(reason="Excluded because of bug #1554")),
+        pytest.param(
+            ["col1", "col2"],
+            marks=pytest.mark.xfail(reason="Excluded because of bug #1554"),
+        ),
+        pytest.param(
+            ["col2", "col4"],
+            marks=pytest.mark.xfail(reason="Excluded because of bug #1554"),
+        ),
+        pytest.param(
+            ["col4", "col2"],
+            marks=pytest.mark.xfail(reason="Excluded because of bug #1554"),
+        ),
+        pytest.param(
+            ["col3", "col4", "col2"],
+            marks=pytest.mark.xfail(reason="Excluded because of bug #1554"),
+        ),
         # but cum* functions produce undefined results with NaNs so we need to test the same combinations without NaN too
         ["col5"],
         ["col1", "col5"],
