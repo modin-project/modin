162825c0a42013416aa2a4341e9f88b1289f0f9f
Vasilij Litvinov <vasilij.n.litvinov@intel.com>
2020-07-09 14:28:07 +0300

    Implement abstract Cluster and RPyC connection
    Signed-off-by: Vasilij Litvinov <vasilij.n.litvinov@intel.com>

---
+++ modin/experimental/cloud/cluster.py
@@ -0,0 +1,160 @@
+# Licensed to Modin Development Team under one or more contributor license agreements.
+# See the NOTICE file distributed with this work for additional information regarding
+# copyright ownership.  The Modin Development Team licenses this file to you under the
+# Apache License, Version 2.0 (the "License"); you may not use this file except in
+# compliance with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific language
+# governing permissions and limitations under the License.
+
+import os
+import errno
+from typing import NamedTuple
+
+from modin import set_backends
+
+from . import ConnectionDetails
+from .connection import Connection
+
+
+class _RegionZone(NamedTuple):
+    region: str
+    zone: str
+
+
+class Provider:
+    AWS = "aws"
+
+    __KNOWN = {AWS: [_RegionZone(region="us-west-1", zone="us-west-1a")]}
+    __DEFAULT_HEAD = {AWS: "m5.large"}
+    __DEFAULT_WORKER = {AWS: "m5.large"}
+
+    def __init__(
+        self, name: str, credentials_file: str, region: str = None, zone: str = None
+    ):
+        """
+        Class that holds all information about particular connection to cluster provider, namely
+            * provider name (must be one of known ones)
+            * path to file with credentials (file format is provider-specific)
+            * region and zone where cluster is to be spawned (optional, would be deduced if omitted)
+        """
+
+        if name not in self.__KNOWN:
+            raise ValueError(f"Unknown provider name: {name}")
+        if not os.path.exists(credentials_file):
+            raise OSError(
+                errno.ENOENT, "Credentials file does not exist", credentials_file
+            )
+
+        if region is None:
+            if zone is not None:
+                raise ValueError("Cannot specify a zone without specifying a region")
+            try:
+                region, zone = self.__KNOWN[name][0]
+            except IndexError:
+                raise ValueError(f"No defaults for provider {name}")
+        elif zone is None:
+            for regzone in self.__KNOWN[name]:
+                if regzone.region == region:
+                    zone = regzone.zone
+                    break
+            else:
+                raise ValueError(f"No default for region {region} for provider {name}")
+
+        self.name = name
+        self.region = region
+        self.zone = zone
+        self.credentials_file = os.path.abspath(credentials_file)
+
+    @property
+    def default_head_type(self):
+        return self.__DEFAULT_HEAD[self.name]
+
+    @property
+    def default_worker_type(self):
+        return self.__DEFAULT_WORKER[self.name]
+
+
+class Cluster:
+    """
+    Cluster manager for Modin. Knows how to use certain tools to spawn and destroy clusters,
+    can serve as context manager to switch execution engine and partition to remote.
+    """
+
+    target_engine = None
+    target_partition = None
+
+    def __init__(
+        self,
+        provider: Provider,
+        project_name: str = None,
+        cluster_name: str = "modin-cluster",
+        worker_count: int = 4,
+        head_node_type: str = None,
+        worker_node_type: str = None,
+    ):
+        """
+        Prepare the cluster manager. It needs to know a few things:
+            * which cloud provider to use
+            * what is project name (could be omitted to use default one for account used to connect)
+            * cluster name
+            * worker count
+            * head and worker node instance types (can be omitted to default to provider-defined)
+        """
+
+        self.provider = provider
+        self.project_name = project_name
+        self.cluster_name = cluster_name
+        self.worker_count = worker_count
+        self.head_node_type = head_node_type or provider.default_head_type
+        self.worker_node_type = worker_node_type or provider.default_worker_type
+
+        self.old_backends = None
+        self.connection = None
+        self.spawn(wait=False)
+
+    def spawn(self, wait=True):
+        """
+        Actually spawns the cluster. When already spawned, should be a no-op.
+
+        When wait==False it spawns cluster asynchronously.
+        """
+        raise NotImplementedError()
+
+    def destroy(self):
+        """
+        Destroys the cluster. When already destroyed, should be a no-op.
+        """
+        raise NotImplementedError()
+
+    def _get_connection_details(self) -> ConnectionDetails:
+        """
+        Gets the coordinates on how to connect to cluster frontend node.
+        """
+        raise NotImplementedError()
+
+    def _get_main_python(self) -> str:
+        """
+        Gets the path to 'main' interpreter (the one that houses created environment for running everything)
+        """
+        raise NotImplementedError()
+
+    def __enter__(self):
+        self.spawn(wait=True)  # make sure cluster is ready
+        self.connection = Connection(
+            self._get_connection_details(), self._get_main_python()
+        )
+        self.old_backends = set_backends(self.target_engine, self.target_partition)
+        return self
+
+    def __exit__(self, *a, **kw):
+        set_backends(*self.old_backends)
+        self.connection.stop()
+        self.old_backends = None
+
+    def __del__(self):
+        self.destroy()
---
+++ modin/experimental/cloud/connection.py
@@ -0,0 +1,133 @@
+# Licensed to Modin Development Team under one or more contributor license agreements.
+# See the NOTICE file distributed with this work for additional information regarding
+# copyright ownership.  The Modin Development Team licenses this file to you under the
+# Apache License, Version 2.0 (the "License"); you may not use this file except in
+# compliance with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific language
+# governing permissions and limitations under the License.
+
+import subprocess
+import signal
+import os
+import random
+from shlex import quote
+
+import rpyc
+
+from . import ClusterError, ConnectionDetails
+
+
+class Connection:
+    __current = None
+    connect_timeout = 5
+    tries = 10
+    rpyc_port = 18813
+
+    def __init__(self, details: ConnectionDetails, main_python: str, log_rpyc=None):
+        if log_rpyc is None:
+            log_rpyc = os.environ.get("MODIN_LOG_RPYC", "").title() == "True"
+        self.proc = None
+
+        # find where rpyc_classic is located
+        locator = self.__run(
+            self.__build_opts(details)
+            + [
+                main_python,
+                "-c",
+                "import os; from distutils.dist import Distribution; from distutils.command.install import install; cmd = install(Distribution()); cmd.finalize_options(); print(os.path.join(cmd.install_scripts, 'rpyc_classic.py'))",
+            ]
+        )
+        out, err = locator.communicate(timeout=5)
+        if locator.returncode != 0:
+            raise ClusterError(
+                f"Cannot get path to rpyc_classic, return code: {locator.returncode}"
+            )
+        rpyc_classic = out.splitlines()[0].strip()
+        if not rpyc_classic:
+            raise ClusterError("Got empty path to rpyc_classic")
+
+        port = self.rpyc_port
+        for _ in range(self.tries):
+            cmd = self.__build_opts(details, forward_port=port) + [
+                main_python,
+                rpyc_classic,
+                "--port",
+                str(self.rpyc_port),
+            ]
+            if log_rpyc:
+                cmd.extend(["--logfile", "/tmp/rpyc.log"])
+            proc = self.__run(cmd, capture_out=False)
+            if proc.wait(1) is None:
+                # started successfully
+                self.proc = proc
+                self.rpyc_port = port
+                break
+            # most likely port is busy, pick random one
+            port = random.randint(1024, 65000)
+        else:
+            raise ClusterError("Unable to bind a local port when forwarding")
+
+        Connection.__current = self
+
+    @classmethod
+    def get(cls) -> rpyc.core.protocol.Connection:
+        if (
+            not cls.__current
+            or not cls.__current.proc
+            or cls.__current.proc.poll() is not None
+        ):
+            raise ClusterError("SSH tunnel is not running")
+        return rpyc.classic.connect(
+            "localhost", cls.__current.rpyc_port, keepalive=True
+        )
+
+    def stop(self):
+        if self.proc and self.proc.poll() is None:
+            self.proc.send_signal(signal.SIGINT)
+            if self.proc.wait(self.connect_timeout) is None:
+                self.proc.terminate()
+                if self.proc.wait(self.connect_timeout) is None:
+                    self.proc.kill()
+        self.proc = None
+        if Connection.__current is self:
+            Connection.__current = None
+
+    def __del__(self):
+        self.stop()
+
+    def __build_opts(self, details: ConnectionDetails, forward_port: int = None):
+        socks_proxy = os.environ.get("MODIN_SOCKS_PROXY", None)
+
+        opts = [
+            ("ConnectTimeout", "{}s".format(self.connect_timeout)),
+            ("StrictHostKeyChecking", "no"),
+            # Try fewer extraneous key pairs.
+            ("IdentitiesOnly", "yes"),
+            # Abort if port forwarding fails (instead of just printing to stderr).
+            ("ExitOnForwardFailure", "yes"),
+            # Quickly kill the connection if network connection breaks (as opposed to hanging/blocking).
+            ("ServerAliveInterval", 5),
+            ("ServerAliveCountMax", 3),
+        ]
+
+        if socks_proxy:
+            opts += [("ProxyCommand", f"nc -x {quote(socks_proxy)} %h %p")]
+
+        cmdline = ["ssh", "-i", details.key_file]
+        for oname, ovalue in opts:
+            cmdline.extend(["-o", f"{oname}={ovalue}"])
+        if forward_port:
+            cmdline.extend(["-L", f"{forward_port}:localhost:{self.rpyc_port}"])
+        cmdline.append(f"{details.user_name}@{details.address}")
+
+        return cmdline
+
+    @staticmethod
+    def __run(cmd: list, capture_out: bool = True):
+        redirect = subprocess.PIPE if capture_out else None
+        return subprocess.Popen(cmd, stdout=redirect, stderr=redirect)
