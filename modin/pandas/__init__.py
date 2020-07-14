modin-project/modin

commit 6fa14eeae50c19d761831a79c6691c3fc49b40e1

Add Pub-Sub class to enable switching of execution engine on the fly (#1578)
Signed-off-by: Vasilij Litvinov <vasilij.n.litvinov@intel.com>

modin/pandas/__init__.py
@@ -134,7 +134,7 @@ from .general import (
     unique,
 )
 from .plotting import Plotting as plotting
-from .. import __execution_engine__ as execution_engine
+from .. import execution_engine, Publisher
 
 # Set this so that Pandas doesn't try to multithread by itself
 os.environ["OMP_NUM_THREADS"] = "1"
@@ -238,28 +238,47 @@ def initialize_ray():
         ray.worker.global_worker.run_function_on_all_workers(import_pandas)
 
 
-if execution_engine == "Ray":
-    import ray
+DEFAULT_NPARTITIONS = 4
+_is_first_update = {}
+dask_client = None
 
-    initialize_ray()
-    num_cpus = ray.cluster_resources()["CPU"]
-elif execution_engine == "Dask":  # pragma: no cover
-    from distributed.client import get_client
-    import warnings
 
-    if threading.current_thread().name == "MainThread":
-        warnings.warn("The Dask Engine for Modin is experimental.")
+def _update_engine(publisher: Publisher):
+    global DEFAULT_NPARTITIONS, dask_client
+
+    num_cpus = DEFAULT_NPARTITIONS
+    if publisher.get() == "Ray":
+        import ray
+
+        if _is_first_update.get("Ray", True):
+            initialize_ray()
+        num_cpus = ray.cluster_resources()["CPU"]
+    elif publisher.get() == "Dask":  # pragma: no cover
+        from distributed.client import get_client
+
+        if threading.current_thread().name == "MainThread" and _is_first_update.get(
+            "Dask", True
+        ):
+            import warnings
+
+            warnings.warn("The Dask Engine for Modin is experimental.")
+
         try:
-            client = get_client()
+            dask_client = get_client()
         except ValueError:
             from distributed import Client
 
             num_cpus = os.environ.get("MODIN_CPUS", None) or multiprocessing.cpu_count()
-            client = Client(n_workers=int(num_cpus))
-elif execution_engine != "Python":
-    raise ImportError("Unrecognized execution engine: {}.".format(execution_engine))
+            dask_client = Client(n_workers=int(num_cpus))
 
-DEFAULT_NPARTITIONS = max(4, int(num_cpus))
+    elif publisher.get() != "Python":
+        raise ImportError("Unrecognized execution engine: {}.".format(publisher.get()))
+
+    _is_first_update[publisher.get()] = False
+    DEFAULT_NPARTITIONS = max(4, int(num_cpus))
+
+
+execution_engine.subscribe(_update_engine)
 
 __all__ = [
     "DataFrame",

modin/__init__.py
@@ -15,6 +15,7 @@ import os
 import sys
 import warnings
 from packaging import version
+import collections
 
 from ._version import get_versions
 
@@ -84,9 +85,48 @@ def get_partition_format():
     return os.environ.get("MODIN_BACKEND", "Pandas").title()
 
 
+class Publisher(object):
+    def __init__(self, name, value):
+        self.name = name
+        self.__value = value
+        self.__subs = set()
+        self.__once = collections.defaultdict(set)
+
+    def subscribe(self, callback):
+        self.__subs.add(callback)
+        callback(self)
+
+    def once(self, onvalue, callback):
+        if onvalue == self.__value:
+            callback(self)
+        else:
+            self.__once[onvalue].add(callback)
+
+    def get(self):
+        return self.__value
+
+    def put(self, value):
+        oldvalue, self.__value = self.__value, value
+        if oldvalue != value:
+            for callback in self.__subs:
+                callback(self)
+            try:
+                once = self.__once[value]
+            except KeyError:
+                return
+            if once:
+                for callback in once:
+                    callback(self)
+            del self.__once[value]
+
+
 __version__ = "0.6.3"
-__execution_engine__ = get_execution_engine()
-__partition_format__ = get_partition_format()
+execution_engine = Publisher(name="execution_engine", value=get_execution_engine())
+partition_format = Publisher(name="partition_format", value=get_partition_format())
+
+# for backwards compatibility, remove when all items are migrated to new PubSub model
+__execution_engine__ = execution_engine.get()
+__partition_format__ = partition_format.get()
 
 # We don't want these used outside of this file.
 del get_execution_engine

.github/workflows/ci.yml
@@ -54,6 +54,19 @@ jobs:
           architecture: "x64"
       - run: pip install -r requirements.txt
       - run: python -m pytest modin/test/test_headers.py
+  test-internals:
+    runs-on: ubuntu-latest
+    name: test-internals
+    steps:
+      - uses: actions/checkout@v1
+        with:
+          fetch-depth: 1
+      - uses: actions/setup-python@v1
+        with:
+          python-version: "3.6.x"
+          architecture: "x64"
+      - run: pip install -r requirements.txt
+      - run: python -m pytest modin/test/test_publisher.py modin/data_management/test/test_dispatcher.py
   test-all:
     needs: [lint-flake8, lint-black, test-api, test-headers]
     runs-on: ubuntu-latest

modin/data_management/dispatcher.py
@@ -13,9 +13,7 @@
 
 import os
 
-from modin import __execution_engine__ as execution_engine
-from modin import __partition_format__ as partition_format
-
+from modin import execution_engine, partition_format
 from modin.data_management import factories
 
 
@@ -65,18 +63,27 @@ class EngineDispatcher(object):
     __engine = None
 
     @classmethod
-    def _update_engine(cls):
+    def get_engine(cls) -> factories.BaseFactory:
+        # mostly for testing
+        return cls.__engine
+
+    @classmethod
+    def _update_engine(cls, _):
         if os.environ.get("MODIN_EXPERIMENTAL", "").title() == "True":
             factory_fmt, experimental = "Experimental{}On{}Factory", True
         else:
             factory_fmt, experimental = "{}On{}Factory", False
-        factory_name = factory_fmt.format(partition_format, execution_engine)
+        factory_name = factory_fmt.format(
+            partition_format.get(), execution_engine.get()
+        )
         try:
             cls.__engine = getattr(factories, factory_name)
         except AttributeError:
             if not experimental:
                 # allow missing factories in experimenal mode only
-                raise FactoryNotFoundError(partition_format, execution_engine)
+                raise FactoryNotFoundError(
+                    partition_format.get(), execution_engine.get()
+                )
             cls.__engine = StubFactory.set_failing_name(factory_name)
 
     @classmethod
@@ -164,4 +171,5 @@ class EngineDispatcher(object):
         return cls.__engine._to_pickle(*args, **kwargs)
 
 
-EngineDispatcher._update_engine()
+execution_engine.subscribe(EngineDispatcher._update_engine)
+partition_format.subscribe(EngineDispatcher._update_engine)

modin/data_management/test/test_dispatcher.py
@@ -1,0 +1,57 @@
+# Licensed to Modin Development Team under one or more contributor license agreements.
+# See the NOTICE file distributed with this work for additional information regarding
+# copyright ownership.  The Modin Development Team licenses this file to you under the
+# Apache License, Version 2.0 (the "License"); you may not use this file except in
+# compliance with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific language
+# governing permissions and limitations under the License.
+
+import pytest
+
+from modin import execution_engine, partition_format
+
+from modin.data_management.dispatcher import EngineDispatcher, FactoryNotFoundError
+from modin.data_management import factories
+
+
+class PandasOnTestFactory(factories.BaseFactory):
+    """
+    Stub factory to ensure we can switch execution engine to 'Test'
+    """
+
+
+class TestOnPythonFactory(factories.BaseFactory):
+    """
+    Stub factory to ensure we can switch partition format to 'Test'
+    """
+
+
+# inject the stubs
+factories.PandasOnTestFactory = PandasOnTestFactory
+factories.TestOnPythonFactory = TestOnPythonFactory
+
+
+def test_default_engine():
+    assert issubclass(EngineDispatcher.get_engine(), factories.BaseFactory)
+    assert EngineDispatcher.get_engine().io_cls
+
+
+def test_engine_switch():
+    execution_engine.put("Test")
+    assert EngineDispatcher.get_engine() == PandasOnTestFactory
+    execution_engine.put("Python")  # revert engine to default
+
+    partition_format.put("Test")
+    assert EngineDispatcher.get_engine() == TestOnPythonFactory
+    partition_format.put("Pandas")  # revert engine to default
+
+
+def test_engine_wrong_factory():
+    with pytest.raises(FactoryNotFoundError):
+        execution_engine.put("BadEngine")
+    execution_engine.put("Python")  # revert engine to default

modin/pandas/test/test_api.py
@@ -56,6 +56,7 @@ def test_top_level_api_equality():
         "datetimes",
         "reshape",
         "execution_engine",
+        "Publisher",
         "types",
         "sys",
         "initialize_ray",
@@ -66,7 +67,7 @@ def test_top_level_api_equality():
         "os",
         "multiprocessing",
         "Client",
-        "client",
+        "dask_client",
         "get_client",
     ]
 

modin/test/test_publisher.py
@@ -1,0 +1,54 @@
+# Licensed to Modin Development Team under one or more contributor license agreements.
+# See the NOTICE file distributed with this work for additional information regarding
+# copyright ownership.  The Modin Development Team licenses this file to you under the
+# Apache License, Version 2.0 (the "License"); you may not use this file except in
+# compliance with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific language
+# governing permissions and limitations under the License.
+
+from collections import defaultdict
+
+from modin import Publisher
+
+
+def test_equals():
+    pub = Publisher("name", "value1")
+    assert pub.get() == "value1"
+
+    pub.put("value2")
+    assert pub.get() == "value2"
+
+
+def test_triggers():
+    results = defaultdict(int)
+    callbacks = []
+
+    def make_callback(name, res=results):
+        def callback(p: Publisher):
+            res[name] += 1
+
+        # keep reference to callbacks so they won't be removed by GC
+        callbacks.append(callback)
+        return callback
+
+    pub = Publisher("name", "init")
+    pub.once("init", make_callback("init"))
+    assert results["init"] == 1
+
+    pub.once("never", make_callback("never"))
+    pub.once("once", make_callback("once"))
+    pub.subscribe(make_callback("subscribe"))
+
+    pub.put("multi")
+    pub.put("once")
+    pub.put("multi")
+    pub.put("once")
+
+    expected = [("init", 1), ("never", 0), ("once", 1), ("subscribe", 5)]
+    for name, val in expected:
+        assert results[name] == val, "{} has wrong count".format(name)

commit 3adbcf806bfcb417942bde8237097011db48e473

Fix regression introduced in #1578 where Dask Client was created (#1723)
Signed-off-by: Devin Petersohn <devin.petersohn@gmail.com>

modin/pandas/__init__.py
@@ -262,13 +262,15 @@ def _update_engine(publisher: Publisher):
 
             warnings.warn("The Dask Engine for Modin is experimental.")
 
-        try:
-            dask_client = get_client()
-        except ValueError:
-            from distributed import Client
+            try:
+                dask_client = get_client()
+            except ValueError:
+                from distributed import Client
 
-            num_cpus = os.environ.get("MODIN_CPUS", None) or multiprocessing.cpu_count()
-            dask_client = Client(n_workers=int(num_cpus))
+                num_cpus = (
+                    os.environ.get("MODIN_CPUS", None) or multiprocessing.cpu_count()
+                )
+                dask_client = Client(n_workers=int(num_cpus))
 
     elif publisher.get() != "Python":
         raise ImportError("Unrecognized execution engine: {}.".format(publisher.get()))