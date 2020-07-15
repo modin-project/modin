054e894f2b0fdcc3ff76b5590d269cb3597c37e8
Vasilij Litvinov <vasilij.n.litvinov@intel.com>
2020-07-16 02:07:35 +0300

    Handle __new__ for DataFrame and BaseDataset properly
    Also fix parser_f getting its args
    
    Signed-off-by: Vasilij Litvinov <vasilij.n.litvinov@intel.com>

--- modin/pandas/dataframe.py
+++ modin/pandas/dataframe.py
@@ -2735,7 +2735,9 @@ class DataFrame(_DataFrame):
             cls.__real_cls__ = _DataFrame
 
     def __new__(cls, *a, **kw):
-        return cls.__real_cls__(*a, **kw)
+        if cls is DataFrame:
+            return cls.__real_cls__(*a, **kw)
+        return super().__new__(cls)
 
 
 execution_engine.subscribe(DataFrame._update_engine)
--- modin/pandas/io.py
+++ modin/pandas/io.py
@@ -106,8 +106,8 @@ def _make_parser_func(sep):
         memory_map=False,
         float_precision=None,
     ):
-        _, _, _, kwargs = inspect.getargvalues(inspect.currentframe())
-        if kwargs.get("sep", sep) is False:
+        kwargs = dict(locals())
+        if kwargs.get("sep", False) is False:
             kwargs["sep"] = "\t"
         return _read(**kwargs)
 
