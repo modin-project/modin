ab2d0369728c01fd03aacfc76602ced0d89133f0
izamyati <igor.zamyatin@intel.com>
2020-07-27 08:08:23 -0500

    Disable lazy fetch

--- modin/experimental/engines/omnisci_on_ray/frame/omnisci_worker.py
+++ modin/experimental/engines/omnisci_on_ray/frame/omnisci_worker.py
@@ -31,7 +31,8 @@ class OmnisciServer:
     @classmethod
     def start_server(cls):
         if cls._server is None:
-            cls._server = PyDbEngine(enable_union=1, enable_columnar_output=1)
+            cls._server = PyDbEngine(enable_union=1, enable_columnar_output=1,
+            enable_lazy_fetch=0)
 
     @classmethod
     def stop_server(cls):
