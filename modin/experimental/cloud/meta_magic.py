7225f2cd9286afef6a5867750564ddf7c413edcf
Vasily Litvinov <45396231+vnlitvinov@users.noreply.github.com>
2020-07-31 15:25:38 +0300

    Fix access to special attributes in experimental mode (#1875)
    Signed-off-by: Vasilij Litvinov <vasilij.n.litvinov@intel.com>

--- modin/experimental/cloud/meta_magic.py
+++ modin/experimental/cloud/meta_magic.py
@@ -17,7 +17,18 @@ import types
 
 from modin import execution_engine
 
-_LOCAL_ATTRS = frozenset(("__new__", "__dict__", "__wrapper_remote__"))
+# the attributes that must be alwasy taken from a local part of dual-nature class,
+# never going to remote end
+_LOCAL_ATTRS = frozenset(
+    (
+        "__new__",
+        "__dict__",
+        "__wrapper_remote__",
+        "__real_cls__",
+        "__mro__",
+        "__class__",
+    )
+)
 
 
 class RemoteMeta(type):
@@ -46,47 +57,44 @@ class RemoteMeta(type):
     def __getattribute__(self, name):
         if name in _LOCAL_ATTRS:
             # never proxy special attributes, always get them from the class type
-            res = object.__getattribute__(self, name)
+            return super().__getattribute__(name)
         else:
             try:
                 # Go for proxying class-level attributes first;
                 # make sure to check for attribute in self.__dict__ to get the class-level
                 # attribute from the class itself, not from some of its parent classes.
-                # Also note we use object.__getattribute__() to skip any potential
-                # class-level __getattr__
-                res = object.__getattribute__(self, "__dict__")[name]
+                res = super().__getattribute__("__dict__")[name]
             except KeyError:
+                # Class-level attribute not found in the class itself; it might be present
+                # in its parents, but we must first see if we should go to a remote
+                # end, because in "remote context" local attributes are only those which
+                # are explicitly allowed by being defined in the class itself.
+                frame = sys._getframe()
                 try:
-                    res = object.__getattribute__(self, name)
+                    is_inspect = frame.f_back.f_code.co_filename == inspect.__file__
                 except AttributeError:
-                    frame = sys._getframe()
+                    is_inspect = False
+                finally:
+                    del frame
+                if is_inspect:
+                    # be always-local for inspect.* functions
+                    return super().__getattribute__(name)
+                else:
                     try:
-                        is_inspect = frame.f_back.f_code.co_filename == inspect.__file__
+                        remote = self.__real_cls__.__wrapper_remote__
                     except AttributeError:
-                        is_inspect = False
-                    finally:
-                        del frame
-                    if is_inspect:
-                        # be always-local for inspect.* functions
-                        res = super().__getattribute__(name)
-                    else:
-                        try:
-                            remote = object.__getattribute__(
-                                object.__getattribute__(self, "__real_cls__"),
-                                "__wrapper_remote__",
-                            )
-                        except AttributeError:
-                            # running in local mode, fall back
-                            res = super().__getattribute__(name)
-                        else:
-                            res = getattr(remote, name)
-        try:
-            # note that any attribute might be in fact a data descriptor,
-            # account for that
-            getter = res.__get__
-        except AttributeError:
-            return res
-        return getter(None, self)
+                        # running in local mode, fall back
+                        return super().__getattribute__(name)
+                    return getattr(remote, name)
+            else:
+                try:
+                    # note that any attribute might be in fact a data descriptor,
+                    # account for that; we only need it for attributes we get from __dict__[],
+                    # because other cases are handled by super().__getattribute__ for us
+                    getter = res.__get__
+                except AttributeError:
+                    return res
+                return getter(None, self)
 
 
 _KNOWN_DUALS = {}
