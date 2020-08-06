c48b4ae87b88ef886962107d6dd7f33e9ac833c5
Itamar Turner-Trauring <itamar@itamarst.org>
2020-08-06 15:46:25 -0400

    FIX-#1497: Don't sort in concat() when sort=False (#1889)
    Signed-off-by: Itamar Turner-Trauring <itamar@itamarst.org>

--- modin/pandas/test/test_concat.py
+++ modin/pandas/test/test_concat.py
@@ -201,3 +201,18 @@ def test_concat_dictionary(axis):
         pd.concat({"A": modin_df, "B": modin_df2}, axis=axis),
         pandas.concat({"A": pandas_df, "B": pandas_df2}, axis=axis),
     )
+
+
+@pytest.mark.parametrize("sort", [False, True])
+@pytest.mark.parametrize("join", ["inner", "outer"])
+@pytest.mark.parametrize("axis", [0, 1])
+def test_sort_order(sort, join, axis):
+    pandas_df = pandas.DataFrame({"c": [3], "d": [4]}, columns=["d", "c"])
+    pandas_df2 = pandas.DataFrame({"a": [1], "b": [2]}, columns=["b", "a"])
+    modin_df, modin_df2 = from_pandas(pandas_df), from_pandas(pandas_df2)
+    pandas_concat = pandas.concat([pandas_df, pandas_df2], join=join, sort=sort)
+    modin_concat = pd.concat([modin_df, modin_df2], join=join, sort=sort)
+    df_equals(
+        pandas_concat, modin_concat,
+    )
+    assert list(pandas_concat.columns) == list(modin_concat.columns)
