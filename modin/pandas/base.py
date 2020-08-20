bf6d7c561a0bea7bb743c3f3a8581c2ed18de5f8
ienkovich <ilya.enkovich@intel.com>
2020-08-20 16:23:39 -0500

    REFACTOR-#1938: avoid index access in a simple column reference
    Signed-off-by: ienkovich <ilya.enkovich@intel.com>

--- modin/pandas/base.py
+++ modin/pandas/base.py
@@ -3442,9 +3442,14 @@ class BasePandasDataset(object):
             return self._default_to_pandas("__getitem__", key)
         # see if we can slice the rows
         # This lets us reuse code in Pandas to error check
-        indexer = convert_to_index_sliceable(
-            getattr(pandas, type(self).__name__)(index=self.index), key
-        )
+        indexer = None
+        if isinstance(key, slice) or (
+            isinstance(key, str)
+            and (not hasattr(self, "columns") or key not in self.columns)
+        ):
+            indexer = convert_to_index_sliceable(
+                getattr(pandas, type(self).__name__)(index=self.index), key
+            )
         if indexer is not None:
             return self._getitem_slice(indexer)
         else:
