04ec6d402d4b6d82325fe8b71d324108f71ac454
Vasilij Litvinov <vasilij.n.litvinov@intel.com>
2020-08-26 19:09:39 +0300

    FEAT-#1981: Add fixture that enables local "cluster" when requested
    Signed-off-by: Vasilij Litvinov <vasilij.n.litvinov@intel.com>

---
+++ modin/conftest.py
@@ -0,0 +1,46 @@
+# Licensed to Modin Development Team under one or more contributor license agreements.
+# See the NOTICE file distributed with this work for additional information regarding
+# copyright ownership.  The Modin Development Team licenses this file to you under the
+# Apache License, Version 2.0 (the "License"); you may not use this file except in
+# compliance with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific language
+# governing permissions and limitations under the License.
+
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--simulate-cloud",
+        action="store",
+        default="off",
+        help="simulate cloud for testing: off|normal|experimental",
+    )
+
+
+@pytest.fixture(scope="session", autouse=True)
+def simulate_cloud(request):
+    mode = request.config.getoption("--simulate-cloud").lower()
+    if mode == "off":
+        return
+    if mode not in ("normal", "experimental"):
+        raise ValueError(f"Unsupported --simulate-cloud mode: {mode}")
+
+    from modin.experimental.cloud import create_cluster, get_connection
+
+    with create_cluster("local", __spawner__="local"):
+
+        def set_env():
+            import os
+
+            os.environ["MODIN_EXPERIMENTAL"] = (
+                "True" if mode == "experimental" else "False"
+            )
+
+        get_connection().teleport(set_env)()
+        yield
