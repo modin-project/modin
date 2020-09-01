9b782ae9adc5997f49baef427bd2f068161d6257
binarycrayon <10211+binarycrayon@users.noreply.github.com>
2020-09-01 09:01:15 -0700

    FEAT-#1958: pin ray to 0.8.7 (#1966)
    Signed-off-by: Yudi Xue <10211+binarycrayon@users.noreply.github.com>

--- modin/__init__.py
+++ modin/__init__.py
@@ -53,7 +53,7 @@ def get_execution_engine():
             except ImportError:
                 pass
             else:
-                if version.parse(ray.__version__) != version.parse("0.8.6"):
+                if version.parse(ray.__version__) != version.parse("0.8.7"):
                     raise ImportError(
                         "Please `pip install modin[ray]` to install compatible Ray version."
                     )
