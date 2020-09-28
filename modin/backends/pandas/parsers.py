1a5613dd38f423c825f91436d3310b8e3fd0af9e
Devin Petersohn <devin.petersohn@gmail.com>
2020-09-28 10:15:04 -0500

    FIX-#1921: Add documentation
    Signed-off-by: Devin Petersohn <devin.petersohn@gmail.com>

--- modin/backends/pandas/parsers.py
+++ modin/backends/pandas/parsers.py
@@ -299,7 +299,9 @@ class PandasExcelParser(PandasParser):
             usecols=usecols,
             **kwargs
         )
-
+        # In excel if you create a row with only a border (no values), this parser will
+        # interpret that as a row of NaN values. Pandas discards these values, so we
+        # also must discard these values.
         pandas_df = parser.read().dropna(how="all")
         # Since we know the number of rows that occur before this partition, we can
         # correctly assign the index in cases of RangeIndex. If it is not a RangeIndex,
