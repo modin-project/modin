7db59fe26071ccadd3738d86ed4dafbe1fddf771
Anatoly Myachev <anatoly.myachev@intel.com>
2020-10-19 19:23:38 +0300

    FIX-#2195: fix describe error for datasets with datetimes
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- modin/backends/pandas/query_compiler.py
+++ modin/backends/pandas/query_compiler.py
@@ -27,6 +27,7 @@ import warnings
 from modin.backends.base.query_compiler import BaseQueryCompiler
 from modin.error_message import ErrorMessage
 from modin.utils import try_cast_to_pandas, wrap_udf_function
+from modin.pandas import Index
 from modin.data_management.functions import (
     FoldFunction,
     MapFunction,
@@ -1559,6 +1560,17 @@ class PandasQueryCompiler(BaseQueryCompiler):
             .astype(self.dtypes)
             .describe(**kwargs)
         )
+        new_index = empty_df.index
+
+        # Note: `describe` convert timestamp type to object type
+        # which results in the loss of two values in index: `first` and `last`
+        # for empty DataFrame.
+        datetime_is_numeric = kwargs.get("datetime_is_numeric") or False
+        if not any(map(is_numeric_dtype, empty_df.dtypes)) and not datetime_is_numeric:
+            for col_name in empty_df.dtypes.index:
+                # if previosly type of `col_name` was datetime or timedelta
+                if is_datetime_or_timedelta_dtype(self.dtypes[col_name]):
+                    new_index = Index(empty_df.index.to_list() + ["first"] + ["last"])
 
         def describe_builder(df, internal_indices=[]):
             return df.iloc[:, internal_indices].describe(**kwargs)
@@ -1568,7 +1580,7 @@ class PandasQueryCompiler(BaseQueryCompiler):
                 0,
                 describe_builder,
                 empty_df.columns,
-                new_index=empty_df.index,
+                new_index=new_index,
                 new_columns=empty_df.columns,
             )
         )
