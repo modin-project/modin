a0a88445356ee945bc4822c57d5752b652f23c0a
Anatoly Myachev <45976948+anmyachev@users.noreply.github.com>
2020-10-20 15:28:48 +0300

    FIX-#2195: Update modin/pandas/test/dataframe/test_reduction.py
    Co-authored-by: Dmitry Chigarev <62142979+dchigarev@users.noreply.github.com>
    
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- modin/pandas/test/dataframe/test_reduction.py
+++ modin/pandas/test/dataframe/test_reduction.py
@@ -140,12 +140,16 @@ def test_describe(data, percentiles):
     )
 
 
+@pytest.mark.parametrize("has_numeric_column", [False, True])
 @pytest.mark.parametrize("datetime_is_numeric", [True, False, None])
-def test_2195(datetime_is_numeric):
+def test_2195(datetime_is_numeric, has_numeric_column):
     data = {
         "categorical": pd.Categorical(["d"] * 10 ** 2),
         "date": [np.datetime64("2000-01-01")] * 10 ** 2,
     }
+
+    if has_numeric_column:
+        data.update({"numeric": [5] * 10 ** 2})
 
     modin_df, pandas_df = pd.DataFrame(data), pandas.DataFrame(data)
 
