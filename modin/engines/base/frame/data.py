bfa24c299a4a281efb9e4f5a16962d62efb45bc8
Anatoly Myachev <anatoly.myachev@intel.com>
2020-11-03 20:37:13 +0300

    FIX-#2322: enable test
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- modin/engines/base/frame/data.py
+++ modin/engines/base/frame/data.py
@@ -1735,7 +1735,7 @@ class BasePandasFrame(object):
                     other[i]._partitions,
                     lambda df: df.reindex(joined_index, axis=axis),
                     lengths=self._row_lengths if axis == 0 else self._column_widths,
-                    manual_partition=False,
+                    manual_partition=True,
                 )
             reindexed_other_list.append(reindexed_other)
         return reindexed_self, reindexed_other_list, joined_index
