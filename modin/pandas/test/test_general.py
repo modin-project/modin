53a4f89c1ecf1a7414d577e9aa408608c35e658a
Itamar Turner-Trauring <itamar@itamarst.org>
2020-11-05 13:49:46 -0500

    FEAT-#2013: More tests for more API options.
    Signed-off-by: Itamar Turner-Trauring <itamar@itamarst.org>

--- modin/pandas/test/test_general.py
+++ modin/pandas/test/test_general.py
@@ -16,7 +16,7 @@ import pytest
 import modin.pandas as pd
 import numpy as np
 from numpy.testing import assert_array_equal
-from modin.utils import get_current_backend
+from modin.utils import get_current_backend, to_pandas
 
 from .utils import test_data_values, test_data_keys, df_equals
 
@@ -283,11 +283,131 @@ def test_merge_asof_suffixes():
         )
 
 
-# TODO test merge_asof
-# by
-# tolerance
-# direction
-# allow_exact_matches
+def test_merge_asof_merge_options():
+    modin_quotes = pd.DataFrame(
+        {
+            "time": [
+                pd.Timestamp("2016-05-25 13:30:00.023"),
+                pd.Timestamp("2016-05-25 13:30:00.023"),
+                pd.Timestamp("2016-05-25 13:30:00.030"),
+                pd.Timestamp("2016-05-25 13:30:00.041"),
+                pd.Timestamp("2016-05-25 13:30:00.048"),
+                pd.Timestamp("2016-05-25 13:30:00.049"),
+                pd.Timestamp("2016-05-25 13:30:00.072"),
+                pd.Timestamp("2016-05-25 13:30:00.075"),
+            ],
+            "ticker": ["GOOG", "MSFT", "MSFT", "MSFT", "GOOG", "AAPL", "GOOG", "MSFT"],
+            "bid": [720.50, 51.95, 51.97, 51.99, 720.50, 97.99, 720.50, 52.01],
+            "ask": [720.93, 51.96, 51.98, 52.00, 720.93, 98.01, 720.88, 52.03],
+        }
+    )
+    modin_trades = pd.DataFrame(
+        {
+            "time": [
+                pd.Timestamp("2016-05-25 13:30:00.023"),
+                pd.Timestamp("2016-05-25 13:30:00.038"),
+                pd.Timestamp("2016-05-25 13:30:00.048"),
+                pd.Timestamp("2016-05-25 13:30:00.048"),
+                pd.Timestamp("2016-05-25 13:30:00.048"),
+            ],
+            "ticker2": ["MSFT", "MSFT", "GOOG", "GOOG", "AAPL"],
+            "price": [51.95, 51.95, 720.77, 720.92, 98.0],
+            "quantity": [75, 155, 100, 100, 100],
+        }
+    )
+    pandas_quotes, pandas_trades = to_pandas(modin_quotes), to_pandas(modin_trades)
+
+    # left_by + right_by
+    df_equals(
+        pandas.merge_asof(
+            pandas_quotes,
+            pandas_trades,
+            on="time",
+            left_by="ticker",
+            right_by="ticker2",
+        ),
+        pd.merge_asof(
+            modin_quotes,
+            modin_trades,
+            on="time",
+            left_by="ticker",
+            right_by="ticker2",
+        ),
+    )
+
+    # Just by:
+    pandas_trades["ticker"] = pandas_trades["ticker2"]
+    modin_trades["ticker"] = modin_trades["ticker2"]
+    df_equals(
+        pandas.merge_asof(
+            pandas_quotes,
+            pandas_trades,
+            on="time",
+            by="ticker",
+        ),
+        pd.merge_asof(
+            modin_quotes,
+            modin_trades,
+            on="time",
+            by="ticker",
+        ),
+    )
+
+    # Tolerance
+    df_equals(
+        pandas.merge_asof(
+            pandas_quotes,
+            pandas_trades,
+            on="time",
+            by="ticker",
+            tolerance=pd.Timedelta("2ms"),
+        ),
+        pd.merge_asof(
+            modin_quotes,
+            modin_trades,
+            on="time",
+            by="ticker",
+            tolerance=pd.Timedelta("2ms"),
+        ),
+    )
+
+    # Direction
+    df_equals(
+        pandas.merge_asof(
+            pandas_quotes,
+            pandas_trades,
+            on="time",
+            by="ticker",
+            direction="forward",
+        ),
+        pd.merge_asof(
+            modin_quotes,
+            modin_trades,
+            on="time",
+            by="ticker",
+            direction="forward",
+        ),
+    )
+
+    # Allow exact matches
+    df_equals(
+        pandas.merge_asof(
+            pandas_quotes,
+            pandas_trades,
+            on="time",
+            by="ticker",
+            tolerance=pd.Timedelta("10ms"),
+            allow_exact_matches=False,
+        ),
+        pd.merge_asof(
+            modin_quotes,
+            modin_trades,
+            on="time",
+            by="ticker",
+            tolerance=pd.Timedelta("10ms"),
+            allow_exact_matches=False,
+        ),
+    )
 
 
 def test_pivot():
