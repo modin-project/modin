16881dc9136418ac058e0fd2577851f01f882117
Devin Petersohn <devin.petersohn@gmail.com>
2021-01-15 09:48:03 -0600

    TEST-#2616: Remove unused import
    Signed-off-by: Devin Petersohn <devin.petersohn@gmail.com>

--- modin/test/backends/base/test_internals.py
+++ modin/test/backends/base/test_internals.py
@@ -11,8 +11,6 @@
 # ANY KIND, either express or implied. See the License for the specific language
 # governing permissions and limitations under the License.
 
-import modin.pandas as pd
-
 import pandas
 import pytest
 
