85f32966b53d52909033a36ca5247c7735f5d742
Devin Petersohn <devin.petersohn@gmail.com>
2021-01-28 16:46:07 -0600

    FIX-#2596: Update excel implementation behavior to match pandas
    Signed-off-by: Devin Petersohn <devin.petersohn@gmail.com>

--- modin/backends/pandas/parsers.py
+++ modin/backends/pandas/parsers.py
@@ -339,7 +339,6 @@ class PandasExcelParser(PandasParser):
                             data[row][col] = last
                         else:
                             last = data[row][col]
-
         parser = TextParser(
             data,
             header=header,
@@ -352,7 +351,7 @@ class PandasExcelParser(PandasParser):
         # In excel if you create a row with only a border (no values), this parser will
         # interpret that as a row of NaN values. Pandas discards these values, so we
         # also must discard these values.
-        pandas_df = parser.read().dropna(how="all")
+        pandas_df = parser.read()
         # Since we know the number of rows that occur before this partition, we can
         # correctly assign the index in cases of RangeIndex. If it is not a RangeIndex,
         # the index is already correct because it came from the data.
