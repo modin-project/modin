d35d87eb7ce3198b87e7507b75c99a4a6af6b2fe
Xiwen Zhang <xzhang778@simontam.cc.gatech.edu>
2021-02-04 00:03:25 -0500

    FEAT-#27: Add environmental variable for cudf backend
    Signed-off-by: Khang Vu <khangvu200391845@gmail.com>

--- modin/config/__main__.py
+++ modin/config/__main__.py
@@ -19,6 +19,7 @@ def print_config_help():
     for objname in sorted(globals()):
         obj = globals()[objname]
         if isinstance(obj, type) and issubclass(obj, Parameter) and not obj.is_abstract:
+            print("print_config_help()")
             print(f"{obj.get_help()}\n\tCurrent value: {obj.get()}")  # noqa: T001
 
 
