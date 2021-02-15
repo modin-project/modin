7935c59a94fafc7a975d62809c04a32a6c05c98f
Dmitry Chigarev <62142979+dchigarev@users.noreply.github.com>
2021-02-15 18:48:37 +0300

    FIX-#2735: move '.reindex' logic about axis dispatching from the base class (#2736)
    Signed-off-by: Dmitry Chigarev <dmitry.chigarev@intel.com>

--- modin/pandas/base.py
+++ modin/pandas/base.py
@@ -1686,65 +1686,28 @@ class BasePandasDataset(object):
 
     def reindex(
         self,
-        labels=None,
         index=None,
         columns=None,
-        axis=None,
-        method=None,
         copy=True,
-        level=None,
-        fill_value=np.nan,
-        limit=None,
-        tolerance=None,
+        **kwargs,
     ):
-        axis = self._get_axis_number(axis)
-        if (columns is not None and self._query_compiler.has_multiindex(axis=1)) or (
-            index is not None and self._query_compiler.has_multiindex()
-        ):
-            return self._default_to_pandas(
-                "reindex",
-                labels=labels,
-                index=index,
-                columns=columns,
-                method=method,
-                copy=copy,
-                level=level,
-                fill_value=fill_value,
-                limit=limit,
-                tolerance=tolerance,
-            )
         if (
-            level is not None
-            or (axis == 1 and self._query_compiler.has_multiindex(axis=1))
-            or (axis == 0 and self._query_compiler.has_multiindex())
+            kwargs.get("level") is not None
+            or (index is not None and self._query_compiler.has_multiindex())
+            or (columns is not None and self._query_compiler.has_multiindex(axis=1))
         ):
-            return self._default_to_pandas(
-                "reindex",
-                labels=labels,
-                level=level,
-                method=method,
-                copy=copy,
-                axis=axis,
-                fill_value=fill_value,
-                limit=limit,
-                tolerance=tolerance,
-            )
-        if axis == 0 and labels is not None:
-            index = labels
-        elif labels is not None:
-            columns = labels
+            if index is not None:
+                kwargs["index"] = index
+            if columns is not None:
+                kwargs["columns"] = columns
+            return self._default_to_pandas("reindex", copy=copy, **kwargs)
         new_query_compiler = None
         if index is not None:
             if not isinstance(index, pandas.Index):
                 index = pandas.Index(index)
             if not index.equals(self.index):
                 new_query_compiler = self._query_compiler.reindex(
-                    axis=0,
-                    labels=index,
-                    method=method,
-                    fill_value=fill_value,
-                    limit=limit,
-                    tolerance=tolerance,
+                    axis=0, labels=index, **kwargs
                 )
         if new_query_compiler is None:
             new_query_compiler = self._query_compiler
@@ -1754,12 +1717,7 @@ class BasePandasDataset(object):
                 columns = pandas.Index(columns)
             if not columns.equals(self.columns):
                 final_query_compiler = new_query_compiler.reindex(
-                    axis=1,
-                    labels=columns,
-                    method=method,
-                    fill_value=fill_value,
-                    limit=limit,
-                    tolerance=tolerance,
+                    axis=1, labels=columns, **kwargs
                 )
         if final_query_compiler is None:
             final_query_compiler = new_query_compiler
