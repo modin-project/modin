7e41e5fd414a4a4683648e3c5e30b3c07fca7424
Anatoly Myachev <45976948+anmyachev@users.noreply.github.com>
2021-02-26 18:12:44 +0300

    TEST-#2728: add reset_index benchmark (#2765)
    * TEST-#2728: add reset_index benchmark
    
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>
    
    * TEST-#2728: add multiindex case
    
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- asv_bench/benchmarks/benchmarks.py
+++ asv_bench/benchmarks/benchmarks.py
@@ -506,6 +506,24 @@ class TimeMultiIndexing:
         )
 
 
+class TimeResetIndex:
+    param_names = ["shape", "drop", "level"]
+    params = [UNARY_OP_DATA_SIZE[ASV_DATASET_SIZE], [False, True], [None, "level_1"]]
+
+    def setup(self, shape, drop, level):
+        self.df = generate_dataframe(ASV_USE_IMPL, "int", *shape, RAND_LOW, RAND_HIGH)
+
+        if level:
+            index = pd.MultiIndex.from_product(
+                [self.df.index[: shape[0] // 2], ["bar", "foo"]],
+                names=["level_1", "level_2"],
+            )
+            self.df.index = index
+
+    def time_reset_index(self, shape, drop, level):
+        execute(self.df.reset_index(drop=drop, level=level))
+
+
 class TimeAstype:
     param_names = ["shape", "dtype", "astype_ncolumns"]
     params = [
