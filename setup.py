a6c7a4d649dd095dc40daa1e80f8a4081f145efb
Richard Lin <richard.lin.047@berkeley.edu>
2021-03-02 12:50:17 -0500

    FEAT-#2796: Add modin[spreadsheet] install and modin.spreadsheet import
    Signed-off-by: Richard Lin <richard.lin.047@berkeley.edu>

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ class ModinDistribution(Distribution):
 dask_deps = ["dask>=2.12.0,<=2.19.0", "distributed>=2.12.0,<=2.19.0"]
 ray_deps = ["ray>=1.0.0,<1.2.0", "pyarrow==1.0"]
 remote_deps = ["rpyc==4.1.5", "cloudpickle==1.4.1", "boto3==1.4.8"]
+spreadsheet_deps = ["modin-spreadsheet>=0.1.0"]
 
 all_deps = dask_deps + ray_deps + remote_deps
 
@@ -61,6 +62,7 @@ setup(
         "dask": dask_deps,
         "ray": ray_deps,
         "remote": remote_deps,
+        "spreadsheet": spreadsheet_deps,
         "all": all_deps,
     },
     python_requires=">=3.7.1",
