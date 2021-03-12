f623560d2d1f5f3a2440c9ca533199675f9a5072
Devin Petersohn <devin.petersohn@gmail.com>
2021-03-12 10:21:55 -0600

    FIX-#2859: Lint
    Signed-off-by: Devin Petersohn <devin.petersohn@gmail.com>

--- modin/pandas/test/dataframe/test_reduction.py
+++ modin/pandas/test/dataframe/test_reduction.py
@@ -142,6 +142,7 @@ def test_count_dtypes(data):
         lambda df: df.isna().count(axis=0),
     )
 
+
 @pytest.mark.parametrize("percentiles", [None, 0.10, 0.11, 0.44, 0.78, 0.99])
 @pytest.mark.parametrize("data", test_data_values, ids=test_data_keys)
 def test_describe(data, percentiles):
