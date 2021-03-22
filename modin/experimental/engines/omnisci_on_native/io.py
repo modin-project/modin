2f47ea1f96de12c6d47038b9db3ff5bc2c50330c
Alexander Myskov <alexander.myskov@intel.com>
2021-03-22 06:36:03 -0500

    FEAT-2787: implement true/false_values
    Signed-off-by: Alexander Myskov <alexander.myskov@intel.com>

--- modin/experimental/engines/omnisci_on_native/io.py
+++ modin/experimental/engines/omnisci_on_native/io.py
@@ -129,8 +129,6 @@ class OmnisciOnNativeIO(BaseIO, TextFileDispatcher):
         "prefix",
         "converters",
         "skipfooter",
-        "true_values",
-        "false_values",
         "nrows",
         "skipinitialspace",
         "squeeze",
@@ -273,8 +271,13 @@ class OmnisciOnNativeIO(BaseIO, TextFileDispatcher):
                 check_utf8=None,
                 column_types=column_types,
                 null_values=None,
-                true_values=None,
-                false_values=None,
+                # we need to add default true/false_values like Pandas does
+                true_values=true_values + ["TRUE", "True", "true"]
+                if true_values is not None
+                else true_values,
+                false_values=false_values + ["False", "FALSE", "false"]
+                if false_values is not None
+                else false_values,
                 # timestamp fields should be handled as strings if parse_dates
                 # didn't passed explicitly as an array or a dict
                 timestamp_parsers=[""] if isinstance(parse_dates, bool) else None,
