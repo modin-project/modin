dc4d8d6d67a05de7b83b2de36c7fc2702ccb56cf
Anatoly Myachev <45976948+anmyachev@users.noreply.github.com>
2021-04-05 18:56:38 +0300

    DOCS-#2940: add documentation for asv  (#2948)
    Co-authored-by: Vasily Litvinov <vasilij.n.litvinov@intel.com>
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- asv_bench/benchmarks/utils.py
+++ asv_bench/benchmarks/utils.py
@@ -11,12 +11,20 @@
 # ANY KIND, either express or implied. See the License for the specific language
 # governing permissions and limitations under the License.
 
+"""
+This module contains the functionality that is used when benchmarking modin commits.
+In the case of using utilities from the main modin code, there is a chance that when
+benchmarking old commits, the utilities changed, which in turn can unexpectedly affect
+the performance results, hence some utility functions are duplicated here.
+"""
+
 import os
 import logging
 import modin.pandas as pd
 import pandas
 import numpy as np
 import uuid
+from typing import Optional, Union
 
 RAND_LOW = 0
 RAND_HIGH = 100
@@ -90,7 +98,20 @@ IMPL = {
 }
 
 
-def translator_groupby_ngroups(groupby_ngroups, shape):
+def translator_groupby_ngroups(groupby_ngroups: Union[str, int], shape: tuple) -> int:
+    """
+    Translate a string representation of the number of groups, into a number.
+
+    Parameters
+    ----------
+    groupby_ngroups: str or int
+        number of groups that will be used in `groupby` operation
+    shape: tuple
+
+    Return
+    ------
+    int
+    """
     if ASV_DATASET_SIZE == "big":
         if groupby_ngroups == "huge_amount_groups":
             return min(shape[0] // 2, 5000)
@@ -107,7 +128,30 @@ data_cache = dict()
 dataframes_cache = dict()
 
 
-def gen_int_data(nrows, ncols, rand_low, rand_high):
+def gen_int_data(nrows: int, ncols: int, rand_low: int, rand_high: int) -> dict:
+    """
+    Generate int data with caching.
+
+    The generated data are saved in the dictionary and on a subsequent call,
+    if the keys match, saved data will be returned. Therefore, we need
+    to carefully monitor the changing of saved data and make its copy if needed.
+
+    Parameters
+    ----------
+    nrows: int
+        number of rows
+    ncols: int
+        number of columns
+    rand_low: int
+        low bound for random generator
+    rand_high:int
+        high bound for random generator
+
+    Return
+    ------
+    dict
+        ncols number of keys, storing numpy.array of nrows length
+    """
     cache_key = ("int", nrows, ncols, rand_low, rand_high)
     if cache_key in data_cache:
         return data_cache[cache_key]
@@ -125,7 +169,31 @@ def gen_int_data(nrows, ncols, rand_low, rand_high):
     return data
 
 
-def gen_str_int_data(nrows, ncols, rand_low, rand_high):
+def gen_str_int_data(nrows: int, ncols: int, rand_low: int, rand_high: int) -> dict:
+    """
+    Generate int data and string data with caching.
+
+    The generated data are saved in the dictionary and on a subsequent call,
+    if the keys match, saved data will be returned. Therefore, we need
+    to carefully monitor the changing of saved data and make its copy if needed.
+
+    Parameters
+    ----------
+    nrows: int
+        number of rows
+    ncols: int
+        number of columns
+    rand_low: int
+        low bound for random generator
+    rand_high:int
+        high bound for random generator
+
+    Return
+    ------
+    dict
+        ncols+1 number of keys, storing numpy.array of nrows length
+        one column with string values
+    """
     cache_key = ("str_int", nrows, ncols, rand_low, rand_high)
     if cache_key in data_cache:
         return data_cache[cache_key]
@@ -143,7 +211,35 @@ def gen_str_int_data(nrows, ncols, rand_low, rand_high):
     return data
 
 
-def gen_data(data_type, nrows, ncols, rand_low, rand_high):
+def gen_data(
+    data_type: str, nrows: int, ncols: int, rand_low: int, rand_high: int
+) -> dict:
+    """
+    Generate data with caching.
+
+    The generated data are saved in the dictionary and on a subsequent call,
+    if the keys match, saved data will be returned. Therefore, we need
+    to carefully monitor the changing of saved data and make its copy if needed.
+
+    Parameters
+    ----------
+    data_type: str
+        type of data generation
+    nrows: int
+        number of rows
+    ncols: int
+        number of columns
+    rand_low: int
+        low bound for random generator
+    rand_high:int
+        high bound for random generator
+
+    Return
+    ------
+    dict
+        ncols+1 number of keys, storing numpy.array of nrows length
+        one column with string values
+    """
     if data_type == "int":
         return gen_int_data(nrows, ncols, rand_low, rand_high)
     elif data_type == "str_int":
@@ -153,33 +249,67 @@ def gen_data(data_type, nrows, ncols, rand_low, rand_high):
 
 
 def generate_dataframe(
-    impl,
-    data_type,
-    nrows,
-    ncols,
-    rand_low,
-    rand_high,
-    groupby_ncols=None,
-    count_groups=None,
-):
+    impl: str,
+    data_type: str,
+    nrows: int,
+    ncols: int,
+    rand_low: int,
+    rand_high: int,
+    groupby_ncols: Optional[int] = None,
+    count_groups: Optional[int] = None,
+) -> Union[pd.DataFrame, pandas.DataFrame]:
+    """
+    Generate DataFrame with caching.
+
+    The generated dataframes are saved in the dictionary and on a subsequent call,
+    if the keys match, one of the saved dataframes will be returned. Therefore, we need
+    to carefully monitor that operations that change the dataframe work with its copy.
+
+    Parameters
+    ----------
+    impl: str
+        implementation used to create the dataframe;
+        supported implemetations: {"modin", "pandas"}
+    data_type: str
+        type of data generation;
+        supported types: {"int", "str_int"}
+    nrows: int
+        number of rows
+    ncols: int
+        number of columns
+    rand_low: int
+        low bound for random generator
+    rand_high: int
+        high bound for random generator
+    groupby_ncols: int or None
+        number of columns for which `groupby` will be called in the future;
+        to get more stable performance results, we need to have the same number of values
+        in each group every benchmarking time
+    count_groups: int or None
+        count of groups in groupby columns
+
+    Return
+    ------
+    modin.DataFrame or pandas.DataFrame [and list of groupby columns names if
+        columns for groupby were generated]
+    """
     assert not (
         (groupby_ncols is None) ^ (count_groups is None)
     ), "You must either specify both parameters 'groupby_ncols' and 'count_groups' or none of them."
 
     if groupby_ncols and count_groups:
         ncols -= groupby_ncols
-        cache_key = (
-            impl,
-            data_type,
-            nrows,
-            ncols,
-            rand_low,
-            rand_high,
-            groupby_ncols,
-            count_groups,
-        )
-    else:
-        cache_key = (impl, data_type, nrows, ncols, rand_low, rand_high)
+
+    cache_key = (
+        impl,
+        data_type,
+        nrows,
+        ncols,
+        rand_low,
+        rand_high,
+        groupby_ncols,
+        count_groups,
+    )
 
     if cache_key in dataframes_cache:
         return dataframes_cache[cache_key]
@@ -211,20 +341,59 @@ def generate_dataframe(
     return df
 
 
-def random_string():
-    return str(uuid.uuid1())
+def random_string() -> str:
+    """
+    Create a 36-character random string.
+
+    Return
+    ------
+    str
+    """
+    return str(uuid.uuid4())
 
 
-def random_columns(df_columns, columns_number):
+def random_columns(df_columns: list, columns_number: int) -> list:
+    """
+    Pick sublist of random columns from a given sequence.
+
+    Parameters
+    ----------
+    df_columns: list
+        columns to choose from
+    columns_number: int
+        how many columns to pick
+
+    Return
+    ------
+    list
+    """
     return list(random_state.choice(df_columns, size=columns_number))
 
 
-def random_booleans(number):
+def random_booleans(number: int) -> list:
+    """
+    Create random list of booleans with `number` elements.
+
+    Parameters
+    ----------
+    number: int
+        count of booleans in result list
+
+    Return
+    ------
+    list
+    """
     return list(random_state.choice([True, False], size=number))
 
 
-def execute(df):
-    "Make sure the calculations are done."
+def execute(df: Union[pd.DataFrame, pandas.DataFrame]):
+    """
+    Make sure the calculations are finished.
+
+    Parameters
+    ----------
+    df: modin.DataFrame of pandas.Datarame
+    """
     if ASV_USE_IMPL == "modin":
         partitions = df._query_compiler._modin_frame._partitions
         all(
@@ -248,5 +417,16 @@ def execute(df):
         pass
 
 
-def get_shape_id(array):
-    return "_".join([str(element) for element in array])
+def get_shape_id(shape: tuple) -> str:
+    """
+    Join shape numbers into a string with `_` delimiters.
+
+    Parameters
+    ----------
+    shape: tuple
+
+    Return
+    ------
+    str
+    """
+    return "_".join([str(element) for element in shape])
