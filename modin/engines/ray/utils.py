820f0278c2ab27b72d72b9cbdea10387344f90e5
Devin Petersohn <devin.petersohn@gmail.com>
2021-06-04 10:00:26 -0500

    FIX-#3083: Workaround for Ray import thread race condition
    Resolves #3083
    
    Signed-off-by: Devin Petersohn <devin.petersohn@gmail.com>

--- modin/engines/ray/utils.py
+++ modin/engines/ray/utils.py
@@ -220,9 +220,6 @@ def initialize_ray(
         ray.worker.global_worker.run_function_on_all_workers(
             _move_stdlib_ahead_of_site_packages
         )
-
-        ray.worker.global_worker.run_function_on_all_workers(_import_pandas)
-
         if Backend.get() == "Cudf":
             from modin.engines.ray.cudf_on_ray.frame.gpu_manager import GPUManager
             from modin.engines.ray.cudf_on_ray.frame.partition_manager import (
@@ -233,7 +230,7 @@ def initialize_ray(
             if not GPU_MANAGERS:
                 for i in range(GpuCount.get()):
                     GPU_MANAGERS.append(GPUManager.remote(i))
-
+    ray.worker.global_worker.run_function_on_all_workers(_import_pandas)
     num_cpus = int(ray.cluster_resources()["CPU"])
     num_gpus = int(ray.cluster_resources().get("GPU", 0))
     if Backend.get() == "Cudf":
