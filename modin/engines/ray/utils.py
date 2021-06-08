7910104d380d65e411c7bc8c5cd17adf5a36ae36
Devin Petersohn <devin.petersohn@gmail.com>
2021-06-08 08:46:00 -0500

    FIX-#3130: Address comments
    Signed-off-by: Devin Petersohn <devin.petersohn@gmail.com>

--- modin/engines/ray/utils.py
+++ modin/engines/ray/utils.py
@@ -119,6 +119,7 @@ def initialize_ray(
                 address=redis_address or "auto",
                 include_dashboard=False,
                 ignore_reinit_error=True,
+                _redis_password=redis_password,
             )
         else:
             from modin.error_message import ErrorMessage
