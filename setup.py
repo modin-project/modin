e4e330ae8759e283b2d2ec4cb973ef4048c8cce8
Anatoly Myachev <45976948+anmyachev@users.noreply.github.com>
2021-06-30 14:09:52 +0300

    TEST-#3188: remove Dask and cloudpickle pins (#3189)
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- setup.py
+++ setup.py
@@ -37,9 +37,9 @@ class ModinDistribution(Distribution):
         return False
 
 
-dask_deps = ["dask>=2.12.0,<=2.19.0", "distributed>=2.12.0,<=2.19.0"]
+dask_deps = ["dask>=2.22.0", "distributed>=2.22.0"]
 ray_deps = ["ray[default]>=1.4.0", "pyarrow>=1.0"]
-remote_deps = ["rpyc==4.1.5", "cloudpickle==1.4.1", "boto3==1.4.8"]
+remote_deps = ["rpyc==4.1.5", "cloudpickle", "boto3==1.4.8"]
 spreadsheet_deps = ["modin-spreadsheet>=0.1.0"]
 sql_deps = ["dfsql>=0.2.6"]
 all_deps = dask_deps + ray_deps + remote_deps + spreadsheet_deps
