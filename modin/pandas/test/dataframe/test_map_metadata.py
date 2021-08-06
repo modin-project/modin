ec4dbb948dd020adc1aff8f0d61882fa85883119
Yaroslav Igoshev <yaroslav.igoshev@intel.com>
2021-08-06 09:46:27 +0300

    FIX-#3289: Update pandas to 1.3.1 (#3290)
    Co-authored-by: Vasily Litvinov <vasilij.n.litvinov@intel.com>
    Signed-off-by: Igoshev, Yaroslav <yaroslav.igoshev@intel.com>

--- modin/pandas/test/dataframe/test_map_metadata.py
+++ modin/pandas/test/dataframe/test_map_metadata.py
@@ -980,13 +980,7 @@ def test_insert(data):
     )
 
     # Bad inserts
-    with pytest.raises(ValueError):  # see pandas issue #42403
-        if len(modin_df) == len(modin_df.columns):
-            # Error won't be arisen in pandas if the frame is square-shaped.
-            # See pandas issue #42403 for more.
-            modin_df = modin_df.iloc[:, :-1]
-            pandas_df = pandas_df.iloc[:, :-1]
-        eval_insert(modin_df, pandas_df, col="Bad Column", value=lambda df: df)
+    eval_insert(modin_df, pandas_df, col="Bad Column", value=lambda df: df)
     eval_insert(
         modin_df,
         pandas_df,
