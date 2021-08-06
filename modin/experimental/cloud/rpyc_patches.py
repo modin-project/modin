e340ace227e6f6065309b21bdcb394f50b506486
Lozovskii-Aleksandr <86873279+Lozovskii-Aleksandr@users.noreply.github.com>
2021-08-06 11:06:12 +0300

    FIX-#3296: Update modin/experimental/cloud/rpyc_patches.py
    Co-authored-by: Vasily Litvinov <vasilij.n.litvinov@intel.com>
    Signed-off-by: aleksandr.lozovskii <aleksandr.lozovskii@intel.com>

--- modin/experimental/cloud/rpyc_patches.py
+++ modin/experimental/cloud/rpyc_patches.py
@@ -23,7 +23,7 @@ def apply_pathes():
                 # Note that protocol=-1 will only work between python
                 # interpreters of the same version.
                 res = netref.pickle.loads(
-                    netref.syncreq(self, netref.consts.HANDLE_PICKLE, -1)
+                    netref.syncreq(self, netref.consts.HANDLE_PICKLE, pickle.HIGHEST_PROTOCOL)
                 )
 
                 if dtype is not None:
