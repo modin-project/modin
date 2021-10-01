aee31ba6506ff0138c7908af3d108b85130b8a8c
Alexey Prutskov <alexey.prutskov@intel.com>
2021-10-01 17:00:29 +0300

    FIX-#3502: `Unpin` boto3 in setup.py (#3503)
    Signed-off-by: Alexey Prutskov <alexey.prutskov@intel.com>

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ class ModinDistribution(Distribution):
 
 dask_deps = ["dask>=2.22.0", "distributed>=2.22.0"]
 ray_deps = ["ray[default]>=1.4.0", "pyarrow>=1.0"]
-remote_deps = ["rpyc==4.1.5", "cloudpickle", "boto3==1.4.8"]
+remote_deps = ["rpyc==4.1.5", "cloudpickle", "boto3"]
 spreadsheet_deps = ["modin-spreadsheet>=0.1.0"]
 sql_deps = ["dfsql>=0.4.2"]
 all_deps = dask_deps + ray_deps + remote_deps + spreadsheet_deps
