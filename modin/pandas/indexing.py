0d6b31db9ca761758988e98cf2560925193fe44a
Naren Krishna <naren@ponder.io>
2021-10-29 11:48:42 -0700

    FIX-#3619: Fix ValueError when doing loc on empty dataframe
    Signed-off-by: Naren Krishna <naren@ponder.io>

--- modin/pandas/indexing.py
+++ modin/pandas/indexing.py
@@ -528,6 +528,8 @@ class _LocIndexer(_LocationIndexerBase):
             else:
                 result_slice = self.df.columns.slice_locs(col_loc.start, col_loc.stop)
                 return self.df.iloc[:, slice(*result_slice)]
+        if isinstance(col_loc, slice) and col_loc == slice(None):
+            return self.df._default_to_pandas(lambda df: df.loc[key])
 
         row_lookup, col_lookup = self._compute_lookup(row_loc, col_loc)
         if any(i == -1 for i in row_lookup) or any(i == -1 for i in col_lookup):
