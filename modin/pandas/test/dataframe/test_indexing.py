b0d0e8ad123c6c2ac25a5b46f845abd3a8564eda
Naren Krishna <naren@ponder.io>
2021-11-15 12:28:18 -0800

    simplify test
    Signed-off-by: Naren Krishna <naren@ponder.io>

--- modin/pandas/test/dataframe/test_indexing.py
+++ modin/pandas/test/dataframe/test_indexing.py
@@ -434,18 +434,13 @@ def test_loc_multi_index():
 
 
 def test_loc_empty():
-    pandas_df1 = pandas.DataFrame(
-        np.array([i ** 2 for i in range(5)]), columns=["TIME"]
-    )
-    pandas_df2 = pandas.DataFrame(index=pandas_df1.index)
+    pandas_df = pandas.DataFrame(index=range(5))
+    modin_df = pd.DataFrame(index=range(5))
 
-    modin_df1 = pd.DataFrame(np.array([i ** 2 for i in range(5)]), columns=["TIME"])
-    modin_df2 = pd.DataFrame(index=modin_df1.index)
-
-    df_equals(pandas_df2.loc[1], modin_df2.loc[1])
-    pandas_df2.loc[1] = 3
-    modin_df2.loc[1] = 3
-    df_equals(pandas_df2, modin_df2)
+    df_equals(pandas_df.loc[1], modin_df.loc[1])
+    pandas_df.loc[1] = 3
+    modin_df.loc[1] = 3
+    df_equals(pandas_df, modin_df)
 
 
 @pytest.mark.parametrize("index", [["row1", "row2", "row3"]])
@@ -557,18 +552,13 @@ def test_iloc_nested_assignment(data):
 
 
 def test_iloc_empty():
-    pandas_df1 = pandas.DataFrame(
-        np.array([i ** 2 for i in range(5)]), columns=["TIME"]
-    )
-    pandas_df2 = pandas.DataFrame(index=pandas_df1.index)
+    pandas_df = pandas.DataFrame(index=range(5))
+    modin_df = pd.DataFrame(index=range(5))
 
-    modin_df1 = pd.DataFrame(np.array([i ** 2 for i in range(5)]), columns=["TIME"])
-    modin_df2 = pd.DataFrame(index=modin_df1.index)
-
-    df_equals(pandas_df2.iloc[1], modin_df2.iloc[1])
-    pandas_df2.iloc[1] = 3
-    modin_df2.iloc[1] = 3
-    df_equals(pandas_df2, modin_df2)
+    df_equals(pandas_df.iloc[1], modin_df.iloc[1])
+    pandas_df.iloc[1] = 3
+    modin_df.iloc[1] = 3
+    df_equals(pandas_df, modin_df)
 
 
 def test_loc_series():
