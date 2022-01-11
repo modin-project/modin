565f4f4678201457e25beaf5ff33f5ffc86b21b7
mvashishtha <mahesh@ponder.io>
2022-01-11 09:55:34 -0600

    Fix formatting.
    Signed-off-by: mvashishtha <mahesh@ponder.io>

--- modin/pandas/dataframe.py
+++ modin/pandas/dataframe.py
@@ -34,6 +34,7 @@ import sys
 from typing import IO, Optional, Union, Mapping, Iterator
 import warnings
 
+from modin import pandas as pd
 from modin.error_message import ErrorMessage
 from modin.utils import _inherit_docstrings, to_pandas, hashable
 from modin.config import Engine, IsExperimental, PersistentPickle
@@ -2533,9 +2534,7 @@ class DataFrame(BasePandasDataset):
                 value = value.T.reshape(-1)
                 if len(self) > 0:
                     value = value[: len(self)]
-            if not isinstance(
-                value, (Series, pd.Categorical)
-            ):
+            if not isinstance(value, (Series, pd.Categorical)):
                 value = list(value)
 
         if not self._query_compiler.lazy_execution and len(self.index) == 0:
