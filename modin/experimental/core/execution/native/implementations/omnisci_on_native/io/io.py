25769c2babb9100bef89ca3f91d5877fff01886d
mvashishtha <mahesh@ponder.io>
2022-01-27 19:52:26 -0600

    Fix an omnisci bug.
    Signed-off-by: mvashishtha <mahesh@ponder.io>

--- modin/experimental/core/execution/native/implementations/omnisci_on_native/io/io.py
+++ modin/experimental/core/execution/native/implementations/omnisci_on_native/io/io.py
@@ -467,7 +467,7 @@ class OmnisciOnNativeIO(BaseIO, TextFileDispatcher):
 
         parse_dates_unsupported = isinstance(parse_dates, dict) or (
             isinstance(parse_dates, list)
-            and len(list) > 0
+            and len(parse_dates) > 0
             and all(isinstance(date, list) for date in parse_dates)
         )
         if parse_dates_unsupported:
