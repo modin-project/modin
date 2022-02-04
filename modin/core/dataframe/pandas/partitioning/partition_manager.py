FIX-#3981, FIX-#3801: Stop broadcasting scalars to set items.
Signed-off-by: mvashishtha <mahesh@ponder.io>

modin/core/dataframe/pandas/partitioning/partition_manager.py
@@ -25,6 +25,7 @@ import warnings
 
 from modin.error_message import ErrorMessage
 from modin.core.storage_formats.pandas.utils import compute_chunksize
+from modin.core.dataframe.base.dataframe.utils import no_item_to_distribute
 from modin.core.dataframe.pandas.utils import concatenate
 from modin.config import NPartitions, ProgressBar, BenchmarkMode
 
@@ -1114,7 +1115,7 @@ class PandasDataframePartitionManager(ABC):
         func,
         row_partitions_list,
         col_partitions_list,
-        item_to_distribute=None,
+        item_to_distribute=no_item_to_distribute,
         row_lengths=None,
         col_widths=None,
     ):
@@ -1135,7 +1136,7 @@ class PandasDataframePartitionManager(ABC):
             Iterable of tuples, containing 2 values:
                 1. Integer column partition index.
                 2. Internal column indexer of this partition.
-        item_to_distribute : item, default: None
+        item_to_distribute : np.ndarray, Sentinel, or scalar, default: no_item_to_distribute
             The item to split up so it can be applied over both axes.
         row_lengths : list of ints, optional
             Lengths of partitions for every row. If not specified this information
@@ -1190,16 +1191,7 @@ class PandasDataframePartitionManager(ABC):
                     col_internal_idx, remote_part, col_idx, axis=1
                 )
 
-                # We want to eventually make item_to_distribute an np.ndarray,
-                # but that doesn't work for setting a subset of a categorical
-                # column, as per https://github.com/modin-project/modin/issues/3736.
-                # In that case, `item` is not an ndarray but instead some
-                # categorical variable, which we we don't need to distribute
-                # at all. Note that np.ndarray is not hashable, so it can't
-                # be a categorical variable.
-                # TODO(https://github.com/pandas-dev/pandas/issues/44703): Delete
-                # this special case once the pandas bug is fixed.
-                if item_to_distribute is not None:
+                if item_to_distribute is not no_item_to_distribute:
                     if isinstance(item_to_distribute, np.ndarray):
                         item = item_to_distribute[
                             row_position_counter : row_position_counter + row_offset,

modin/core/execution/ray/implementations/pandas_on_ray/partitioning/partition_manager.py
@@ -18,6 +18,7 @@ import numpy as np
 import threading
 
 from modin.config import ProgressBar, NPartitions
+from modin.core.dataframe.base.dataframe.utils import no_item_to_distribute
 from modin.core.execution.ray.generic.partitioning.partition_manager import (
     GenericRayDataframePartitionManager,
 )
@@ -550,7 +551,7 @@ class PandasOnRayDataframePartitionManager(GenericRayDataframePartitionManager):
         func,
         row_partitions_list,
         col_partitions_list,
-        item_to_distribute=None,
+        item_to_distribute=no_item_to_distribute,
         row_lengths=None,
         col_widths=None,
     ):
@@ -567,7 +568,7 @@ class PandasOnRayDataframePartitionManager(GenericRayDataframePartitionManager):
             List of row partitions.
         col_partitions_list : list
             List of column partitions.
-        item_to_distribute : item, optional
+        item_to_distribute : np.ndarray, Sentinel, or scalar, default: no_item_to_distribute
             The item to split up so it can be applied over both axes.
         row_lengths : list of ints, optional
             Lengths of partitions for every row. If not specified this information