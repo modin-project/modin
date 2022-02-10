d161847bc9d8bf1107937dc192758d943799c7a0
mvashishtha <mahesh@ponder.io>
2022-02-10 17:53:43 -0600

    FEAT-#979: Allow read_sql from SQL server.
    Signed-off-by: mvashishtha <mahesh@ponder.io>

--- modin/db_conn.py
+++ modin/db_conn.py
@@ -25,9 +25,15 @@ driver or a worker wants one.
 
 _PSYCOPG_LIB_NAME = "psycopg2"
 _SQLALCHEMY_LIB_NAME = "sqlalchemy"
+_POSTGRES_DIALECT = "postgres"
+_MICROSOFT_SQL_DIALECT = "microsoft_sql"
 
 
 class UnsupportedDatabaseException(Exception):
+    pass
+
+
+class UnsupportedSqlDialectException(Exception):
     pass
 
 
@@ -45,11 +51,17 @@ class ModinDatabaseConnection:
         Keyword arguments to pass when creating the connection.
     """
 
-    def __init__(self, lib, *args, **kwargs):
+    def __init__(self, lib, modin_sql_dialect=_POSTGRES_DIALECT, *args, **kwargs):
         lib = lib.lower()
         if lib not in (_PSYCOPG_LIB_NAME, _SQLALCHEMY_LIB_NAME):
             raise UnsupportedDatabaseException(f"Unsupported database library {lib}")
         self.lib = lib
+        modin_sql_dialect = modin_sql_dialect.lower()
+        if modin_sql_dialect not in (_POSTGRES_DIALECT, _MICROSOFT_SQL_DIALECT):
+            raise UnsupportedSqlDialectException(
+                f"Unsupported sql dialect {modin_sql_dialect}"
+            )
+        self.modin_sql_dialect = modin_sql_dialect
         self.args = args
         self.kwargs = kwargs
 
@@ -76,4 +88,22 @@ class ModinDatabaseConnection:
 
             return create_engine(*self.args, **self.kwargs).connect()
 
-        raise Exception("Unsupported database library")
+        raise UnsupportedDatabaseException("Unsupported database library")
+
+    def column_names_query(self, query):
+        # This query looks odd, but it works in both PostgreSQL and Microsoft
+        # SQL, which doesn't let you use a "limit" clause to select 0 rows.
+        return f"SELECT * FROM ({query}) AS _ WHERE 1 = 0"
+
+    def row_count_query(self, query):
+        return f"SELECT COUNT(*) FROM ({query}) AS _"
+
+    def partition_query(self, query, limit, offset):
+        return (
+            f"SELECT * FROM ({query}) LIMIT {limit} OFFSET {offset}"
+            if self.modin_sql_dialect == _POSTGRES_DIALECT
+            else (
+                f"SELECT * FROM ({query}) AS _ ORDER BY(SELECT NULL)"
+                f" OFFSET {offset} ROWS FETCH NEXT {limit} ROWS ONLY"
+            )
+        )
