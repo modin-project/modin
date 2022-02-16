7bfd447126b21f6acd71d3ce73653711003521ed
jeffreykennethli <jkli@ponder.io>
2022-02-16 17:36:11 -0500

    Rename file headers
    Signed-off-by: jeffreykennethli <jkli@ponder.io>

--- modin/pandas/window.py
+++ modin/pandas/window.py
@@ -11,7 +11,7 @@
 # ANY KIND, either express or implied. See the License for the specific language
 # governing permissions and limitations under the License.
 
-"""Implement DataFrame/Series public API as pandas does."""
+"""Implement Window public API."""
 
 import pandas
 import pandas.core.window.rolling
