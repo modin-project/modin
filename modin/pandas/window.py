ff4bcf8329130cff4eed060a5330e0c11b053c3c
jeffreykennethli <jkli@ponder.io>
2022-02-17 08:54:54 -0500

    Update file header
    Signed-off-by: jeffreykennethli <jkli@ponder.io>

--- modin/pandas/window.py
+++ modin/pandas/window.py
@@ -11,7 +11,7 @@
 # ANY KIND, either express or implied. See the License for the specific language
 # governing permissions and limitations under the License.
 
-"""Implement Window public API."""
+"""Implement Window and Rolling public API."""
 
 from typing import Optional
 import pandas.core.resample
