0279ff572b16ce77eab42854571ed3fa393b0d90
mvashishtha <mahesh@ponder.io>
2022-02-22 18:24:01 -0800

    Try using apt-get instead of apt on windows.
    Also use 127.0.0.1 instead of localhost on ubuntu. Add
    docker debugging to ubuntu.
    
    Signed-off-by: mvashishtha <mahesh@ponder.io>

--- set_up_windows_for_sql_server.sh
+++ set_up_windows_for_sql_server.sh
@@ -2,9 +2,9 @@ set -e
 
 # https://dev.to/bowmanjd/install-docker-on-windows-wsl-without-docker-desktop-34m9
 
-apt update && apt install sudo
+apt-get update && apt-get install sudo
 echo "installed sudo"
-sudo apt install --no-install-recommends apt-transport-https ca-certificates curl gnupg2
+sudo apt-get install --no-install-recommends apt-transport-https ca-certificates curl gnupg2
 echo "installed prereqs"
 source /etc/os-release
 ehcho "ran source"
@@ -12,7 +12,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 echo "ran curl"
 echo "deb [arch=amd64] https://download.docker.com/linux/${ID} ${VERSION_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list
 echo "ran echo"
-sudo apt update
+sudo apt-get update
 echo "ran apt update"
-sudo apt install docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 echo "installed docker!"
