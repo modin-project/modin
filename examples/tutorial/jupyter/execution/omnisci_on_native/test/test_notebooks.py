0a01344fef819e752f7269c2220ce9642374c099
Maria Rubtsova <maria.rubtsova@intel.com>
2022-02-25 23:19:35 +0300

    change tests & ci-tests

--- examples/tutorial/jupyter/execution/omnisci_on_native/test/test_notebooks.py
+++ examples/tutorial/jupyter/execution/omnisci_on_native/test/test_notebooks.py
@@ -11,95 +11,35 @@
 # ANY KIND, either express or implied. See the License for the specific language
 # governing permissions and limitations under the License.
 
+import os
+import sys
+
 import nbformat
-from nbconvert.preprocessors import ExecutePreprocessor
 
-test_dataset_path = "taxi.csv"
-ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
+MODIN_DIR = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), *[".." for _ in range(6)])
+)
+sys.path.insert(0, MODIN_DIR)
+from examples.tutorial.jupyter.execution.test.utils import (  # noqa: E402
+    _replace_str,
+    _execute_notebook,
+    _find_code_cell_idx,
+    test_dataset_path,
+    download_taxi_dataset,
+)
 
-download_taxi_dataset = f"""import os
-import urllib.request
-if not os.path.exists("{test_dataset_path}"):
-    url_path = "https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-01.csv"
-    urllib.request.urlretrieve(url_path, "{test_dataset_path}")
-    """
-
-
-def _execute_notebook(notebook):
-    """
-    Execute a jupyter notebook.
-
-    Parameters
-    ----------
-    notebook : file-like or str
-        File-like object or path to the notebook to execute.
-    """
-    nb = nbformat.read(notebook, as_version=nbformat.NO_CONVERT)
-    ep.preprocess(nb)
-
-
-def _find_code_cell_idx(nb, identifier):
-    """
-    Find code cell index by provided ``identifier``.
-
-    Parameters
-    ----------
-    nb : dict
-        Dictionary representation of the notebook to look for.
-    identifier : str
-        Unique string which target code cell should contain.
-
-    Returns
-    -------
-    int
-        Code cell index by provided ``identifier``.
-
-    Notes
-    -----
-    Assertion will be raised if ``identifier`` is found in
-    several code cells or isn't found at all.
-    """
-    import_cell_idx = [
-        idx
-        for idx, cell in enumerate(nb["cells"])
-        if cell["cell_type"] == "code" and identifier in cell["source"]
-    ]
-    assert len(import_cell_idx) == 1
-    return import_cell_idx[0]
-
-
-def _replace_str(nb, original_str, str_to_replace):
-    """
-    Replace ``original_str`` with ``str_to_replace`` in the provided notebook.
-
-    Parameters
-    ----------
-    nb : dict
-        Dictionary representation of the notebook which requires replacement.
-    original_str : str
-        Original string which should be replaced.
-    str_to_replace : str
-        String to replace original string.
-
-    Notes
-    -----
-    Assertion will be raised if ``original_str`` is found in
-    several code cells or isn't found at all.
-    """
-    import_cell_idx = _find_code_cell_idx(nb, original_str)
-    nb["cells"][import_cell_idx]["source"] = nb["cells"][import_cell_idx][
-        "source"
-    ].replace(original_str, str_to_replace)
+local_notebooks_dir = "examples/tutorial/jupyter/execution/omnisci_on_native/local"
 
 
 # in this notebook user should replace 'import pandas as pd' with
 # 'import modin.pandas as pd' to make notebook work
 def test_exercise_1():
-    modified_notebook_path = "examples/tutorial/jupyter/execution/omnisci_on_native/local/exercise_1_test.ipynb"
+    modified_notebook_path = os.path.join(local_notebooks_dir, "exercise_1_test.ipynb")
     nb = nbformat.read(
-        "examples/tutorial/jupyter/execution/omnisci_on_native/local/exercise_1.ipynb",
+        os.path.join(local_notebooks_dir, "exercise_1.ipynb"),
         as_version=nbformat.NO_CONVERT,
     )
+
     _replace_str(nb, "import pandas as pd", "import modin.pandas as pd")
 
     nbformat.write(nb, modified_notebook_path)
@@ -108,9 +48,9 @@ def test_exercise_1():
 
 # this notebook works "as is" but for testing purposes we can use smaller dataset
 def test_exercise_2():
-    modified_notebook_path = "examples/tutorial/jupyter/execution/omnisci_on_native/local/exercise_2_test.ipynb"
+    modified_notebook_path = os.path.join(local_notebooks_dir, "exercise_2_test.ipynb")
     nb = nbformat.read(
-        "examples/tutorial/jupyter/execution/omnisci_on_native/local/exercise_2.ipynb",
+        os.path.join(local_notebooks_dir, "exercise_2.ipynb"),
         as_version=nbformat.NO_CONVERT,
     )
 
@@ -125,9 +65,9 @@ def test_exercise_2():
 
 # this notebook works "as is"
 def test_exercise_3():
-    modified_notebook_path = "examples/tutorial/jupyter/execution/omnisci_on_native/local/exercise_3_test.ipynb"
+    modified_notebook_path = os.path.join(local_notebooks_dir, "exercise_3_test.ipynb")
     nb = nbformat.read(
-        "examples/tutorial/jupyter/execution/omnisci_on_native/local/exercise_3.ipynb",
+        os.path.join(local_notebooks_dir, "exercise_3.ipynb"),
         as_version=nbformat.NO_CONVERT,
     )
 
