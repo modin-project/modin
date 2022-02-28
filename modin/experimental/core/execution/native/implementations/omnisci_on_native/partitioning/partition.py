1fa6c3aa2906eeabd5d84cae478103134276b888
Anatoly Myachev <anatoly.myachev@intel.com>
2022-02-28 15:00:41 +0300

    remove inheritance
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- modin/experimental/core/execution/native/implementations/omnisci_on_native/partitioning/partition.py
+++ modin/experimental/core/execution/native/implementations/omnisci_on_native/partitioning/partition.py
@@ -15,12 +15,11 @@
 
 import pandas
 
-from modin.core.dataframe.pandas.partitioning.partition import PandasDataframePartition
 from ..omnisci_worker import OmnisciServer
 import pyarrow
 
 
-class OmnisciOnNativeDataframePartition(PandasDataframePartition):
+class OmnisciOnNativeDataframePartition:
     """
     A partition of ``OmnisciOnNativeDataframe`` frame.
 
@@ -61,8 +60,9 @@ class OmnisciOnNativeDataframePartition(PandasDataframePartition):
         self.pandas_df = pandas_df
         self.frame_id = frame_id
         self.arrow_table = arrow_table
+        self._length_cache = length
+        self._width_cache = width
         self._server = OmnisciServer
-        super().__init__(length, width)
 
     def __del__(self):
         """Deallocate OmniSci resources related to the partition."""
