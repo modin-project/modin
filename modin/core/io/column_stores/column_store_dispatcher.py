1760578bc873cf2680198299d98713585d7f8242
mvashishtha <mahesh@ponder.io>
2022-04-12 16:56:27 -0500

    Fix an indent.
    Signed-off-by: mvashishtha <mahesh@ponder.io>

--- modin/core/io/column_stores/column_store_dispatcher.py
+++ modin/core/io/column_stores/column_store_dispatcher.py
@@ -251,7 +251,6 @@ class ColumnStoreDispatcher(FileDispatcher):
         partition_ids = cls.call_deploy(path, col_partitions, **kwargs)
         index, needs_index_sync = cls.build_index(path)
         remote_parts = cls.build_partition(partition_ids, column_widths)
-
         dtypes = (
             cls.build_dtypes(partition_ids[0], columns)
             if len(partition_ids) > 0
@@ -266,6 +265,7 @@ class ColumnStoreDispatcher(FileDispatcher):
             column_widths=column_widths,
             dtypes=dtypes,
         )
+        new_query_compiler = cls.query_compiler_cls(frame)
         new_query_compiler = cls.query_compiler_cls(frame)
         new_query_compiler = cls.query_compiler_cls(frame)
         if needs_index_sync:
