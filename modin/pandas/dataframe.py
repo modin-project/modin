9f7f1c3d5436c622abe2b86bc904efa6d6640d67
Anatoly Myachev <anatoliimyachev@mail.com>
2022-04-22 14:27:35 +0300

    refactoring
    Signed-off-by: Anatoly Myachev <anatoliimyachev@mail.com>

--- modin/pandas/dataframe.py
+++ modin/pandas/dataframe.py
@@ -1136,14 +1136,6 @@ class DataFrame(BasePandasDataset):
                     f"Expected a 1D array, got an array with shape {value.shape}"
                 )
             value = value.squeeze(axis=1)
-        elif isinstance(value, np.ndarray) and len(value.shape) > 1:
-            if value.shape[1] == 1:
-                # Transform into columnar table and take first column
-                value = value.squeeze(axis=1)
-            else:
-                raise ValueError(
-                    f"Expected a 1D array, got an array with shape {value.shape}"
-                )
         if not self._query_compiler.lazy_execution and len(self.index) == 0:
             if not hasattr(value, "index"):
                 try:
