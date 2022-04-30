d8ce0a1fb2980ca699f1108021e9cebd0b3e4448
Naren Krishna <naren@ponder.io>
2022-04-30 08:59:54 -0700

    fix flake8
    Signed-off-by: Naren Krishna <naren@ponder.io>

--- modin/logging/__init__.py
+++ modin/logging/__init__.py
@@ -1,3 +1,3 @@
-from .logger_metaclass import LoggerMetaClass, logger_class_wrapper  # flake8: noqa
-from .config import get_logger  # flake8: noqa
-from .logger_function import logger_decorator  # flake8: noqa
+from .logger_metaclass import LoggerMetaClass, logger_class_wrapper  # noqa: F401
+from .config import get_logger  # noqa: F401
+from .logger_function import logger_decorator  # noqa: F401
