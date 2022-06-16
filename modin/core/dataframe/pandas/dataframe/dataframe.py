8f35ab57996d18f2c1f001d624682952853454cd
Alexey Prutskov <lehaprutskov@gmail.com>
2022-06-16 16:48:46 +0300

    PERF-#4182, FIX-#4059: Add cell-wise execution for binary ops, fix bin ops for empty dataframes (#4391)
    Signed-off-by: Alexey Prutskov <lehaprutskov@gmail.com>

--- modin/core/dataframe/pandas/dataframe/dataframe.py
+++ modin/core/dataframe/pandas/dataframe/dataframe.py
@@ -2507,18 +2507,38 @@ class PandasDataframe(object, metaclass=LoggerMetaClass):
         left_parts, right_parts, joined_index, row_lengths = self._copartition(
             0, right_frame, join_type, sort=True
         )
-        # unwrap list returned by `copartition`.
-        right_parts = right_parts[0]
-        new_frame = self._partition_mgr_cls.binary_operation(
-            1, left_parts, lambda l, r: op(l, r), right_parts
+        new_left_frame = self.__constructor__(
+            left_parts, joined_index, self.columns, row_lengths, self._column_widths
         )
-        new_columns = self.columns.join(right_frame.columns, how=join_type)
+        new_right_frame = self.__constructor__(
+            right_parts[0],
+            joined_index,
+            right_frame.columns,
+            row_lengths,
+            right_frame._column_widths,
+        )
+
+        (
+            left_parts,
+            right_parts,
+            joined_columns,
+            column_widths,
+        ) = new_left_frame._copartition(1, new_right_frame, join_type, sort=True)
+
+        new_frame = (
+            np.array([])
+            if len(left_parts) == 0 or len(right_parts[0]) == 0
+            else self._partition_mgr_cls.binary_operation(
+                left_parts, op, right_parts[0]
+            )
+        )
+
         return self.__constructor__(
             new_frame,
             joined_index,
-            new_columns,
+            joined_columns,
             row_lengths,
-            column_widths=self._column_widths_cache,
+            column_widths,
         )
 
     @lazy_metadata_decorator(apply_axis="both")
