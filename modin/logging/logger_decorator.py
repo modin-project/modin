6ee90b2b37fe1d833dc65e0d6d418c5e7db136ea
Karthik Velayutham <vkarthik@ponder.io>
2022-06-30 10:55:04 -0500

    Fix CI workflow, lint issue

--- modin/logging/logger_decorator.py
+++ modin/logging/logger_decorator.py
@@ -103,10 +103,7 @@ def enable_logging(
         elif isinstance(obj, staticmethod):
             return staticmethod(decorator(obj.__func__))
 
-        assert isinstance(modin_layer, str), (
-            "obj is not a type or class/static method "
-            "and modin_layer is somehow not a string!"
-        )
+        assert isinstance(modin_layer, str), "modin_layer is somehow not a string!"
 
         start_line = f"START::{modin_layer.upper()}::{name or obj.__name__}"
         stop_line = f"STOP::{modin_layer.upper()}::{name or obj.__name__}"
