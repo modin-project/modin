38e839e1bcf5a083d31c01cb4609efb8a0437ca3
Karthik Velayutham <vkarthik@ponder.io>
2022-07-12 12:48:13 -0500

    FIX-#4634: Check for FrozenList as `by` in `df.groupby()`
    Signed-off-by: Karthik Velayutham <vkarthik@ponder.io>

--- modin/pandas/dataframe.py
+++ modin/pandas/dataframe.py
@@ -21,6 +21,7 @@ from pandas.core.dtypes.common import (
     is_list_like,
     is_numeric_dtype,
 )
+from pandas.core.indexes.frozen import FrozenList
 from pandas.util._validators import validate_bool_kwarg
 from pandas.io.formats.printing import pprint_thing
 from pandas._libs.lib import no_default
@@ -434,7 +435,7 @@ class DataFrame(BasePandasDataset):
 
         if callable(by):
             by = self.index.map(by)
-        elif hashable(by) and not isinstance(by, pandas.Grouper):
+        elif hashable(by) and not isinstance(by, (pandas.Grouper, FrozenList)):
             drop = by in self.columns
             idx_name = by
             if by is not None and by in self._query_compiler.get_index_names(axis):
@@ -3015,6 +3016,7 @@ class DataFrame(BasePandasDataset):
         elif isinstance(key, DataFrame):
             return self.where(key)
         elif is_mi_columns:
+            print("we are here???")
             return self._default_to_pandas(pandas.DataFrame.__getitem__, key)
             # return self._getitem_multilevel(key)
         else:
