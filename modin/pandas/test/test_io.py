3504a6312d3d2068724b04e5cca90748c3faa607
Karthik Velayutham <vkarthik@ponder.io>
2022-08-19 17:02:13 -0500

    Remove redundant test

--- modin/pandas/test/test_io.py
+++ modin/pandas/test/test_io.py
@@ -1646,26 +1646,6 @@ class TestParquet:
 
             df_equals(test_df, read_df)
 
-    @pytest.mark.xfail(
-        condition="config.getoption('--simulate-cloud').lower() != 'off'",
-        reason="The reason of tests fail in `cloud` mode is unknown for now - issue #3264",
-    )
-    def test_read_parquet_glob_path(self):
-        test_dfs = [
-            pandas.DataFrame(np.arange(1, 16).reshape(3, 5)).add_prefix("col")
-            for _ in range(5)
-        ]
-        pandas_df = pd.concat(test_dfs)
-
-        with tempfile.TemporaryDirectory() as directory:
-            for idx, test_df in enumerate(test_dfs):
-                test_df.to_parquet(f"{directory}/{idx}.parquet")
-
-            path = Path(directory)
-            files = list(path.glob("**/*.parquet"))
-            modin_df = pd.concat([pd.read_parquet(f) for f in files])
-            df_equals(pandas_df, modin_df)
-
 
 class TestJson:
     @pytest.mark.parametrize("lines", [False, True])
