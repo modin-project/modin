74a02b975dd31c75f54cba74b706e296a22a93cb
Jonathan Shi <jhshi@ponder.io>
2022-08-24 11:58:38 -0700

    Add partition assert for iloc_partitioned_types
    Signed-off-by: Jonathan Shi <jhshi@ponder.io>

--- modin/pandas/test/dataframe/test_indexing.py
+++ modin/pandas/test/dataframe/test_indexing.py
@@ -264,6 +264,7 @@ def test_iloc_partitioned_dtypes():
     # See issue #4675
     modin_df = pd.concat([pd.DataFrame(["a"]), pd.DataFrame([2])])
     pandas_df = pandas.concat([pandas.DataFrame(["a"]), pandas.DataFrame([2])])
+    assert modin_df._query_compiler._modin_frame._partitions.shape == (2, 1)
     modin_result = modin_df.iloc[1:].dtypes
     pandas_result = pandas_df.iloc[1:].dtypes
     assert modin_result.equals(pandas_result)
