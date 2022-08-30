70b46a8db6466ab10aeb4640e8d09645b19e56ae
Karthik Velayutham <vkarthik@ponder.io>
2022-08-30 11:21:05 -0500

    Add gs test to read_csv_glob

--- modin/experimental/pandas/test/test_io_exp.py
+++ modin/experimental/pandas/test/test_io_exp.py
@@ -167,8 +167,11 @@ class TestCsvGlob:
 @pytest.mark.skipif(
     Engine.get() != "Ray", reason="Currently only support Ray engine for glob paths."
 )
-def test_read_multiple_csv_s3():
-    path = "s3://modin-datasets/testing/multiple_csv/test_data*.csv"
+def test_read_multiple_csv_cloud_store():
+    paths = [
+        "s3://modin-datasets/testing/multiple_csv/test_data*.csv",
+        "gs://modin-testing/testing/multiple_csv/test_data*.csv",
+    ]
 
     def _pandas_read_csv_glob(path, storage_options):
         pandas_dfs = [
@@ -179,14 +182,17 @@ def test_read_multiple_csv_s3():
         ]
         return pandas.concat(pandas_dfs).reset_index(drop=True)
 
-    eval_general(
-        pd,
-        pandas,
-        lambda module, **kwargs: pd.read_csv_glob(path, **kwargs).reset_index(drop=True)
-        if hasattr(module, "read_csv_glob")
-        else _pandas_read_csv_glob(path, **kwargs),
-        storage_options={"anon": True},
-    )
+    for path in paths:
+        eval_general(
+            pd,
+            pandas,
+            lambda module, **kwargs: pd.read_csv_glob(path, **kwargs).reset_index(
+                drop=True
+            )
+            if hasattr(module, "read_csv_glob")
+            else _pandas_read_csv_glob(path, **kwargs),
+            storage_options={"anon": True},
+        )
 
 
 test_default_to_pickle_filename = "test_default_to_pickle.pkl"
