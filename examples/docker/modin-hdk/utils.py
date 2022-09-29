b18eee0bb97e2921192a77168e4130a566f68195
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2022-09-29 20:15:27 +0200

    TEST-#5102: Add HDK benchmarks to github workflows
    Signed-off-by: Andrey Pavlenko <andrey.a.pavlenko@gmail.com>

---
+++ examples/docker/modin-hdk/utils.py
@@ -0,0 +1,28 @@
+# Licensed to Modin Development Team under one or more contributor license agreements.
+# See the NOTICE file distributed with this work for additional information regarding
+# copyright ownership.  The Modin Development Team licenses this file to you under the
+# Apache License, Version 2.0 (the "License"); you may not use this file except in
+# compliance with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific language
+# governing permissions and limitations under the License.
+
+import sys
+import time
+from os.path import dirname, realpath
+
+MODIN_DIR = dirname(dirname(dirname(dirname(realpath(__file__)))))
+if MODIN_DIR not in sys.path:
+    sys.path.insert(0, MODIN_DIR)
+
+
+def measure(name, func, *args, **kw):
+    t0 = time.time()
+    res = func(*args, **kw)
+    t1 = time.time()
+    print(f"{name}: {t1 - t0} sec")
+    return res
