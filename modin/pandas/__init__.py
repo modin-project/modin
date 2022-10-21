079d6447f5994149bbf391c174792cdf2eedde2d
Igoshev, Iaroslav <iaroslav.igoshev@intel.com>
2022-10-21 09:32:25 +0200

    FEAT-#5145: Upgrade pandas to 1.5.1
    Signed-off-by: Igoshev, Iaroslav <iaroslav.igoshev@intel.com>

--- modin/pandas/__init__.py
+++ modin/pandas/__init__.py
@@ -30,7 +30,7 @@ if PandasCompatVersion.CURRENT == PandasCompatVersion.PY36:
             f"Starting Modin in compatibility mode to support legacy pandas version {__pandas_version__}"
         )
 elif PandasCompatVersion.CURRENT == PandasCompatVersion.LATEST:
-    __pandas_version__ = "1.5.0"
+    __pandas_version__ = "1.5.1"
 
     if pandas.__version__ != __pandas_version__:
         warnings.warn(
