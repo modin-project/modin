2f3cf0e70a8e6a1e9ec5ec39367840f3b6f125bc
Bill Wang <billiam@ponder.io>
2022-10-31 09:22:12 -0700

    Reset default InitializeWithSmallQueryCompilers envvar to False
    Signed-off-by: Bill Wang <billiam@ponder.io>

--- modin/config/envvars.py
+++ modin/config/envvars.py
@@ -596,7 +596,7 @@ class InitializeWithSmallQueryCompilers(EnvironmentVariable, type=str):
     """Set to true to use implementation of SmallQueryCompiler."""
 
     varname = "MODIN_SMALL_QUERY_COMPILER"
-    default = True
+    default = False
 
 
 def _check_vars() -> None:
