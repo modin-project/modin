7f508b7be1fe0b983cdd122b0849a681bea99384
arunjose696@gmail.com <arunjose@ansatclx1005.an.intel.com>
2022-11-23 05:16:19 -0600

    Have added classes Reindex, ReindexMethod, Fillna, LevelAlign, DropDuplicates, Removed NonNumericSeriesIndexing to stay aligned with the source PR.

--- asv_bench/benchmarks/benchmarks.py
+++ asv_bench/benchmarks/benchmarks.py
@@ -41,7 +41,9 @@ from .utils import (
 
 from modin.pandas import (
     Series,
-    Index
+    Index,
+    DataFrame,
+    MultiIndex
     
 )
 
@@ -855,83 +857,189 @@ class NumericSeriesIndexing:
         self.array_list = self.array.tolist()
 
     def time_getitem_scalar(self, index, index_structure):
-        self.data[800000]
+        execute(self.data[800000])
 
     def time_getitem_slice(self, index, index_structure):
-        self.data[:800000]
+        execute(self.data[:800000])
 
     def time_getitem_list_like(self, index, index_structure):
-        self.data[[800000]]
+        execute(self.data[[800000]])
 
     def time_getitem_array(self, index, index_structure):
-        self.data[self.array]
+        execute(self.data[self.array])
 
     def time_getitem_lists(self, index, index_structure):
-        self.data[self.array_list]
+        execute(self.data[self.array_list])
 
     def time_iloc_array(self, index, index_structure):
-        self.data.iloc[self.array]
+        execute(self.data.iloc[self.array])
 
     def time_iloc_list_like(self, index, index_structure):
-        self.data.iloc[[800000]]
+        execute(self.data.iloc[[800000]])
 
     def time_iloc_scalar(self, index, index_structure):
-        self.data.iloc[800000]
+        execute(self.data.iloc[800000])
 
     def time_iloc_slice(self, index, index_structure):
-        self.data.iloc[:800000]
+        execute(self.data.iloc[:800000])
 
     def time_loc_array(self, index, index_structure):
-        self.data.loc[self.array]
+        execute(self.data.loc[self.array])
 
     def time_loc_list_like(self, index, index_structure):
-        self.data.loc[[800000]]
+        execute(self.data.loc[[800000]])
 
     def time_loc_scalar(self, index, index_structure):
-        self.data.loc[800000]
+        execute(self.data.loc[800000])
 
     def time_loc_slice(self, index, index_structure):
-        self.data.loc[:800000]
-
-class NonNumericSeriesIndexing:
-
-    params = [
-        ("string", "datetime", "period"),
-        ("unique_monotonic_inc", "nonunique_monotonic_inc", "non_monotonic"),
-    ]
-    param_names = ["index_dtype", "index_structure"]
-
-    def setup(self, index, index_structure):
-        N = 10**6
-        if index == "string":
-            index = tm.makeStringIndex(N)
-        elif index == "datetime":
-            index = date_range("1900", periods=N, freq="s")
-        elif index == "period":
-            index = period_range("1900", periods=N, freq="s")
-        index = index.sort_values()
-        assert index.is_unique and index.is_monotonic_increasing
-        if index_structure == "nonunique_monotonic_inc":
-            index = index.insert(item=index[2], loc=2)[:-1]
-        elif index_structure == "non_monotonic":
-            index = index[::2].append(index[1::2])
-            assert len(index) == N
-        self.s = Series(np.random.rand(N), index=index)
-        self.lbl = index[80000]
-        # warm up index mapping
-        self.s[self.lbl]
-
-    def time_getitem_label_slice(self, index, index_structure):
-        self.s[: self.lbl]
-
-    def time_getitem_pos_slice(self, index, index_structure):
-        self.s[:80000]
-
-    def time_getitem_scalar(self, index, index_structure):
-        self.s[self.lbl]
-
-    def time_getitem_list_like(self, index, index_structure):
-        self.s[[self.lbl]]
+        execute(self.data.loc[:800000])
 
 
+class Reindex:
+    def setup(self):
+        rng = date_range(start="1/1/1970", periods=10000, freq="1min")
+        self.df = DataFrame(np.random.rand(10000, 10), index=rng, columns=range(10))
+        self.df["foo"] = "bar"
+        self.rng_subset = Index(rng[::2])
+        self.df2 = DataFrame(
+            index=range(10000), data=np.random.rand(10000, 30), columns=range(30)
+        )
+        N = 5000
+        K = 200
+        level1 = tm.makeStringIndex(N).values.repeat(K)
+        level2 = np.tile(tm.makeStringIndex(K).values, N)
+        index = MultiIndex.from_arrays([level1, level2])
+        self.s = Series(np.random.randn(N * K), index=index)
+        self.s_subset = self.s[::2]
+        self.s_subset_no_cache = self.s[::2].copy()
 
+        mi = MultiIndex.from_product([rng, range(100)])
+        self.s2 = Series(np.random.randn(len(mi)), index=mi)
+        self.s2_subset = self.s2[::2].copy()
+
+    def time_reindex_dates(self):
+        execute(self.df.reindex(self.rng_subset))
+
+    def time_reindex_columns(self):
+        execute(self.df2.reindex(columns=self.df.columns[1:5]))
+
+    def time_reindex_multiindex_with_cache(self):
+        # MultiIndex._values gets cached
+        execute(self.s.reindex(self.s_subset.index))
+
+    def time_reindex_multiindex_no_cache(self):
+        # Copy to avoid MultiIndex._values getting cached
+        execute(self.s.reindex(self.s_subset_no_cache.index.copy()))
+
+    def time_reindex_multiindex_no_cache_dates(self):
+        # Copy to avoid MultiIndex._values getting cached
+        execute(self.s2_subset.reindex(self.s2.index.copy()))
+
+class ReindexMethod:
+
+    params = [["pad", "backfill"], [date_range, period_range]]
+    param_names = ["method", "constructor"]
+
+    def setup(self, method, constructor):
+        N = 100000
+        self.idx = constructor("1/1/2000", periods=N, freq="1min")
+        self.ts = Series(np.random.randn(N), index=self.idx)[::2]
+
+    def time_reindex_method(self, method, constructor):
+        execute(self.ts.reindex(self.idx, method=method))
+
+class Fillna:
+
+    params = ["pad", "backfill"]
+    param_names = ["method"]
+
+    def setup(self, method):
+        N = 100000
+        self.idx = date_range("1/1/2000", periods=N, freq="1min")
+        ts = Series(np.random.randn(N), index=self.idx)[::2]
+        self.ts_reindexed = ts.reindex(self.idx)
+        self.ts_float32 = self.ts_reindexed.astype("float32")
+
+    def time_reindexed(self, method):
+        execute(self.ts_reindexed.fillna(method=method))
+
+    def time_float_32(self, method):
+        execute(self.ts_float32.fillna(method=method))
+        
+class LevelAlign:
+    def setup(self):
+        self.index = MultiIndex(
+            levels=[np.arange(10), np.arange(100), np.arange(100)],
+            codes=[
+                np.arange(10).repeat(10000),
+                np.tile(np.arange(100).repeat(100), 10),
+                np.tile(np.tile(np.arange(100), 100), 10),
+            ],
+        )
+        self.df = DataFrame(np.random.randn(len(self.index), 4), index=self.index)
+        self.df_level = DataFrame(np.random.randn(100, 4), index=self.index.levels[1])
+
+    def time_align_level(self):
+        execute(self.df.align(self.df_level, level=1, copy=False))
+
+    def time_reindex_level(self):
+        execute(self.df_level.reindex(self.index, level=1))
+
+class DropDuplicates:
+
+    params = [True, False]
+    param_names = ["inplace"]
+
+    def setup(self, inplace):
+        N = 10000
+        K = 10
+        key1 = tm.makeStringIndex(N).values.repeat(K)
+        key2 = tm.makeStringIndex(N).values.repeat(K)
+        self.df = DataFrame(
+            {"key1": key1, "key2": key2, "value": np.random.randn(N * K)}
+        )
+        self.df_nan = self.df.copy()
+        self.df_nan.iloc[:10000, :] = np.nan
+
+        self.s = Series(np.random.randint(0, 1000, size=10000))
+        self.s_str = Series(np.tile(tm.makeStringIndex(1000).values, 10))
+
+        N = 1000000
+        K = 10000
+        key1 = np.random.randint(0, K, size=N)
+        self.df_int = DataFrame({"key1": key1})
+        self.df_bool = DataFrame(np.random.randint(0, 2, size=(K, 10), dtype=bool))
+
+    def time_frame_drop_dups(self, inplace):
+        execute(self.df.drop_duplicates(["key1", "key2"], inplace=inplace))
+
+    def time_frame_drop_dups_na(self, inplace):
+        execute(self.df_nan.drop_duplicates(["key1", "key2"], inplace=inplace))
+
+    def time_series_drop_dups_int(self, inplace):
+        execute(self.s.drop_duplicates(inplace=inplace))
+
+    def time_series_drop_dups_string(self, inplace):
+        execute(self.s_str.drop_duplicates(inplace=inplace))
+
+    def time_frame_drop_dups_int(self, inplace):
+        execute(self.df_int.drop_duplicates(inplace=inplace))
+
+    def time_frame_drop_dups_bool(self, inplace):
+        execute(self.df_bool.drop_duplicates(inplace=inplace))
+
+class Align:
+    # blog "pandas escaped the zoo"
+    def setup(self):
+        n = 50000
+        indices = tm.makeStringIndex(n)
+        subsample_size = 40000
+        self.x = Series(np.random.randn(n), indices)
+        self.y = Series(
+            np.random.randn(subsample_size),
+            index=np.random.choice(indices, subsample_size, replace=False),
+        )
+
+    def time_align_series_irregular_string(self):
+        execute(self.x + self.y)
