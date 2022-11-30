8ceb88f9e7e1c464e3b2e23874f2e8349379fc20
Myachev <anatoly.myachev@intel.com>
2022-11-30 21:21:00 +0100

    address review comments
    Signed-off-by: Myachev <anatoly.myachev@intel.com>

--- asv_bench/benchmarks/utils/common.py
+++ asv_bench/benchmarks/utils/common.py
@@ -496,10 +496,9 @@ def execute(
             df._query_compiler._modin_frame._execute()
             return
         partitions = df._query_compiler._modin_frame._partitions.flatten()
-        if len(partitions) > 0 and hasattr(partitions[0], "wait"):
-            df._query_compiler._modin_frame._partition_mgr_cls.wait_partitions(
-                partitions
-            )
+        mgr_cls = df._query_compiler._modin_frame._partition_mgr_cls
+        if len(partitions) and hasattr(mgr_cls, "wait_partitions"):
+            mgr_cls.wait_partitions(partitions)
             return
 
         # compatibility with old Modin versions
