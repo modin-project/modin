d90347cafd6c14c7af8c203b3d9f5176979cb968
Andrey <andrey.a.pavlenko@gmail.com>
2022-12-07 14:24:52 +0100

    Update modin/experimental/core/execution/native/implementations/hdk_on_native/dataframe/utils.py
    Co-authored-by: Iaroslav Igoshev <Poolliver868@mail.ru>

--- modin/experimental/core/execution/native/implementations/hdk_on_native/dataframe/utils.py
+++ modin/experimental/core/execution/native/implementations/hdk_on_native/dataframe/utils.py
@@ -11,7 +11,7 @@
 # ANY KIND, either express or implied. See the License for the specific language
 # governing permissions and limitations under the License.
 
-"""Utilities for internal use by te hdk_on_native module."""
+"""Utilities for internal use by the ``HdkOnNativeDataframe``."""
 
 
 import pandas
