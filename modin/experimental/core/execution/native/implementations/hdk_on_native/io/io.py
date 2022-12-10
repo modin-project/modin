0bdaf70a13c965f96a72f47f875b889fb91c0ed2
Anatoly Myachev <anatoly.myachev@intel.com>
2022-12-10 12:15:23 +0100

    fix delimiter
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- modin/experimental/core/execution/native/implementations/hdk_on_native/io/io.py
+++ modin/experimental/core/execution/native/implementations/hdk_on_native/io/io.py
@@ -146,8 +146,11 @@ class HdkOnNativeIO(BaseIO, TextFileDispatcher):
 
             sep = kwargs["sep"]
             delimiter = kwargs["delimiter"]
-            if delimiter is None and sep is not lib.no_default:
-                delimiter = sep
+            if delimiter is None:
+                if sep is not lib.no_default:
+                    delimiter = sep
+                else:
+                    delimiter = ","
 
             usecols_md = cls._prepare_pyarrow_usecols(kwargs)
 
