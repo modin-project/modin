c3efc141c95281adb24e6662cd0af57d6aac795a
mvashishtha <mahesh@ponder.io>
2023-01-13 12:39:20 -0800

    Fix client qc.
    Signed-off-by: mvashishtha <mahesh@ponder.io>

--- modin/core/execution/client/query_compiler.py
+++ modin/core/execution/client/query_compiler.py
@@ -356,7 +356,7 @@ class ClientQueryCompiler(BaseQueryCompiler):
         if lower_is_qc:
             lower = lower._id
 
-    def isin(self, values):
+    def isin(self, values, values_is_series: bool):
         # isin is unusal because it passes API layer objects to query compiler
         # instead of converting them to query compiler objects (Modin issue #3106)
         from modin.pandas import DataFrame, Series
@@ -364,7 +364,9 @@ class ClientQueryCompiler(BaseQueryCompiler):
         is_qc = isinstance(values, (DataFrame, Series))
         if is_qc:
             values = values._query_compiler._id
-        return self.__constructor__(self._service.isin(self._id, values, is_qc))
+        return self.__constructor__(
+            self._service.isin(self._id, values, is_qc, values_is_series)
+        )
 
     def where(self, cond, other, **kwargs):
         cond_is_qc = isinstance(cond, type(self))
