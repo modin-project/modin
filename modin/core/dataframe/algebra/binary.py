dbb7da9a26c78147ed6a3ba19ea3cbc4f2e7df44
Arun Jose <40291569+arunjose696@users.noreply.github.com>
2023-01-16 09:23:24 +0100

    Update modin/core/dataframe/algebra/binary.py
    Co-authored-by: Iaroslav Igoshev <Poolliver868@mail.ru>

--- modin/core/dataframe/algebra/binary.py
+++ modin/core/dataframe/algebra/binary.py
@@ -67,8 +67,8 @@ def compute_dtypes_common_cast(first, second) -> np.dtype:
     -----
     The dtypes of the operands are supposed to be known.
     """
-    dtypes_first = dict(zip(first.columns, first._modin_frame._dtypes))
-    dtypes_second = dict(zip(second.columns, second._modin_frame._dtypes))
+    dtypes_first = first._modin_frame._dtypes.to_dict()
+    dtypes_second = second._modin_frame._dtypes.to_dict()
     columns_first = set(first.columns)
     columns_second = set(second.columns)
     common_columns = columns_first.intersection(columns_second)
