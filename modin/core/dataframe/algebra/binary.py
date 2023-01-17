a776db5c77d5b91e5a34f0c0da4a911b8790936a
Arun Jose <40291569+arunjose696@users.noreply.github.com>
2023-01-17 18:40:51 +0100

    PERF-#5182: Precompute dtypes when performing binary operations in certain cases (#5494)
    Co-authored-by: Iaroslav Igoshev <Poolliver868@mail.ru>
    Signed-off-by: arunjose696 <arunjose696@gmail.com>

--- modin/core/dataframe/algebra/binary.py
+++ modin/core/dataframe/algebra/binary.py
@@ -19,11 +19,126 @@ import pandas
 from .operator import Operator
 
 
+def coerce_int_to_float64(dtype: np.dtype) -> np.dtype:
+    """
+    Coerce dtype to float64 if it is a variant of integer.
+
+    If dtype is integer, function returns float64 datatype.
+    If not, returns the datatype argument itself.
+
+    Parameters
+    ----------
+    dtype : np.dtype
+        NumPy datatype.
+
+    Returns
+    -------
+    dtype : np.dtype
+        Returns float64 for all int datatypes or returns the datatype itself
+        for other types.
+
+    Notes
+    -----
+    Used to precompute datatype in case of division in pandas.
+    """
+    if dtype in np.sctypes["int"] + np.sctypes["uint"]:
+        return np.dtype(np.float64)
+    else:
+        return dtype
+
+
+def compute_dtypes_common_cast(first, second) -> np.dtype:
+    """
+    Precompute data types for binary operations by finding common type between operands.
+
+    Parameters
+    ----------
+    first : PandasQueryCompiler
+        First operand for which the binary operation would be performed later.
+    second : PandasQueryCompiler
+        Second operand for which the binary operation would be performed later.
+
+    Returns
+    -------
+    dtypes
+        The pandas series with precomputed dtypes.
+
+    Notes
+    -----
+    The dtypes of the operands are supposed to be known.
+    """
+    dtypes_first = first._modin_frame._dtypes.to_dict()
+    dtypes_second = second._modin_frame._dtypes.to_dict()
+    columns_first = set(first.columns)
+    columns_second = set(second.columns)
+    common_columns = columns_first.intersection(columns_second)
+    mismatch_columns = columns_first.union(columns_second) - common_columns
+    # If at least one column doesn't match, the result of the non matching column would be nan.
+    nan_dtype = np.dtype(type(np.nan))
+    dtypes = pandas.Series(
+        [
+            pandas.core.dtypes.cast.find_common_type(
+                [
+                    dtypes_first[x],
+                    dtypes_second[x],
+                ]
+            )
+            for x in common_columns
+        ],
+        index=common_columns,
+    )
+    dtypes = pandas.concat(
+        [
+            dtypes,
+            pandas.Series(
+                [nan_dtype] * (len(mismatch_columns)),
+                index=mismatch_columns,
+            ),
+        ]
+    )
+    dtypes = dtypes.sort_index()
+    return dtypes
+
+
+def compute_dtypes_boolean(first, second) -> np.dtype:
+    """
+    Precompute data types for boolean operations.
+
+    Parameters
+    ----------
+    first : PandasQueryCompiler
+        First operand for which the binary operation would be performed later.
+    second : PandasQueryCompiler
+        Second operand for which the binary operation would be performed later.
+
+    Returns
+    -------
+    dtypes
+        The pandas series with precomputed dtypes.
+
+    Notes
+    -----
+    Finds a union of columns and finds dtypes for all these columns.
+    """
+    columns_first = set(first.columns)
+    columns_second = set(second.columns)
+    columns_union = columns_first.union(columns_second)
+    dtypes = pandas.Series([np.dtype(bool)] * len(columns_union), index=columns_union)
+    dtypes = dtypes.sort_index()
+    return dtypes
+
+
 class Binary(Operator):
     """Builder class for Binary operator."""
 
     @classmethod
-    def register(cls, func, join_type="outer", labels="replace"):
+    def register(
+        cls,
+        func,
+        join_type="outer",
+        labels="replace",
+        infer_dtypes=None,
+    ):
         """
         Build template binary operator.
 
@@ -36,6 +151,14 @@ class Binary(Operator):
         labels : {"keep", "replace", "drop"}, default: "replace"
             Whether keep labels from left Modin DataFrame, replace them with labels
             from joined DataFrame or drop altogether to make them be computed lazily later.
+        infer_dtypes : {"common_cast", "float", "bool", None}, default: None
+            How dtypes should be inferred.
+                * If "common_cast", casts to common dtype of operand columns.
+                * If "float", performs type casting by finding common dtype.
+                  If the common dtype is any of the integer types, perform type casting to float.
+                  Used in case of truediv.
+                * If "bool", dtypes would be a boolean series with same size as that of operands.
+                * If ``None``, do not infer new dtypes (they will be computed manually once accessed).
 
         Returns
         -------
@@ -96,11 +219,23 @@ class Binary(Operator):
                         )
                     )
                 else:
+                    if (
+                        other._modin_frame._dtypes is not None
+                        and query_compiler._modin_frame._dtypes is not None
+                    ):
+                        if infer_dtypes == "bool":
+                            dtypes = compute_dtypes_boolean(query_compiler, other)
+                        if infer_dtypes == "common_cast":
+                            dtypes = compute_dtypes_common_cast(query_compiler, other)
+                        elif infer_dtypes == "float":
+                            dtypes = compute_dtypes_common_cast(query_compiler, other)
+                            dtypes = dtypes.apply(coerce_int_to_float64)
                     return query_compiler.__constructor__(
                         query_compiler._modin_frame.n_ary_op(
                             lambda x, y: func(x, y, *args, **kwargs),
                             [other._modin_frame],
                             join_type=join_type,
+                            dtypes=dtypes,
                         )
                     )
             else:
