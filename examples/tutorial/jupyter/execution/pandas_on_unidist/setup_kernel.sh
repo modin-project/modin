15c9563dace428d47437fcccc04f662fe8058c8a
Kirill Suvorov <kirill.suvorov@intel.com>
2023-01-18 13:27:28 +0100

    Check kernel configuration

--- examples/tutorial/jupyter/execution/pandas_on_unidist/setup_kernel.sh
+++ examples/tutorial/jupyter/execution/pandas_on_unidist/setup_kernel.sh
@@ -1,4 +1,6 @@
+kernel_prefix=$(which python | dirname $(awk '{print $0}') | dirname $(awk '{print $0}'))
 # create a new kernel configuration
-python -m ipykernel install --user --name python3mpi --display-name "Python 3 (ipykernel) with MPI"
+python -m ipykernel install --name python3mpi --display-name "Python 3 (ipykernel) with MPI" --prefix $kernel_prefix
 # update created kernel configuration for mpi execution
-sed -i 's/\"argv\"\: \[\n/\"argv\"\: \[ \"mpiexec\"\, \"-n\"\, \"1\"\,/' $(jupyter kernelspec list | grep python3mpi | awk '{print $2"/kernel.json"}')
+configuration_file=$(jupyter kernelspec list | grep python3mpi | awk '{print $2"/kernel.json"}')
+sed -i 's/\"argv\"\: \[/\"argv\"\: \[ \"mpiexec\"\, \"-n\"\, \"1\"\,/' $configuration_file
