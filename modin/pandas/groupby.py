aa2114676750e069f57be305989e52f09f30b129
Karthik Velayutham <vkarthik@ponder.io>
2023-01-17 18:59:23 -0600

    FIX: Set numeric_only to True in groupby quantile

--- modin/pandas/groupby.py
+++ modin/pandas/groupby.py
@@ -839,7 +839,7 @@ class DataFrameGroupBy(ClassLogger):
         return self._check_index(
             self._wrap_aggregation(
                 type(self._query_compiler).groupby_quantile,
-                numeric_only=False,
+                numeric_only=True,
                 agg_kwargs=dict(q=q, interpolation=interpolation),
             )
         )
