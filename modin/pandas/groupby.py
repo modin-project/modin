c11ff2a319b1d8ff51371129c7688b1d6b4273e5
Karthik Velayutham <karthik.velayutham@gmail.com>
2023-01-23 11:49:32 -0600

    Add QC method for groupby.sem (#47) (core)

--- modin/pandas/groupby.py
+++ modin/pandas/groupby.py
@@ -221,7 +221,11 @@ class DataFrameGroupBy(ClassLogger):
         return self._default_to_pandas(lambda df: df.ffill(limit=limit))
 
     def sem(self, ddof=1):
-        return self._default_to_pandas(lambda df: df.sem(ddof=ddof))
+        return self._wrap_aggregation(
+            type(self._query_compiler).groupby_sem,
+            agg_kwargs=dict(ddof=ddof),
+            numeric_only=True,
+        )
 
     def value_counts(
         self,
@@ -674,10 +678,13 @@ class DataFrameGroupBy(ClassLogger):
                 kwargs = {}
             func = func_dict
         elif is_list_like(func):
-            return self._default_to_pandas(
-                lambda df, *args, **kwargs: df.aggregate(func, *args, **kwargs),
-                *args,
-                **kwargs,
+            return self._wrap_aggregation(
+                qc_method=type(self._query_compiler).groupby_agg,
+                numeric_only=False,
+                agg_func=func,
+                agg_args=args,
+                agg_kwargs=kwargs,
+                how="axis_wise",
             )
         elif callable(func):
             return self._check_index(
