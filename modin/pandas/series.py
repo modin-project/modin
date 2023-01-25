ba7ba0b09ea6240e5cf7619cb1aa47acbcc3aff5
Naren Krishna <92325366+naren-ponder@users.noreply.github.com>
2023-01-25 15:42:41 -0800

    Fix nlargest and nsmallest Series support (#46) (core)
    Signed-off-by: Naren Krishna <naren@ponder.io>

--- modin/pandas/series.py
+++ modin/pandas/series.py
@@ -1290,9 +1290,9 @@ class Series(BasePandasDataset):
             raise NotImplementedError(
                 "Series.nlargest is not implemented for empty Series."
             )
-        return DataFrame(
+        return Series(
             query_compiler=self._query_compiler.nlargest(
-                n, [self._query_compiler.columns[0]], keep
+                n=n, columns=self.name, keep=keep
             )
         )
 
@@ -1302,11 +1302,11 @@ class Series(BasePandasDataset):
         """
         if len(self._query_compiler.columns) == 0:
             raise NotImplementedError(
-                "Series.nlargest is not implemented for empty Series."
+                "Series.nsmallest is not implemented for empty Series."
             )
-        return DataFrame(
+        return Series(
             query_compiler=self._query_compiler.nsmallest(
-                n, [self._query_compiler.columns[0]], keep
+                n=n, columns=self.name, keep=keep
             )
         )
 
