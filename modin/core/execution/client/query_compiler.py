466d8c725cbe9bfae0e5e9ce7786df70d4792621
Naren Krishna <92325366+naren-ponder@users.noreply.github.com>
2023-01-25 15:42:41 -0800

    Fix nlargest and nsmallest Series support (#46) (service)
    Signed-off-by: Naren Krishna <naren@ponder.io>

--- modin/core/execution/client/query_compiler.py
+++ modin/core/execution/client/query_compiler.py
@@ -213,10 +213,10 @@ class ClientQueryCompiler(BaseQueryCompiler):
             self._service.concat(self._id, axis, other, **kwargs)
         )
 
-    def sort_rows_by_column_values(self, columns, ascending=True, **kwargs):
+    def sort_rows_by_column_values(self, columns, ascending=True, handle_duplicates=None, **kwargs):
         return self.__constructor__(
             self._service.sort_rows_by_column_values(
-                self._id, columns, ascending=ascending, **kwargs
+                self._id, columns, ascending=ascending, handle_duplicates=None, **kwargs
             )
         )
 
