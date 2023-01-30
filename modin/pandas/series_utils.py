b6231d5c19d1e09e32ecb19f74a7b7e2b606b19b
Hazem Elmeleegy <hazem@ponder.io>
2023-01-30 02:01:57 -0800

    Fix dt_day_of_week/day_of_year, str_partition/replace/rpartition

--- modin/pandas/series_utils.py
+++ modin/pandas/series_utils.py
@@ -141,8 +141,14 @@ class StringMethods(ClassLogger):
     def cat(self, others=None, sep=None, na_rep=None, join=None):
         if isinstance(others, Series):
             others = others._to_pandas()
-        return self._default_to_pandas(
-            pandas.Series.str.cat, others=others, sep=sep, na_rep=na_rep, join=join
+        data = Series(query_compiler=self._query_compiler)
+        return data._reduce_dimension(
+            self._query_compiler.str_cat(
+                others=others,
+                sep=sep,
+                na_rep=na_rep,
+                join=join
+            )
         )
 
     def decode(self, encoding, errors="strict"):
@@ -329,9 +335,20 @@ class StringMethods(ClassLogger):
     def partition(self, sep=" ", expand=True):
         if sep is not None and len(sep) == 0:
             raise ValueError("empty separator")
-        return Series(
-            query_compiler=self._query_compiler.str_partition(sep=sep, expand=expand)
-        )
+
+        if expand:
+            from .dataframe import DataFrame
+            return DataFrame(
+                query_compiler=self._query_compiler.str_partition(
+                    sep=sep, expand=expand
+                )
+            )
+        else:
+            return Series(
+                query_compiler=self._query_compiler.str_partition(
+                    sep=sep, expand=expand
+                )
+            )
 
     def removeprefix(self, prefix):
         return Series(query_compiler=self._query_compiler.str_removeprefix(prefix))
@@ -343,11 +360,22 @@ class StringMethods(ClassLogger):
         return Series(query_compiler=self._query_compiler.str_repeat(repeats))
 
     def rpartition(self, sep=" ", expand=True):
-        return Series(
-            query_compiler=self._query_compiler.str_rpartition(
-                sep=sep, expand=expand
+        if sep is not None and len(sep) == 0:
+            raise ValueError("empty separator")
+
+        if expand:
+            from .dataframe import DataFrame
+            return DataFrame(
+                query_compiler=self._query_compiler.str_rpartition(
+                    sep=sep, expand=expand
+                )
             )
-        )
+        else:
+            return Series(
+                query_compiler=self._query_compiler.str_rpartition(
+                    sep=sep, expand=expand
+                )
+            )
 
     def lower(self):
         return Series(query_compiler=self._query_compiler.str_lower())
