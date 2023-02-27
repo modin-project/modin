819faec22d5d51cc85936bd30205803a1ec0e963
Rehan Durrani <rehan@ponder.io>
2023-02-26 22:43:22 -0800

    Add type hints
    Signed-off-by: Rehan Durrani <rehan@ponder.io>

--- modin/error_message.py
+++ modin/error_message.py
@@ -94,7 +94,7 @@ class ErrorMessage(object):
         )
 
     @classmethod
-    def bad_type_for_numpy_op(cls, function_name, operand_type) -> None:
+    def bad_type_for_numpy_op(cls, function_name: str, operand_type: type) -> None:
         cls.single_warning(
             f"Modin NumPy only supports objects of modin.numpy.array types for {function_name}, not {operand_type}. Defaulting to NumPy."
         )
