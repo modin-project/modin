b831cc53f1e9a3c7f37473f5002391085dfc6b88
Anatoly Myachev <anatoly.myachev@intel.com>
2023-03-28 19:45:10 +0200

    FEAT-#5880: add to_sql parallel implementation for Dask
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- modin/core/execution/ray/implementations/pandas_on_ray/io/io.py
+++ modin/core/execution/ray/implementations/pandas_on_ray/io/io.py
@@ -112,8 +112,7 @@ class PandasOnRayIO(RayIO):
         # Ensure that the metadata is syncrhonized
         qc._modin_frame._propagate_index_objs(axis=None)
         result = qc._modin_frame.apply_full_axis(1, func, new_index=[], new_columns=[])
-        # FIXME: we should be waiting for completion less expensievely, maybe use _modin_frame.materialize()?
-        result.to_pandas()  # blocking operation
+        result._partition_mgr_cls.wait_partitions(result._partitions.flatten())
 
     @staticmethod
     def _to_csv_check_support(kwargs):
