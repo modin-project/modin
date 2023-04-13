db67661fef126194033eb80521934d14e76e326b
Anatoly Myachev <anatoly.myachev@intel.com>
2023-04-13 22:05:38 +0200

    update 'all', 'any'
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- modin/pandas/base.py
+++ modin/pandas/base.py
@@ -732,7 +732,7 @@ class BasePandasDataset(ClassLogger):
         )
 
     def all(
-        self, axis=0, bool_only=None, skipna=True, level=None, **kwargs
+        self, axis=0, bool_only=None, skipna=True, **kwargs
     ):  # noqa: PR01, RT01, D200
         """
         Return whether all elements are True, potentially over an axis.
@@ -749,53 +749,35 @@ class BasePandasDataset(ClassLogger):
                     )
                 data_for_compute = self[self.columns[self.dtypes == np.bool_]]
                 return data_for_compute.all(
-                    axis=axis, bool_only=False, skipna=skipna, level=level, **kwargs
+                    axis=axis, bool_only=False, skipna=skipna, **kwargs
                 )
-            if level is not None:
-                if bool_only is not None:
-                    raise NotImplementedError(
-                        "Option bool_only is not implemented with option level."
-                    )
-                if (
-                    not self._query_compiler.has_multiindex(axis=axis)
-                    and (level > 0 or level < -1)
-                    and level != self.index.name
-                ):
-                    raise ValueError(
-                        "level > 0 or level < -1 only valid with MultiIndex"
-                    )
-                return self.groupby(level=level, axis=axis, sort=False).all(**kwargs)
             return self._reduce_dimension(
                 self._query_compiler.all(
-                    axis=axis, bool_only=bool_only, skipna=skipna, level=level, **kwargs
+                    axis=axis, bool_only=bool_only, skipna=skipna, **kwargs
                 )
             )
         else:
             if bool_only:
                 raise ValueError("Axis must be 0 or 1 (got {})".format(axis))
             # Reduce to a scalar if axis is None.
-            if level is not None:
-                raise ValueError("Must specify 'axis' when aggregating by level")
-            else:
-                result = self._reduce_dimension(
-                    # FIXME: Judging by pandas docs `**kwargs` serves only compatibility
-                    # purpose and does not affect the result, we shouldn't pass them to the query compiler.
-                    self._query_compiler.all(
-                        axis=0,
-                        bool_only=bool_only,
-                        skipna=skipna,
-                        level=level,
-                        **kwargs,
-                    )
+            result = self._reduce_dimension(
+                # FIXME: Judging by pandas docs `**kwargs` serves only compatibility
+                # purpose and does not affect the result, we shouldn't pass them to the query compiler.
+                self._query_compiler.all(
+                    axis=0,
+                    bool_only=bool_only,
+                    skipna=skipna,
+                    **kwargs,
                 )
+            )
             if isinstance(result, BasePandasDataset):
                 return result.all(
-                    axis=axis, bool_only=bool_only, skipna=skipna, level=level, **kwargs
+                    axis=axis, bool_only=bool_only, skipna=skipna, **kwargs
                 )
             return result
 
     def any(
-        self, axis=0, bool_only=None, skipna=True, level=None, **kwargs
+        self, *, axis=0, bool_only=None, skipna=True, **kwargs
     ):  # noqa: PR01, RT01, D200
         """
         Return whether any element is True, potentially over an axis.
@@ -812,46 +794,28 @@ class BasePandasDataset(ClassLogger):
                     )
                 data_for_compute = self[self.columns[self.dtypes == np.bool_]]
                 return data_for_compute.any(
-                    axis=axis, bool_only=False, skipna=skipna, level=level, **kwargs
+                    axis=axis, bool_only=False, skipna=skipna, **kwargs
                 )
-            if level is not None:
-                if bool_only is not None:
-                    raise NotImplementedError(
-                        "Option bool_only is not implemented with option level."
-                    )
-                if (
-                    not self._query_compiler.has_multiindex(axis=axis)
-                    and (level > 0 or level < -1)
-                    and level != self.index.name
-                ):
-                    raise ValueError(
-                        "level > 0 or level < -1 only valid with MultiIndex"
-                    )
-                return self.groupby(level=level, axis=axis, sort=False).any(**kwargs)
             return self._reduce_dimension(
                 self._query_compiler.any(
-                    axis=axis, bool_only=bool_only, skipna=skipna, level=level, **kwargs
+                    axis=axis, bool_only=bool_only, skipna=skipna, **kwargs
                 )
             )
         else:
             if bool_only:
                 raise ValueError("Axis must be 0 or 1 (got {})".format(axis))
             # Reduce to a scalar if axis is None.
-            if level is not None:
-                raise ValueError("Must specify 'axis' when aggregating by level")
-            else:
-                result = self._reduce_dimension(
-                    self._query_compiler.any(
-                        axis=0,
-                        bool_only=bool_only,
-                        skipna=skipna,
-                        level=level,
-                        **kwargs,
-                    )
+            result = self._reduce_dimension(
+                self._query_compiler.any(
+                    axis=0,
+                    bool_only=bool_only,
+                    skipna=skipna,
+                    **kwargs,
                 )
+            )
             if isinstance(result, BasePandasDataset):
                 return result.any(
-                    axis=axis, bool_only=bool_only, skipna=skipna, level=level, **kwargs
+                    axis=axis, bool_only=bool_only, skipna=skipna, **kwargs
                 )
             return result
 
