e2af3860423625ec45e9a5f7bf7a4c7607c2bd82
Anatoly Myachev <anatoly.myachev@intel.com>
2023-04-20 21:42:05 +0200

    address review comments
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- modin/core/execution/unidist/implementations/pandas_on_unidist/partitioning/virtual_partition.py
+++ modin/core/execution/unidist/implementations/pandas_on_unidist/partitioning/virtual_partition.py
@@ -52,7 +52,7 @@ class PandasOnUnidistDataframeVirtualPartition(PandasDataframeAxisPartition):
     instance_type = unidist.core.base.object_ref.ObjectRef
     axis = None
 
-    # these variables are specially initialized at runtime (#6023)
+     # these variables are intentionally initialized at runtime (see #6023)
     _DEPLOY_AXIS_FUNC = None
     _DEPLOY_SPLIT_FUNC = None
     _DRAIN_FUNC = None
