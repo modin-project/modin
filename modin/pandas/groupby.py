34d6b6cd0f797dd7530ccbf801766af0c54f36f7
Rehan Durrani <rehan@ponder.io>
2023-05-21 19:59:04 -0700

    Remove unnecessary error checking
    Signed-off-by: Rehan Durrani <rehan@ponder.io>

--- modin/pandas/groupby.py
+++ modin/pandas/groupby.py
@@ -340,9 +340,6 @@ class DataFrameGroupBy(ClassLogger):
         )
 
     def idxmax(self, axis=0, skipna=True, numeric_only=True):
-        if any(d == np.dtype("O") for d in self._df._get_dtypes()):
-            raise TypeError("reduce operation 'argmin' not allowed for this dtype")
-
         return self._wrap_aggregation(
             type(self._query_compiler).groupby_idxmax,
             agg_kwargs=dict(axis=axis, skipna=skipna),
@@ -687,9 +684,6 @@ class DataFrameGroupBy(ClassLogger):
         return self.fillna(limit=limit, method="bfill")
 
     def idxmin(self, axis=0, skipna=True, numeric_only=True):
-        if any(d == np.dtype("O") for d in self._df._get_dtypes()):
-            raise TypeError("reduce operation 'argmin' not allowed for this dtype")
-
         return self._wrap_aggregation(
             type(self._query_compiler).groupby_idxmin,
             agg_kwargs=dict(axis=axis, skipna=skipna),
