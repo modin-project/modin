6d7e8d260bf08a7bb304c258e902c237736aa043
Anatoly Myachev <anatoly.myachev@intel.com>
2023-06-06 01:14:24 +0200

    try to avoid old logic for processing 'numeric_only' param
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- modin/core/storage_formats/pandas/query_compiler.py
+++ modin/core/storage_formats/pandas/query_compiler.py
@@ -3714,21 +3714,12 @@ class PandasQueryCompiler(BaseQueryCompiler):
 
         # that means that exception in `compute_groupby` was raised
         # in every partition, so we also should raise it
-        # TODO: we should be able to drop this logic with pandas 2.0.0 as it removes `numeric_only=None`
-        # parameter for groupby thus making the behavior of processing of non-numeric columns more
-        # predictable (we can decide whether to raise an exception before actually executing groupby)
-        if len(result.columns) == 0 and len(self.columns) != 0:
-            # determening type of raised exception by applying `aggfunc`
-            # to empty DataFrame
-            try:
-                pandas.DataFrame(index=[1], columns=[1]).agg(agg_func) if isinstance(
-                    agg_func, dict
-                ) else agg_func(
-                    pandas.DataFrame(index=[1], columns=[1]).groupby(level=0),
-                    **agg_kwargs,
-                )
-            except Exception as err:
-                raise type(err)("No numeric types to aggregate.")
+        if (
+            len(result.columns) == 0
+            and len(self.columns) != 0
+            and agg_kwargs.get("numeric_only", False)
+        ):
+            raise TypeError("No numeric types to aggregate.")
 
         return result
 
