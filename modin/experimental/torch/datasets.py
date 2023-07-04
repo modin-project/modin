f47007da0ca7c1d0f80824e90d3e683e5f53f949
RenChu Wang <patrick1031wang@gmail.com>
2023-07-03 21:28:21 -0400

    FEAT-#6139: Made it more performant.
    I was fixating on DataLoader interop. By not using it, it's much faster.
    
    Signed-off-by: RenChu Wang <patrick1031wang@gmail.com>

--- modin/experimental/torch/datasets.py
+++ modin/experimental/torch/datasets.py
@@ -12,90 +12,69 @@
 # governing permissions and limitations under the License.
 from __future__ import annotations
 
+import math
+from typing import Iterable, Sequence, Hashable
+
 import numpy as np
 from pandas import DataFrame
-from torch.utils import data as data_utils
-from torch.utils.data import DataLoader, IterableDataset
 
 from modin.pandas import DataFrame as ModinDataFrame
 
 
-class ModinIterableDataset(IterableDataset):
-    """
-    PandasIterableDataset is responsible for converting a pandas/modin dataframe into a torch-compatible dataset.
-    """
+class ModinDataLoader:
+    "A self explainatory class to convert a DataFrame into a DataLoader that batches rows."
 
     def __init__(
         self,
         df: DataFrame | ModinDataFrame,
-        with_index: bool = False,
+        batch_size: int,
+        features: Sequence[Hashable] = (),
     ) -> None:
         """
+        Converts a Pandas/Modin DataFrame into a torch DataLoader.
+
+        NOTE: This function should eventually go into modin/utils.py.
+
         Parameters
         ----------
-        df : DataFrame | ModinDataFrame
-            DataFrame-like object from which the iterable will draw data.
+        df : DataFrame
 
-        with_index : bool, default: False
-            If true, include the index object with the resulting iterable, similar to `DataFrame.iterrows`.
+        batch_size : int, default: 1
+
+        features : Sequence[Hashable], default: ()
+            If specified, only these features will be used.
+
+        Returns
+        -------
+        DataLoader
+            DataLoader object backed by desired data.
         """
 
-        super().__init__()
+        self._batch_size = batch_size
+
+        if features:
+            df = df[features]
+
         self._df = df
-        self._with_index = with_index
+
+    def __len__(self):
+        "Batched so the length is reduced."
+        return math.ceil(len(self._df) / self._batch_size)
+
+    def __getitem__(self, idx: int):
+        "Using iloc to perform batched query."
+
+        idx_start = idx * self._batch_size
+        idx_end = min((idx + 1) * self._batch_size, len(self._df))
+        return self._df.iloc[idx_start:idx_end].to_numpy()
 
     def __iter__(self):
-        worker_info = data_utils.get_worker_info()
-
-        # FIXME: Multi-threaded IterableDataset would generate duplicate samples.
-        # See: https://pytorch.org/docs/stable/data.html#multi-process-data-loading
-        #
-        # However, modin's iterrows doesn't have convenient row-skipping functions.
-        # And random access (un-batched) is quite slow. Don't know how to fix.
-        #
-        # This means it's running on a single thread.
-        if worker_info is None:
-            yield from self._iter()
-        else:
-            raise NotImplementedError("Multi-threaded iterator not implemented.")
-
-    def _iter(self):
-        """
-        Single threaded iterator.
-        """
-        for idx, row in self._df.iterrows():
-            np_row = np.array(row)
-
-            if self._with_index:
-                yield idx, np_row
-            else:
-                yield np_row
+        for i in range(len(self)):
+            yield self[i]
 
 
-PandasIterableDataset = ModinIterableDataset
+PandasDataLoader = ModinDataLoader
 
 
-def to_dataloader(
-    df: DataFrame | ModinDataFrame, batch_size: int = 1, with_index: bool = False
-) -> DataLoader:
-    """
-    Converts a Pandas/Modin DataFrame into a torch DataLoader.
-
-    NOTE: This function should eventually go into modin/utils.py.
-
-    Parameters
-    ----------
-    batch_size : int, default: 1
-        Batch size that dataloader uses.
-
-    with_index : bool, default: False
-        If true, include the index object with the resulting iterable, similar to `DataFrame.iterrows`.
-
-    Returns
-    -------
-    DataLoader
-        DataLoader object backed by desired data.
-    """
-
-    dataset = ModinIterableDataset(df=df, with_index=with_index)
-    return DataLoader(dataset, batch_size=batch_size)
+def to_dataloader(df: DataFrame | ModinDataFrame, batch_size: int = 1) -> Iterable:
+    return ModinDataLoader(df, batch_size=batch_size)
--- modin/experimental/torch/tests/test_dataloader.py
+++ modin/experimental/torch/tests/test_dataloader.py
@@ -15,10 +15,9 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 import pandas as pd
-import torch
 from torch import Tensor
 
-from modin.experimental.torch.datasets import to_dataloader
+from modin.experimental.torch.datasets import ModinDataLoader
 
 if TYPE_CHECKING:
     from typing import ModuleType
@@ -28,27 +27,24 @@ def _load_test_dataframe(pandas: ModuleType):
     df = pandas.read_csv(
         "https://raw.githubusercontent.com/ponder-org/ponder-datasets/main/USA_Housing.csv"
     )
-    features = df[
-        [
+    return df
+
+
+def _test_torch_dataloader(pandas: ModuleType):
+    df = _load_test_dataframe(pandas)
+    loader = ModinDataLoader(
+        df,
+        batch_size=16,
+        features=[
             "AVG_AREA_INCOME",
             "AVG_AREA_HOUSE_AGE",
             "AVG_AREA_NUM_ROOMS",
             "AVG_AREA_NUM_BEDROOMS",
             "POPULATION",
             "PRICE",
-        ]
-    ]
-    return features
-
-
-def _test_torch_dataloader(pandas: ModuleType, with_index: bool):
-    df = _load_test_dataframe(pandas)
-    loader = to_dataloader(df, batch_size=16, with_index=with_index)
+        ],
+    )
     for batch in loader:
-        if with_index:
-            idx, batch = batch
-            assert isinstance(idx, Tensor)
-            assert idx.dtype in {int, torch.int, torch.int32, torch.int64}
         assert batch.shape[0] <= 16, batch.shape
         assert batch.shape[1] == 6, batch.shape
 
