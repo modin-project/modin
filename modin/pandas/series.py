0d861f44245b5ca925cd409c20a87a40f4d443ce
Iaroslav Igoshev <iaroslav.igoshev@intel.com>
2023-07-21 16:11:01 +0200

    FIX-#6405: Apply `disable_logging` to `__getattr__` (#6406)
    Signed-off-by: Igoshev, Iaroslav <iaroslav.igoshev@intel.com>

--- modin/pandas/series.py
+++ modin/pandas/series.py
@@ -30,6 +30,7 @@ from pandas._typing import IndexKeyFunc, Axis
 from typing import Union, Optional, Hashable, TYPE_CHECKING, IO
 import warnings
 
+from modin.logging import disable_logging
 from modin.utils import (
     _inherit_docstrings,
     to_pandas,
@@ -293,6 +294,7 @@ class Series(BasePandasDataset):
     def __rfloordiv__(self, right):
         return self.rfloordiv(right)
 
+    @disable_logging
     def __getattr__(self, key):
         """
         Return item identified by `key`.
