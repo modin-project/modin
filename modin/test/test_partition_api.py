92432fadfba0b73d6ffe9a67188b5e97f1c0b17c
Anatoly Myachev <anatoly.myachev@intel.com>
2023-09-15 14:19:48 +0200

    FIX-#5164: Fix unwrap_partitions for virtual partitions when `axis=None` (#6560)
    Co-authored-by: Rehan Durrani <rehan@ponder.io>
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- modin/test/test_partition_api.py
+++ modin/test/test_partition_api.py
@@ -114,6 +114,28 @@ def test_unwrap_partitions(axis, reverse_index, reverse_columns):
                 )
 
 
+def test_unwrap_virtual_partitions():
+    # see #5164 for details
+    data = test_data["int_data"]
+    df = pd.DataFrame(data)
+    virtual_partitioned_df = pd.concat([df] * 10)
+    actual_partitions = np.array(unwrap_partitions(virtual_partitioned_df, axis=None))
+    expected_df = pd.concat([pd.DataFrame(data)] * 10)
+    expected_partitions = expected_df._query_compiler._modin_frame._partitions
+    assert expected_partitions.shape == actual_partitions.shape
+
+    for row_idx in range(expected_partitions.shape[0]):
+        for col_idx in range(expected_partitions.shape[1]):
+            df_equals(
+                get_func(
+                    expected_partitions[row_idx][col_idx]
+                    .force_materialization()
+                    .list_of_blocks[0]
+                ),
+                get_func(actual_partitions[row_idx][col_idx]),
+            )
+
+
 @pytest.mark.parametrize("column_widths", [None, "column_widths"])
 @pytest.mark.parametrize("row_lengths", [None, "row_lengths"])
 @pytest.mark.parametrize("columns", [None, "columns"])
