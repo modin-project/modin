76cf14effcd70f3cc30864b0fe90e9986c4a3570
Dmitry Chigarev <dmitry.chigarev@intel.com>
2023-10-16 12:36:52 +0000

    expand docs for 'AsyncReadMode'
    Signed-off-by: Dmitry Chigarev <dmitry.chigarev@intel.com>

--- modin/config/envvars.py
+++ modin/config/envvars.py
@@ -660,8 +660,20 @@ class AsyncReadMode(EnvironmentVariable, type=bool):
     """
     It does not wait for the end of reading information from the source.
 
-    Can break situations when reading occurs in a context, when exiting
-    from which the source is deleted.
+    It basically means, that the reading function only launches tasks for the dataframe
+    to be read/created, but not ensures that the construction is finalized by the time
+    the reading function returns a dataframe.
+
+    This option was brought to improve performance of reading/construction
+    of Modin DataFrames, however it may also:
+
+    1. Increase the peak memory consumption. Since the garbage collection of the
+    temporary objects created during the reading is now also lazy and will only
+    be performed when the reading/construction is actually finished.
+
+    2. Can break situations when the source is manually deleted after the reading
+    function returns a result, for example, when reading inside of a context-block
+    that deletes the file on ``__exit__()``.
     """
 
     varname = "MODIN_ASYNC_READ_MODE"
