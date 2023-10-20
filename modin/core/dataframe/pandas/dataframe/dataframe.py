131637faf795679dd3e2a9914ada0f290d9e0275
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2023-10-20 11:05:13 +0200

    Apply suggestions from code review

--- modin/core/dataframe/pandas/dataframe/dataframe.py
+++ modin/core/dataframe/pandas/dataframe/dataframe.py
@@ -1486,25 +1486,18 @@ class PandasDataframe(ClassLogger):
         BaseDataFrame
             Dataframe with updated dtypes.
         """
+        new_dtypes = None
         self_dtypes = self.dtypes
-        if all(is_dtype_equal(self_dtypes[k], v) for k, v in col_dtypes.items()):
-            return self
-
-        columns = col_dtypes.keys()
-        # Create Series for the updated dtypes
-        new_dtypes = self_dtypes.copy()
         # When casting to "category" we have to make up the whole axis partition
         # to get the properly encoded table of categories. Every block partition
         # will store the encoded table. That can lead to higher memory footprint.
         # TODO: Revisit if this hurts users.
         use_full_axis_cast = False
         has_categorical_cast = False
-        for i, column in enumerate(columns):
-            dtype = col_dtypes[column]
-            if (
-                not isinstance(dtype, type(self_dtypes[column]))
-                or dtype != self_dtypes[column]
-            ):
+        for column, dtype in col_dtypes.items():
+            if not is_dtype_equal(dtype, self_dtypes[column]):
+                if new_dtypes is None:
+                    new_dtypes = self_dtypes.copy()
                 # Update the new dtype series to the proper pandas dtype
                 try:
                     new_dtype = np.dtype(dtype)
@@ -1528,6 +1521,9 @@ class PandasDataframe(ClassLogger):
                     use_full_axis_cast = has_categorical_cast = True
                 else:
                     new_dtypes[column] = new_dtype
+
+        if new_dtypes is None:
+            return self.copy()
 
         def astype_builder(df):
             """Compute new partition frame with dtypes updated."""
