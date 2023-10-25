d665b150ced98a09c4770cdcab017385f0e3c4a6
Igor Zamyatin <igor.zamyatin@intel.com>
2023-10-25 15:48:03 -0500

    FIX-#6680: Specify navigation_with_keys=True to fix docs build (#6681)
    Signed-off-by: izamyati <igor.zamyatin@intel.com>

--- docs/conf.py
+++ docs/conf.py
@@ -178,6 +178,7 @@ html_theme_options = {
             "icon": "fas fa-envelope-square",
         },
     ],
+    "navigation_with_keys": True,
 }
 
 # Custom sidebar templates, must be a dictionary that maps document names
