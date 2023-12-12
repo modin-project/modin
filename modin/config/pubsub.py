d5f4670ed1ca1afc21aedd057190dd8b0396f394
Dmitry Chigarev <dmitry.chigarev@intel.com>
2023-12-12 15:45:10 +0100

    change signature of 'DeprDesc'
    Signed-off-by: Dmitry Chigarev <dmitry.chigarev@intel.com>

--- modin/config/pubsub.py
+++ modin/config/pubsub.py
@@ -24,6 +24,7 @@ from typing import (
     NamedTuple,
     Optional,
     Tuple,
+    cast,
 )
 
 if TYPE_CHECKING:
@@ -36,22 +37,22 @@ class DeprecationDescriptor:
 
     Parameters
     ----------
-    parameter : type[EnvironmentVariable]
+    parameter : type[Parameter]
         Deprecated parameter.
-    new_parameter : type[EnvironmentVariable], optional
+    new_parameter : type[Parameter], optional
         If there's a replacement parameter for the deprecated one, specify it here.
     when_removed : str, optional
         If known, the exact release when the deprecated parameter is planned to be removed.
     """
 
-    _parameter: type["EnvironmentVariable"]
-    _new_parameter: Optional[type["EnvironmentVariable"]]
+    _parameter: type["Parameter"]
+    _new_parameter: Optional[type["Parameter"]]
     _when_removed: str
 
     def __init__(
         self,
-        parameter: type["EnvironmentVariable"],
-        new_parameter: Optional[type["EnvironmentVariable"]] = None,
+        parameter: type["Parameter"],
+        new_parameter: Optional[type["Parameter"]] = None,
         when_removed: Optional[str] = None,
     ):
         self._parameter = parameter
@@ -72,11 +73,15 @@ class DeprecationDescriptor:
         -------
         str
         """
-        name = self._parameter.varname if use_envvar_names else self._parameter.__name__
+        name = (
+            cast("EnvironmentVariable", self._parameter).varname
+            if use_envvar_names
+            else self._parameter.__name__
+        )
         msg = f"'{name}' is deprecated and will be removed in {self._when_removed} version."
         if self._new_parameter is not None:
             new_name = (
-                self._new_parameter
+                cast("EnvironmentVariable", self._new_parameter).varname
                 if use_envvar_names
                 else self._new_parameter.__name__
             )
