808b0bfffc55f93b124dea40a768b54a4a525bde
Anatoly Myachev <anatoliimyachev@mail.com>
2024-01-26 13:07:52 +0100

    TEST-#6868: Remove tests for 'gs' remote protocol since we rely on 'fsspec' (#6882)
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- modin/experimental/pandas/test/test_io_exp.py
+++ modin/experimental/pandas/test/test_io_exp.py
@@ -179,14 +179,9 @@ class TestCsvGlob:
     "path",
     [
         "s3://modin-test/modin-bugs/multiple_csv/test_data*.csv",
-        "gs://modin-testing/testing/multiple_csv/test_data*.csv",
     ],
 )
 def test_read_multiple_csv_cloud_store(path, s3_resource, s3_storage_options):
-    storage_options_new = {"anon": True}
-    if path.startswith("s3"):
-        storage_options_new = s3_storage_options
-
     def _pandas_read_csv_glob(path, storage_options):
         pandas_dfs = [
             pandas.read_csv(
@@ -202,7 +197,7 @@ def test_read_multiple_csv_cloud_store(path, s3_resource, s3_storage_options):
         lambda module, **kwargs: pd.read_csv_glob(path, **kwargs).reset_index(drop=True)
         if hasattr(module, "read_csv_glob")
         else _pandas_read_csv_glob(path, **kwargs),
-        storage_options=storage_options_new,
+        storage_options=s3_storage_options,
     )
 
 
