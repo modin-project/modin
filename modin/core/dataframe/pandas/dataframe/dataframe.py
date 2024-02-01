4f06079fb6ea1fc87a5609235161ef209714ad50
Dmitry Chigarev <dmitry.chigarev@intel.com>
2024-02-01 11:57:52 +0100

    FIX-#6897: Revert unidist specific fix for groupby
    Signed-off-by: Dmitry Chigarev <dmitry.chigarev@intel.com>

--- modin/core/dataframe/pandas/dataframe/dataframe.py
+++ modin/core/dataframe/pandas/dataframe/dataframe.py
@@ -3912,9 +3912,7 @@ class PandasDataframe(ClassLogger):
                 # aligned columns
                 parts = result._partitions.flatten()
                 aligned_columns = parts[0].apply(
-                    # TODO: unidist on MPI execution requires for this function to be preprocessed,
-                    # otherwise, the execution fails. Look into the issue later.
-                    self._partition_mgr_cls.preprocess_func(compute_aligned_columns),
+                    compute_aligned_columns,
                     *[part._data for part in parts[1:]],
                     initial_columns=self.columns,
                 )
