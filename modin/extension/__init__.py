7c2a9d54b0fb0166abac5935e46bebd700122c5f
Devin Petersohn <devin.petersohn@snowflake.com>
2024-02-26 09:29:39 -0600

    Apply suggestions from code review
    Co-authored-by: Iaroslav Igoshev <Poolliver868@mail.ru>

--- modin/extension/__init__.py
+++ modin/extension/__init__.py
@@ -10,10 +10,11 @@
 # the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific language
 # governing permissions and limitations under the License.
+
 from .extensions import (
     register_dataframe_accessor,
-    register_pd_accessor,
     register_series_accessor,
+    register_pd_accessor,
 )
 
 __all__ = [
--- modin/extension/test/test_extension_dataframe.py
+++ modin/extension/test/test_extension_dataframe.py
@@ -10,6 +10,7 @@
 # the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific language
 # governing permissions and limitations under the License.
+
 import modin.pandas as pd
 from modin.extension import register_dataframe_accessor
 
