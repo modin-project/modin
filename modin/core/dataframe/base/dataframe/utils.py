b9aae195a76593cdeb22715a2fdde253a0a5b219
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2024-02-27 22:42:22 +0100

    FEAT-#6929: Implement Series.case_when in a distributed way
    Signed-off-by: Andrey Pavlenko <andrey.a.pavlenko@gmail.com>

--- modin/core/dataframe/base/dataframe/utils.py
+++ modin/core/dataframe/base/dataframe/utils.py
@@ -21,6 +21,7 @@ JoinType is an enum that represents the `join_type` or `how` argument for the jo
 from enum import Enum
 from typing import Dict, List, Sequence, Tuple, cast
 
+import numpy as np
 import pandas
 from pandas._typing import IndexLabel
 from pandas.api.types import is_scalar
@@ -150,3 +151,30 @@ def join_columns(
 
     new_columns = pandas.Index(new_left + new_right)
     return new_columns, left_renamer, right_renamer
+
+
+def is_trivial_index(index: pandas.Index) -> bool:
+    """
+    Check if the index is a trivial index, i.e. a sequence [0..n].
+
+    Parameters
+    ----------
+    index : pandas.Index
+        An index to check.
+
+    Returns
+    -------
+    bool
+    """
+    if len(index) == 0:
+        return True
+    if isinstance(index, pandas.RangeIndex):
+        return index.start == 0 and index.step == 1
+    if not (isinstance(index, pandas.Index) and index.dtype == np.int64):
+        return False
+    return (
+        index.is_monotonic_increasing
+        and index.is_unique
+        and index.min() == 0
+        and index.max() == len(index) - 1
+    )
