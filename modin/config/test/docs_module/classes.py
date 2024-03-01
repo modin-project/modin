d3b8e6f1047960a9000b3e32596a266e0036f146
Devin Petersohn <devin.petersohn@snowflake.com>
2024-03-01 12:15:27 -0600

    lint
    Signed-off-by: Devin Petersohn <devin.petersohn@snowflake.com>

--- modin/config/test/docs_module/classes.py
+++ modin/config/test/docs_module/classes.py
@@ -11,6 +11,7 @@
 # ANY KIND, either express or implied. See the License for the specific language
 # governing permissions and limitations under the License.
 
+
 class DataFrame:
     def apply(self):
         """This is a test of the documentation module for DataFrame."""
--- modin/config/test/docs_module/functions.py
+++ modin/config/test/docs_module/functions.py
@@ -11,6 +11,7 @@
 # ANY KIND, either express or implied. See the License for the specific language
 # governing permissions and limitations under the License.
 
+
 def read_csv():
     """Test override for functions on the module."""
     return
