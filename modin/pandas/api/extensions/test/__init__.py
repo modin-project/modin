c4e2921c25807b41dd05387c1ba09e4f1aa0df7a
Igoshev, Iaroslav <iaroslav.igoshev@intel.com>
2024-03-01 08:31:40 +0000

    Address comments
    Signed-off-by: Igoshev, Iaroslav <iaroslav.igoshev@intel.com>

--- modin/pandas/api/extensions/test/__init__.py
+++ modin/pandas/api/extensions/test/__init__.py
@@ -10,7 +10,3 @@
 # the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific language
 # governing permissions and limitations under the License.
-
-from modin.pandas.api import extensions
-
-__all__ = ["extensions"]
