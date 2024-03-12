622b1cdcdb84fffa11dd310f1ecd509b0ce748f2
Anatoly Myachev <anatoliimyachev@mail.com>
2024-03-12 14:33:34 +0100

    FIX-#7058: Update exception message for `insert` function (#7059)
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- modin/pandas/test/dataframe/test_map_metadata.py
+++ modin/pandas/test/dataframe/test_map_metadata.py
@@ -1344,7 +1344,15 @@ def test_insert(data):
     )
 
     # Bad inserts
-    eval_insert(modin_df, pandas_df, col="Bad Column", value=lambda df: df)
+    eval_insert(
+        modin_df,
+        pandas_df,
+        col="Bad Column",
+        value=lambda df: df,
+        raising_exceptions=ValueError(
+            f"Expected a one-dimensional object, got a DataFrame with {len(pandas_df.columns)} columns instead."
+        ),
+    )
     eval_insert(
         modin_df,
         pandas_df,
