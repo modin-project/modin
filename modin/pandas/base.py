c4bd6b295557364a2e9463b08b80812d26baf2c3
Anatoly Myachev <anatoly.myachev@intel.com>
2024-03-13 13:32:19 +0100

    TEST-#7086: Explicitly check for exceptions in 'test_reduce.py'
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- modin/pandas/base.py
+++ modin/pandas/base.py
@@ -2362,6 +2362,10 @@ class BasePandasDataset(ClassLogger):
         ascending: bool = True,
         pct: bool = False,
     ):
+        if axis is None:
+            raise ValueError(
+                f"No axis named None for object type {type(self).__name__}"
+            )
         axis = self._get_axis_number(axis)
         return self.__constructor__(
             query_compiler=self._query_compiler.rank(
