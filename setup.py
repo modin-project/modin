87d147f10d1aa3cd05eb8967865d945a9980150c
Devin Petersohn <devin.petersohn@snowflake.com>
2024-04-09 16:47:16 -0500

    FEAT-#7162: Pin pandas to 2.2.1
    This change will be reverted in a subsequent PR
    
    Signed-off-by: Devin Petersohn <devin.petersohn@snowflake.com>

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=[
-        "pandas>=2.2,<2.3",
+        "pandas==2.2.1",
         "packaging>=21.0",
         "numpy>=1.22.4",
         "fsspec>=2022.11.0",
