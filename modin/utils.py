18ec7aa9f93bdf14878cf7d89c2e3fee323a41b8
sfc-gh-mvashishtha <mahesh.vashishtha@snowflake.com>
2024-06-10 19:16:05 -0700

    Fix test
    Signed-off-by: sfc-gh-mvashishtha <mahesh.vashishtha@snowflake.com>

--- modin/utils.py
+++ modin/utils.py
@@ -403,7 +403,7 @@ def _update_inherited_docstrings(doc_module: DocModule) -> None:
         The current DocModule
     """
     for doc_inheritance_call in _docstring_inheritance_calls:
-        doc_inheritance_call(doc_module.get())
+        doc_inheritance_call(doc_module=doc_module.get())  # type: ignore[call-arg]
 
 
 def _inherit_docstrings_in_place(
