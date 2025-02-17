6d19b89311e5d2a8c58dcd77c765cb1392ec642b
John Kew <john.kew@snowflake.com>
2025-02-17 15:54:57 -0800

    update import ;)

--- modin/logging/__init__.py
+++ modin/logging/__init__.py
@@ -14,6 +14,7 @@
 from .class_logger import ClassLogger  # noqa: F401
 from .config import get_logger  # noqa: F401
 from .logger_decorator import disable_logging, enable_logging  # noqa: F401
+from .metrics import emit_metric, add_metric_handler, clear_metric_handler
 
 __all__ = [
     "ClassLogger",
