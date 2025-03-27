52ee2b033ad074f4a0975cfe596bb1bdbd799919
John Kew <john.kew@snowflake.com>
2025-03-27 12:06:25 -0700

    lint

--- modin/tests/pandas/native_df_interoperability/test_compiler_caster.py
+++ modin/tests/pandas/native_df_interoperability/test_compiler_caster.py
@@ -301,7 +301,7 @@ def test_no_solution(pico_df, local_df, cluster_df, cloud_df):
 
 
 def test_no_solution(pico_df, local_df, cluster_df, cloud_df):
-    with pytest.raises(ValueError, match=r'pico,local_machine,cluster,cloud'):
+    with pytest.raises(ValueError, match=r"pico,local_machine,cluster,cloud"):
         pico_df.concat(axis=1, other=[local_df, cluster_df, cloud_df])
 
 
