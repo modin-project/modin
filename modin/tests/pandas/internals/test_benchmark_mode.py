ff8e24d3f02deb9892b007249ea2903790154796
sfc-gh-mvashishtha <mahesh.vashishtha@snowflake.com>
2025-04-17 06:41:19 -0700

    Try empty dataframe
    Signed-off-by: sfc-gh-mvashishtha <mahesh.vashishtha@snowflake.com>

--- modin/tests/pandas/internals/test_benchmark_mode.py
+++ modin/tests/pandas/internals/test_benchmark_mode.py
@@ -57,8 +57,7 @@ def test_turn_off(set_benchmark_mode):
 @pytest.mark.parametrize("set_benchmark_mode", [True], indirect=True)
 def test_turn_on(set_benchmark_mode):
     # breakpoint()
-    df = pd.DataFrame([0]).dropna()
+    pd.DataFrame()
     # with mock.patch(wait_method) as wait:
     # breakpoint()
-    df.dropna()
     # wait.assert_called()
