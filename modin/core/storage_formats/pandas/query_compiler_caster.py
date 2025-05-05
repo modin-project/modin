428db01170c8aef5096e3e9eefe465ad25c15a83
Mahesh Vashishtha <mahesh.vashishtha@snowflake.com>
2025-05-05 10:24:53 -0700

    FIX-#7559: Create the dummy query compiler just once per backend. (#7560)
    Resolves #7559
    
    Signed-off-by: sfc-gh-mvashishtha <mahesh.vashishtha@snowflake.com>

--- modin/core/storage_formats/pandas/query_compiler_caster.py
+++ modin/core/storage_formats/pandas/query_compiler_caster.py
@@ -88,6 +88,25 @@ _CLASS_AND_BACKEND_TO_PRE_OP_SWITCH_METHODS: _AUTO_SWITCH_CLASS = _AUTO_SWITCH_C
 )
 
 
+def _get_empty_qc_for_default_backend() -> BaseQueryCompiler:
+    """
+    Get an empty query compiler for the default backend.
+
+    Returns
+    -------
+    BaseQueryCompiler
+        An empty query compiler for the default backend.
+    """
+    from modin.core.execution.dispatching.factories.dispatcher import FactoryDispatcher
+
+    return FactoryDispatcher.get_factory().io_cls.from_pandas(pandas.DataFrame())
+
+
+_BACKEND_TO_EMPTY_QC: defaultdict[str, BaseQueryCompiler] = defaultdict(
+    _get_empty_qc_for_default_backend
+)
+
+
 class QueryCompilerCaster(ABC):
     """Cast all query compiler arguments of the member function to current query compiler."""
 
@@ -793,16 +812,11 @@ def wrap_function_in_argument_caster(
         #    backends, raise a ValueError.
 
         if len(input_query_compilers) == 0:
-            # For nullary functions, we need to create a dummy query compiler
-            # to calculate the cost of switching backends.
-            from modin.core.execution.dispatching.factories.dispatcher import (
-                FactoryDispatcher,
-            )
-
-            input_qc_for_pre_op_switch = (
-                FactoryDispatcher.get_factory().io_cls.from_pandas(pandas.DataFrame())
-            )
             input_backend = Backend.get()
+            # For nullary functions, we need to create a dummy query compiler
+            # to calculate the cost of switching backends. We should only
+            # create the dummy query compiler once per backend.
+            input_qc_for_pre_op_switch = _BACKEND_TO_EMPTY_QC[input_backend]
         else:
             input_qc_for_pre_op_switch = input_query_compilers[0]
             input_backend = input_qc_for_pre_op_switch.get_backend()
