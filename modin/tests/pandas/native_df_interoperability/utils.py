7deb959a2bdcad48de859e193bed3b1662b4abf1
John Kew <john.kew@snowflake.com>
2025-09-25 19:51:52 -0700

    Minor comment inline

--- modin/tests/pandas/native_df_interoperability/utils.py
+++ modin/tests/pandas/native_df_interoperability/utils.py
@@ -45,6 +45,8 @@ def create_test_df_in_defined_mode(
 
     if not isinstance(native, bool):
         raise ValueError("`native` should be True or False.")
+
+     # Use the default backend unless native
     hybrid_backend = "Pandas" if native else Backend.get()
     with switch_to_native_execution() if native else nullcontext():
         with config_context(AutoSwitchBackend=False, Backend=hybrid_backend):
@@ -62,6 +64,7 @@ def create_test_series_in_defined_mode(
     if not isinstance(native, bool):
         raise ValueError("`native` should be True or False.")
 
+    # Use the default backend unless native
     hybrid_backend = "Pandas" if native else Backend.get()
     with switch_to_native_execution() if native else nullcontext():
         with config_context(AutoSwitchBackend=False, Backend=hybrid_backend):
