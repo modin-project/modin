2d506ae9a99c7e124c1971e8cf3d2c3269ed95e3
John Kew <john.kew@snowflake.com>
2025-09-30 23:42:41 -0700

    Update modin/tests/pandas/native_df_interoperability/test_compiler_caster.py
    Co-authored-by: Mahesh Vashishtha <mahesh.vashishtha@snowflake.com>

--- modin/tests/pandas/native_df_interoperability/test_compiler_caster.py
+++ modin/tests/pandas/native_df_interoperability/test_compiler_caster.py
@@ -729,7 +729,7 @@ def test_information_asymmetry(default_df, cloud_df, eager_df, lazy_df):
     # the other way around, eager has a special ability to
     # control the directionality of the cast.
     df = default_df.merge(eager_df)
-    assert df.get_backend() is eager_df.get_backend()
+    assert df.get_backend() == eager_df.get_backend()
     df = cloud_df.merge(eager_df)
     assert df.get_backend() is eager_df.get_backend()
 
